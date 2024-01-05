d81e48d9c29d8032b991e330549f041b6122f26d
zhennann <zhen.nann@icloud.com>
2024-01-05 12:22:52 +0800

    feat: egg

--- packages/egg-born-backend/index.js
+++ packages/egg-born-backend/index.js
@@ -5,7 +5,6 @@ require('./lib/base/json.js');
 // process.traceDeprecation = true;
 
 const moduleAlias = require('module-alias');
-moduleAlias.addAlias('egg', '@zhennann/egg');
 moduleAlias.addAlias('koa-static-cache', '@zhennann/koa-static-cache');
 
 const Master = require('egg-cluster/lib/master.js');
