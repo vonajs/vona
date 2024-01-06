ea490fa5cbde11f6643937f1f44692d85adac98e
zhennann <zhen.nann@icloud.com>
2024-01-06 18:27:50 +0800

    feat: package scripts

---
+++ scripts/egg-born-scripts.js
@@ -0,0 +1,18 @@
+const path = require('path');
+const fs = require('fs');
+
+// egg-born-scripts
+const pathEggBornBin = __getPathEggBornBin();
+if (!pathEggBornBin) {
+  process.exit(0);
+}
+require(pathEggBornBin);
+
+function __getPathEggBornBin() {
+  const basePath = path.join(__dirname, '../');
+  let cabloyPath = path.join(basePath, 'node_modules/egg-born-scripts/bin/egg-born-scripts.js');
+  if (fs.existsSync(cabloyPath)) return cabloyPath;
+  cabloyPath = path.join(basePath, 'packages-egg-born/egg-born-scripts/bin/egg-born-scripts.js');
+  if (fs.existsSync(cabloyPath)) return cabloyPath;
+  return null;
+}
