489cf35a87de4d7ea9353e356015aa09bf09966c
zhennann <zhen.nann@icloud.com>
2024-01-06 10:52:11 +0800

    chore: update egg-born

--- packages-egg-born/egg-born/lib/init_command.js
+++ packages-egg-born/egg-born/lib/init_command.js
@@ -3,43 +3,32 @@ const path = require('path');
 const randomize = require('randomatic');
 const uuid = require('uuid');
 
-co(function* () {
-  const options = {
-    name: 'egg-born',
-    configName: 'egg-born-init-config',
-    pkgInfo: require('../package.json'),
-  };
-
-  const command = new Command(options);
-
-  command.printUsage = function () {
+module.exports = class EggBornCommand extends Command {
+  constructor(options) {
+    super(options);
+  }
+  printUsage() {
     this.log(`usage:
-      - cd ${this.targetDir}
-      - npm install
-      - npm run cli
-      - npm run db:reset
-      - npm run test:backend
-      - npm run dev:backend
-      - npm run dev:front
-      - npm run build:front
-      - npm run start:backend
-      - npm run start:backend-daemon
-      - npm run stop:backend
-      - npm run debug:backend
-      - npm run cov:backend
-      - npm run lint
-      - npm run format
-    `);
-  };
+        - cd ${this.targetDir}
+        - pnpm install
+        - npm run cli
+        - npm run db:reset
+        - npm run test:backend
+        - npm run dev:backend
+        - npm run dev:front
+        - npm run build:front
+        - npm run start:backend
+        - npm run start:backend-daemon
+        - npm run stop:backend
+        - npm run debug:backend
+        - npm run cov:backend
+        - npm run lint
+        - npm run format
+      `);
+  }
 
-  command.getTemplateDir = function* () {
-    // download boilerplate
-    return yield this.downloadBoilerplate('cabloy-template-api');
-  };
-
-  const askForVariable = command.askForVariable;
-  command.askForVariable = function* (targetDir, templateDir) {
-    const locals = yield askForVariable.call(command, targetDir, templateDir);
+  async askForVariable(targetDir, templateDir) {
+    const locals = await super.askForVariable(targetDir, templateDir);
     // targetDir
     locals.targetDir = this.targetDir.replace(/\\/gi, '/');
     // publicDir
@@ -52,20 +41,8 @@ co(function* () {
     locals.safeKeys = `${uuid.v4()}_${Date.now()}_${random(100, 10000)}`;
     // ready
     return locals;
-  };
-
-  const processFiles = command.processFiles;
-  command.processFiles = function* (targetDir, templateDir) {
-    // process files
-    yield processFiles.call(command, targetDir, templateDir);
-  };
-
-  // run
-  yield command.run(process.cwd(), process.argv.slice(2));
-}).catch(err => {
-  console.error(err.stack);
-  process.exit(1);
-});
+  }
+};
 
 function random(start, end) {
   return Math.floor(Math.random() * (end - start) + start);
