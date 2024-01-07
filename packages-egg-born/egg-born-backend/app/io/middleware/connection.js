fe31a0ec35d984a0af295bb08665bea6ed049eb5
zhennann <zhen.nann@icloud.com>
2024-01-07 12:47:38 +0800

    feat: egg-born-backend

--- packages-egg-born/egg-born-backend/app/io/middleware/connection.js
+++ packages-egg-born/egg-born-backend/app/io/middleware/connection.js
@@ -1,43 +1,42 @@
-const compose = require('koa-compose');
-
-module.exports = app => {
-  function loadMiddlewares() {
-    const _middlewares = [];
-
-    // all middlewares
-    const ebMiddlewares = app.meta.middlewaresSocketIoConnection;
-    for (const item of ebMiddlewares) {
-      _middlewares.push(wrapMiddleware(item));
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const koa_compose_1 = require("koa-compose");
+function default_1(app) {
+    function loadMiddlewares() {
+        const _middlewares = [];
+        // all middlewares
+        const ebMiddlewares = app.meta.middlewaresSocketIoConnection;
+        for (const item of ebMiddlewares) {
+            _middlewares.push(wrapMiddleware(item));
+        }
+        return (0, koa_compose_1.default)(_middlewares);
     }
-    return compose(_middlewares);
-  }
-
-  let _middlewares = null;
-
-  return async (ctx, next) => {
-    if (!_middlewares) {
-      _middlewares = loadMiddlewares();
-    }
-    await _middlewares(ctx, next);
-  };
-};
-
-function wrapMiddleware(item) {
-  const fn = (ctx, next) => {
-    // enable match ignore dependencies
-    if (item.options.enable === false) {
-      return next();
-    }
-    // bean
-    const bean = item.bean;
-    // execute
-    const beanFullName = `${bean.module}.middleware.io.${bean.name}`;
-    const beanInstance = ctx.bean._getBean(beanFullName);
-    if (!beanInstance) {
-      throw new Error(`socketio middleware bean not found: ${beanFullName}`);
-    }
-    return beanInstance.execute(item.options, next);
-  };
-  fn._name = item.name;
-  return fn;
+    let _middlewares = null;
+    return async (ctx, next) => {
+        if (!_middlewares) {
+            _middlewares = loadMiddlewares();
+        }
+        await _middlewares(ctx, next);
+    };
 }
+exports.default = default_1;
+function wrapMiddleware(item) {
+    const fn = (ctx, next) => {
+        // enable match ignore dependencies
+        if (item.options.enable === false) {
+            return next();
+        }
+        // bean
+        const bean = item.bean;
+        // execute
+        const beanFullName = `${bean.module}.middleware.io.${bean.name}`;
+        const beanInstance = ctx.bean._getBean(beanFullName);
+        if (!beanInstance) {
+            throw new Error(`socketio middleware bean not found: ${beanFullName}`);
+        }
+        return beanInstance.execute(item.options, next);
+    };
+    fn._name = item.name;
+    return fn;
+}
+//# sourceMappingURL=connection.js.map
--- packages-egg-born/egg-born-backend/app/middleware/appReady.js
+++ packages-egg-born/egg-born-backend/app/middleware/appReady.js
@@ -1,10 +1,12 @@
+"use strict";
 module.exports = (options, app) => {
-  return async function (ctx, next) {
-    // check appReady
-    if (!ctx.innerAccess) {
-      await ctx.bean.instance.checkAppReady();
-    }
-    // next
-    await next();
-  };
+    return async function (ctx, next) {
+        // check appReady
+        if (!ctx.innerAccess) {
+            await ctx.bean.instance.checkAppReady();
+        }
+        // next
+        await next();
+    };
 };
+//# sourceMappingURL=appReady.js.map
