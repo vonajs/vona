d3409cb4126c637065ff6995a0ba606d2582c4b7
zhennann <zhen.nann@icloud.com>
2024-01-08 09:31:21 +0800

    feat: cluster-reload

--- packages-cabloy/core/src/index.ts
+++ packages-cabloy/core/src/index.ts
@@ -1,11 +1,10 @@
 import '@cabloy/set';
 import '@cabloy/json5';
 import 'regenerator-runtime';
+import moduleAlias from 'module-alias';
+import Master from 'egg-cluster/lib/master.js';
 
-const moduleAlias = require('module-alias');
 moduleAlias.addAlias('koa-static-cache', '@zhennann/koa-static-cache');
-
-const Master = require('egg-cluster/lib/master.js');
 Master.prototype.onReload = require('../utils/reload.js');
 
 export * from './lib/framework/framework.js';
