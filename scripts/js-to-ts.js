27c809f1d56c3d65a7e102f3c3be4946d5ce7268
zhennann <zhen.nann@icloud.com>
2024-01-08 15:37:46 +0800

    Create js-to-ts.js

---
+++ scripts/js-to-ts.js
@@ -0,0 +1,27 @@
+#!/usr/bin/env node
+
+const path = require('path');
+const fs = require('node:fs/promises');
+const eggBornUtils = require('egg-born-utils');
+const argv = require('./lib/parse_argv')('sync');
+
+(async function () {
+  await main();
+})();
+
+async function main() {
+  // paths
+  const paths = argv.args;
+  const patterns = paths.map(item => {
+    return `${item}/**/*.js`;
+  });
+  // files
+  const files = await eggBornUtils.tools.globbyAsync(patterns);
+  // convert
+  for (const file of files) {
+    const pos = String(file).lastIndexOf('.js');
+    const fileTo = String(file).substring(0, pos) + '.ts';
+    await fs.rename(file, fileTo);
+  }
+  console.log(files);
+}
