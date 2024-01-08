46c20e228e9b6d76b5738632fdae1725d6c95dab
zhennann <zhen.nann@icloud.com>
2024-01-08 10:43:38 +0800

    feat: koa-static-cache

--- packages-cabloy/core/src/index.ts
+++ packages-cabloy/core/src/index.ts
@@ -5,7 +5,7 @@ import moduleAlias from 'module-alias';
 import Master from 'egg-cluster/lib/master.js';
 import Reload from '@cabloy/cluster-reload';
 
-moduleAlias.addAlias('koa-static-cache', '@zhennann/koa-static-cache');
+moduleAlias.addAlias('koa-static-cache', '@cabloy/koa-static-cache');
 Master.prototype.onReload = Reload;
 
 export * from './lib/framework/framework.js';
