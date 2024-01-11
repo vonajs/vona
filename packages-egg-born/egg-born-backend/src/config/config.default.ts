cfdec922a38bbf4f4bff1ff08126789a5a275f61
zhennann <zhen.nann@icloud.com>
2024-01-11 15:34:48 +0800

    Update config.default.ts

--- packages-egg-born/egg-born-backend/src/config/config.default.ts
+++ packages-egg-born/egg-born-backend/src/config/config.default.ts
@@ -332,6 +332,7 @@ function getFullPath(ctx, dir, filename, options) {
 }
 
 function onQuery(hook, ms, query /* , args*/) {
+  if (!query._connection) return;
   if (!hook.meta.long_query_time || hook.meta.long_query_time < ms) {
     const message = `connectionId: ${query._connection.connectionId}, ${ms}ms ==> ${query.sql}`;
     console.log(chalk.keyword(hook.meta.color)(message));
