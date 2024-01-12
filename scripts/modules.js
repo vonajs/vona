648663f0961d3646662718af2f1032e8d89648cc
zhennann <zhen.nann@icloud.com>
2024-01-12 19:06:10 +0800

    feat: move backend

--- scripts/modules.js
+++ scripts/modules.js
@@ -1,6 +1,7 @@
 const fse = require('fs-extra');
 const { ProcessHelper } = require('@cabloy/process-helper');
 const mglob = require('@cabloy/module-glob').default;
+const eggBornUtils = require('egg-born-utils');
 const argv = require('./lib/parse_argv')('sync');
 
 (async function () {
@@ -28,13 +29,26 @@ async function main() {
 }
 
 async function _moduleHandle({ module }) {
-  await _moduleRemoveFront({ module });
+  // await _moduleRemoveFront({ module });
 }
 
 async function _moduleRemoveFront({ module }) {
   // console.log(module);
   // front/icons/src
-  await fse.remove(`${module.root}/front`);
-  await fse.remove(`${module.root}/icons`);
-  await fse.remove(`${module.root}/src`);
+  // await fse.remove(`${module.root}/front`);
+  // await fse.remove(`${module.root}/icons`);
+  // await fse.remove(`${module.root}/src`);
+  // backend->root
+  // files
+  // const files = await eggBornUtils.tools.globbyAsync(`*`, {
+  //   expandDirectories: false,
+  //   onlyFiles: false,
+  //   absolute: false,
+  //   cwd: `${module.root}/backend`,
+  // });
+  // console.log(files);
+  // for (const file of files) {
+  //   await fse.move(`${module.root}/backend/${file}`, `${module.root}/${file}`);
+  // }
+  // await fse.remove(`${module.root}/backend`);
 }
