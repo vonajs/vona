b15b82ddd31b2df0d9f76cdecbe2d0eec7735fa6
zhennann <zhen.nann@icloud.com>
2024-01-13 11:25:49 +0800

    feat: glob

--- packages-cabloy/core/src/types/interface/module.ts
+++ packages-cabloy/core/src/types/interface/module.ts
@@ -1,35 +1,11 @@
-import { IModuleParseInfo } from '@cabloy/module-parse';
-
-export interface IAppModulePackage {
-  name: string;
-  version: string;
-  eggBornModule: {
-    fileVersion: number;
-    dependencies: Record<string, string>;
-  };
-  description: string;
-  author: string;
-  dependencies: string;
-}
 // todo:
 export interface IAppModuleMain {
   beans: Record<string, any>;
   aops: Record<string, any>;
 }
 
-export interface IAppModule {
-  name: string;
-  info: IModuleParseInfo;
-  root: string;
-  pkg: string;
-  package: IAppModulePackage;
-  main: IAppModuleMain;
-}
-
-export interface IAppSuite {
-  name: string;
-  info: IModuleParseInfo;
-  root: string;
-  pkg: string;
-  package: IAppModulePackage;
+declare module '@cabloy/module-glob' {
+  export interface IAppModule {
+    main: IAppModuleMain;
+  }
 }
--- packages-utils/module-glob/src/interface.ts
+++ packages-utils/module-glob/src/interface.ts
@@ -1,7 +1,37 @@
+import { IModuleParseInfo } from '@cabloy/module-parse';
+
 export interface IModuleGlobOptions {
   projectPath: string;
   disabledModules?: string[];
   disabledSuites?: string[];
   log?: boolean;
   type: 'front' | 'backend';
+}
+
+export interface IAppSuite {
+  name: string;
+  info: IModuleParseInfo;
+  root: string;
+  pkg: string;
+  package: IAppModulePackage;
+}
+
+export interface IAppModule {
+  name: string;
+  info: IModuleParseInfo;
+  root: string;
+  pkg: string;
+  package: IAppModulePackage;
+}
+
+export interface IAppModulePackage {
+  name: string;
+  version: string;
+  eggBornModule: {
+    fileVersion: number;
+    dependencies: Record<string, string>;
+  };
+  description: string;
+  author: string;
+  dependencies: string;
 }
