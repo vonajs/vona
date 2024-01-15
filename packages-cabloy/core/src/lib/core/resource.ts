17b564cbaf5aa3fa2f8b8f6b36943c1a662a5c51
zhennann <zhen.nann@icloud.com>
2024-01-15 18:09:37 +0800

    feat: bean

--- packages-cabloy/core/src/lib/core/resource.ts
+++ packages-cabloy/core/src/lib/core/resource.ts
@@ -2,7 +2,7 @@ import { IDecoratorBeanOptionsBase } from '../decorator/index.js';
 import { BeanBase } from '../module/bean/beanBase.js';
 
 export class AppResource extends BeanBase {
-  beans: Record<string, IDecoratorBeanOptionsBase>;
+  beans: Record<string, IDecoratorBeanOptionsBase> = {};
 }
 
 export const appResource = new AppResource();
---
+++ packages-cabloy/core/src/lib/decorator/class/util.ts
@@ -0,0 +1,3 @@
+import { ParseModuleNameLevelInit } from '@cabloy/module-info';
+
+export const ParseModuleNameLevel = ParseModuleNameLevelInit + 2;
