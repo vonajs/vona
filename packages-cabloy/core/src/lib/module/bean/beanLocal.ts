82adaa7c7e20109d1bbab4633ade69879ec6945f
zhennann <zhen.nann@icloud.com>
2024-01-17 14:42:25 +0800

    feat: bean

--- packages-cabloy/core/src/lib/module/bean/beanLocal.ts
+++ packages-cabloy/core/src/lib/module/bean/beanLocal.ts
@@ -1,7 +1,9 @@
-module.exports = class Local extends module.meta.class.BeanModuleBase {
+import { BeanModuleBase } from './beanModuleBase.js';
+
+export class BeanLocal extends BeanModuleBase {
   // magic
   __get__(prop) {
     const bean = this.ctx ? this.ctx.bean : this.app.bean;
     return bean._getBean(`${this.moduleScope}.local.${prop}`);
   }
-};
+}
--- packages-cabloy/core/src/lib/module/index.ts
+++ packages-cabloy/core/src/lib/module/index.ts
@@ -1,2 +1,4 @@
 export * from './bean/beanBase.js';
+export * from './bean/beanContainer.js';
+export * from './bean/beanLocal.js';
 export * from './bean/beanModuleBase.js';
