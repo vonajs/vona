966002b79004b936cc76873caeeef8670de7c486
zhennann <zhen.nann@icloud.com>
2024-01-17 11:07:04 +0800

    feat: moduleScope

--- packages-cabloy/core/src/lib/decorator/class/use.ts
+++ packages-cabloy/core/src/lib/decorator/class/use.ts
@@ -1,17 +1,26 @@
 import { MetadataKey, appMetadata } from '../../core/metadata.js';
 import { appResource } from '../../core/resource.js';
-import { Constructable } from '../index.js';
+import { BeanModuleBase } from '../../module/index.js';
+import { Constructable, IDecoratorUseOptions } from '../index.js';
+import { parseModuleName } from './util.js';
 
-export function Use(): PropertyDecorator {
+export function Use(options?: IDecoratorUseOptions): PropertyDecorator {
   return function (target: Object, prop: MetadataKey) {
+    if (!options) options = {};
     // beanFullName
-    const proto = appMetadata.getDesignType(target, prop);
-    const beanFullName = appResource.getBeanFullName(proto as any);
-    if (!beanFullName) throw new Error(`beanFullName not found for: ${(<any>proto).name}`);
+    const beanClass = appMetadata.getDesignType(target, prop) as Constructable;
+    const beanFullName = appResource.getBeanFullName(beanClass);
+    if (!beanFullName) throw new Error(`beanFullName not found for: ${beanClass.name}`);
+    // moduleScope
+    let moduleScope = options.moduleScope;
+    if (!moduleScope && beanClass.prototype instanceof BeanModuleBase) {
+      moduleScope = parseModuleName();
+    }
     // record
     appResource.addUse(target, {
       prop,
       beanFullName,
+      moduleScope,
     });
   };
 }
--- packages-cabloy/core/src/lib/decorator/interface/useOptions.ts
+++ packages-cabloy/core/src/lib/decorator/interface/useOptions.ts
@@ -3,4 +3,7 @@ import { MetadataKey } from '../../../index.js';
 export interface IDecoratorUseOptionsBase {
   prop: MetadataKey;
   beanFullName: string;
+  moduleScope?: string;
 }
+
+export type IDecoratorUseOptions = Pick<IDecoratorUseOptionsBase, 'moduleScope'>;
