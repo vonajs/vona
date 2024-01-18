84fac7156d35534b7fbd8b882ae97ab606f5af3e
zhennann <zhen.nann@icloud.com>
2024-01-18 18:05:04 +0800

    Update index.ts

--- packages-cabloy/core/src/lib/module/index.ts
+++ packages-cabloy/core/src/lib/module/index.ts
@@ -3,3 +3,4 @@ export * from './bean/beanBase.js';
 export * from './bean/beanContainer.js';
 export * from './bean/beanLocal.js';
 export * from './bean/beanModuleScopeBase.js';
+export * from './bean/beanScopeBase.js';
