bb040512b3f72cc01a056ce2f0a23f2a8ba291e4
zhennann <zhen.nann@icloud.com>
2024-01-18 21:08:57 +0800

    feat: bean

---
+++ packages-cabloy/core/src/lib/module/bean/index.ts
@@ -0,0 +1,6 @@
+export * from './beanSimple.js';
+export * from './beanBase.js';
+export * from './beanContainer.js';
+export * from './beanLocal.js';
+export * from './beanModuleScopeBase.js';
+export * from './beanScopeBase.js';
--- packages-cabloy/core/src/lib/module/index.ts
+++ packages-cabloy/core/src/lib/module/index.ts
@@ -1,6 +1,1 @@
-export * from './bean/beanSimple.js';
-export * from './bean/beanBase.js';
-export * from './bean/beanContainer.js';
-export * from './bean/beanLocal.js';
-export * from './bean/beanModuleScopeBase.js';
-export * from './bean/beanScopeBase.js';
+export * from './bean/index.js';
