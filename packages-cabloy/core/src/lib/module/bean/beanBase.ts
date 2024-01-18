95bfcef755b1baab66e0839d7f442b2a04f46f2f
zhennann <zhen.nann@icloud.com>
2024-01-18 19:39:34 +0800

    feat: scope

--- packages-cabloy/core/src/lib/module/bean/beanBase.ts
+++ packages-cabloy/core/src/lib/module/bean/beanBase.ts
@@ -1,12 +1,23 @@
 import { appResource } from '../../core/resource.js';
 import { BeanSimple } from '../index.js';
 
+const BeanBaseScope = Symbol('BeanBase#Scope');
+
 export class BeanBase extends BeanSimple {
-  get bean() {
+  protected get bean() {
     return this.ctx.bean;
   }
 
-  get moduleBelong() {
+  protected get scope() {
+    if (!this[BeanBaseScope]) {
+      const moduleScope = this.moduleBelong;
+      const bean = this.ctx ? this.ctx.bean : this.app.bean;
+      this[BeanBaseScope] = bean._getBeanScope(`${moduleScope}.scope.module`, moduleScope);
+    }
+    return this[BeanBaseScope];
+  }
+
+  protected get moduleBelong() {
     return appResource._getModuleBelong(this.constructor as any);
   }
 }
--- packages-cabloy/core/src/lib/module/bean/beanScopeBase.ts
+++ packages-cabloy/core/src/lib/module/bean/beanScopeBase.ts
@@ -17,6 +17,6 @@ export class BeanScopeBase extends BeanSimple {
   // other module's bean
   module(moduleScope: string) {
     const bean = this.ctx ? this.ctx.bean : this.app.bean;
-    return bean._getBeanScope(`${this.moduleScope}.scope.module`, moduleScope);
+    return bean._getBeanScope(`${moduleScope}.scope.module`, moduleScope);
   }
 }
