0bef25755dd30ef8243d5c3b82540463b2d03b79
zhennann <zhen.nann@icloud.com>
2024-01-20 21:49:12 +0800

    feat: error

--- packages-cabloy/core/src/lib/bean/scope/beanScopeBase.ts
+++ packages-cabloy/core/src/lib/bean/scope/beanScopeBase.ts
@@ -1,12 +1,25 @@
 import { BeanScopeScene } from './beanScopeScene.js';
 import { BeanBase } from '../beanBase.js';
+import { BeanScopeError } from './beanScopeError.js';
+
+const BeanModuleError = Symbol('BeanScopeBase#BeanModuleError');
 
 export class BeanScopeBase extends BeanBase {
+  private [BeanModuleError]: BeanScopeError;
   private __scenes: Record<string, any> = {};
 
   protected __get__(prop) {
+    const moduleBelong = this.moduleBelong;
+    // error
+    if (prop === 'error') {
+      if (!this[BeanModuleError]) {
+        this[BeanModuleError] = this.bean._newBean(BeanScopeError, moduleBelong);
+      }
+      return this[BeanModuleError];
+    }
+    // scene
     if (!this.__scenes[prop]) {
-      this.__scenes[prop] = this.bean._newBean(BeanScopeScene, this.moduleBelong, prop);
+      this.__scenes[prop] = this.bean._newBean(BeanScopeScene, moduleBelong, prop);
     }
     return this.__scenes[prop];
   }
--- packages-cabloy/core/src/lib/bean/scope/beanScopeError.ts
+++ packages-cabloy/core/src/lib/bean/scope/beanScopeError.ts
@@ -1,4 +1,5 @@
 import { IErrorObject } from '../../error/errorObject.js';
+import { BeanSimple } from '../beanSimple.js';
 
 export interface IBeanScopeError {
   throw(...args: any[]): never;
@@ -8,3 +9,23 @@ export interface IBeanScopeError {
 export type TypeBeanScopeError<T> = {
   [prop in string & keyof T]: IBeanScopeError;
 };
+
+const BeanModuleScope = Symbol('BeanScopeError#ModuleScope');
+
+export class BeanScopeError extends BeanSimple {
+  private [BeanModuleScope]: string;
+  private __instances: Record<string, any> = {};
+
+  constructor(moduleScope) {
+    super();
+    this[BeanModuleScope] = moduleScope;
+  }
+
+  protected __get__(prop) {
+    if (!this.__instances[prop]) {
+      const beanFullName = `${this[BeanModuleScope]}.${this[BeanModuleScene]}.${prop}`;
+      this.__instances[prop] = (<any>this.bean)._injectBeanInstanceProp(beanFullName);
+    }
+    return this.__instances[prop];
+  }
+}
