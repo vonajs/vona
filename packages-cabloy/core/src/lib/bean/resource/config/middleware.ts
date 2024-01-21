399a7dcdc06339f21319df769ab9a6596d467e99
zhennann <zhen.nann@icloud.com>
2024-01-21 21:19:43 +0800

    feat: middleware

---
+++ packages-cabloy/core/src/lib/bean/resource/config/middleware.ts
@@ -0,0 +1,5 @@
+export interface IModuleConfigMiddleware {
+  bean: string | { module: string; name: string };
+  global?: boolean;
+  dependencies: string | string[];
+}
--- packages-cabloy/core/src/lib/bean/resource/config/type.ts
+++ packages-cabloy/core/src/lib/bean/resource/config/type.ts
@@ -1,5 +1,3 @@
 import { CabloyApplication } from '../../../../index.js';
 
-export interface IModuleConfigMiddleware {}
-
 export type TypeModuleConfig<T extends (app: CabloyApplication) => object> = ReturnType<T>;
