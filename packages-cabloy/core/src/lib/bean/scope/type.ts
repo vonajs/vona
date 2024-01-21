f19e4e27f4284a5776649085f56b633ce99bd8fd
zhennann <zhen.nann@icloud.com>
2024-01-22 07:45:37 +0800

    feat: scope

--- packages-cabloy/core/src/lib/bean/scope/type.ts
+++ packages-cabloy/core/src/lib/bean/scope/type.ts
@@ -1,3 +1,16 @@
-export type TypeModuleResource<LOCAL> = {
+import { CabloyApplication } from '../../../types/application/app.js';
+import { TypeModuleConfig } from '../resource/config/type.js';
+import { TypeModuleErrors } from '../resource/error/type.js';
+import { TypeModuleLocales } from '../resource/locale/type.js';
+
+export type TypeModuleResource<
+  LOCAL,
+  CONFIG extends (app: CabloyApplication) => object,
+  ERRORS,
+  LOCALES extends { 'zh-cn': object },
+> = {
   local: LOCAL;
+  config: TypeModuleConfig<CONFIG>;
+  error: TypeModuleErrors<ERRORS>;
+  locale: TypeModuleLocales<LOCALES>;
 };
--- src/suite/a-base/modules/a-version/src/types/error.ts
+++
@@ -1,4 +0,0 @@
-import { TypeModuleErrors } from '@cabloy/core';
-import { Errors } from '../config/errors.js';
-
-export type TypeErrors = TypeModuleErrors<typeof Errors>;
