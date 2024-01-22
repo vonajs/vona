7a2a5608cfdaddb0965436882aa35cf9acb32139
zhennann <zhen.nann@icloud.com>
2024-01-22 09:50:29 +0800

    feat: constants

--- src/suite/a-base/modules/a-version/src/index.ts
+++ src/suite/a-base/modules/a-version/src/index.ts
@@ -6,4 +6,4 @@ export * from './config/locales.js';
 export * from './config/errors.js';
 export * from './config/config.js';
 export * from './routes.js';
-// const constants = require('./config/constants.js');
+export * from './config/constants.js';
