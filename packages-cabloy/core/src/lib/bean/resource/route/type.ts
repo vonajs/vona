d9eea8a5af6b28bd1de363391a144becc0c335c7
zhennann <zhen.nann@icloud.com>
2024-01-23 14:58:44 +0800

    feat: git ignore

--- packages-cabloy/core/src/lib/bean/resource/route/type.ts
+++ packages-cabloy/core/src/lib/bean/resource/route/type.ts
@@ -1,17 +1,18 @@
+export interface IModuleRouteMeta {
+  right?:
+    | { type: 'atom'; atomClass?: string; action: string; stage?: string; checkFlow?: boolean }
+    | { type: 'atomClass' }
+    | { type: 'resource'; module?: string; name?: string };
+  validate?: { validator?: string; schema?: string };
+  auth?: { user?: boolean; enable?: boolean };
+  captchaVerify?: { scene: { name: string } };
+  authOpen?: { enableAuthOpen?: boolean; onlyAuthOpen?: boolean };
+}
 export interface IModuleRoute {
   method: 'get' | 'post';
   path: string;
   controller: string;
   action?: string;
   middlewares?: string | string[];
-  meta?: {
-    right?:
-      | { type: 'atom'; atomClass?: string; action: string; stage?: string; checkFlow?: boolean }
-      | { type: 'atomClass' }
-      | { type: 'resource'; module?: string; name?: string };
-    validate?: { validator?: string; schema?: string };
-    auth?: { user?: boolean; enable?: boolean };
-    captchaVerify?: { scene: { name: string } };
-    authOpen?: { enableAuthOpen?: boolean; onlyAuthOpen?: boolean };
-  };
+  meta?: IModuleRouteMeta;
 }
