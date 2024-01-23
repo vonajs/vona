07d8c2dae3f34684846149b4283613d6f26d7423
zhennann <zhen.nann@icloud.com>
2024-01-23 21:31:05 +0800

    feat: resource index

--- src/suite/a-base/modules/a-version/src/resource/scope.ts
+++ src/suite/a-base/modules/a-version/src/resource/scope.ts
@@ -1,12 +1,20 @@
 import { BeanScopeBase, Scope, TypeModuleResource } from '@cabloy/core';
 import { IModuleLocal } from './locals.js';
+import { IModuleModel } from './models.js';
 import { config, Errors, locales, constants } from '../config/index.js';
 
 @Scope()
 export class ScopeModuleAVersion extends BeanScopeBase {}
 
 export interface ScopeModuleAVersion
-  extends TypeModuleResource<IModuleLocal, null, typeof config, typeof Errors, typeof locales, typeof constants> {}
+  extends TypeModuleResource<
+    IModuleLocal,
+    IModuleModel,
+    typeof config,
+    typeof Errors,
+    typeof locales,
+    typeof constants
+  > {}
 
 declare module '@cabloy/core' {
   export interface IBeanScopeRecord {
