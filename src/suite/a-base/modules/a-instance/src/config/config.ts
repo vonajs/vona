1a0463b6f3ee1a490683affd16b421cf20a85cd3
zhennann <zhen.nann@icloud.com>
2024-01-24 14:35:53 +0800

    feat: config

--- src/suite/a-base/modules/a-instance/src/config/config.ts
+++ src/suite/a-base/modules/a-instance/src/config/config.ts
@@ -1,27 +1,27 @@
 import { IModuleConfigBroadcast } from '@cabloy/core';
 
+const middlewares = {
+  // instance: {
+  //   bean: 'instance',
+  //   global: true,
+  //   dependencies: 'appReady',
+  // } as IModuleConfigMiddleware,
+  // appReady: {
+  //   bean: 'appReady',
+  //   global: true,
+  // } as IModuleConfigMiddleware,
+};
+
+const broadcasts = {
+  resetCache: {
+    bean: 'resetCache',
+  } as IModuleConfigBroadcast,
+  reload: {
+    bean: 'reload',
+  } as IModuleConfigBroadcast,
+};
+
 export const config = _app => {
-  const middlewares = {
-    // instance: {
-    //   bean: 'instance',
-    //   global: true,
-    //   dependencies: 'appReady',
-    // } as IModuleConfigMiddleware,
-    // appReady: {
-    //   bean: 'appReady',
-    //   global: true,
-    // } as IModuleConfigMiddleware,
-  };
-
-  const broadcasts = {
-    resetCache: {
-      bean: 'resetCache',
-    } as IModuleConfigBroadcast,
-    reload: {
-      bean: 'reload',
-    } as IModuleConfigBroadcast,
-  };
-
   return {
     middlewares,
     broadcasts,
--- src/suite/a-base/modules/a-version/src/config/config.ts
+++ src/suite/a-base/modules/a-version/src/config/config.ts
@@ -1,31 +1,31 @@
 import { IModuleConfigBroadcast, IModuleConfigStartup } from '@cabloy/core';
 
+const startups = {
+  workerAlive: {
+    bean: 'workerAlive',
+  } as IModuleConfigStartup,
+  databaseInit: {
+    bean: 'databaseInit',
+    debounce: true,
+  } as IModuleConfigStartup,
+  databaseName: {
+    bean: 'databaseName',
+  } as IModuleConfigStartup,
+  instanceInit: {
+    bean: 'instanceInit',
+    instance: true,
+    debounce: true,
+  } as IModuleConfigStartup,
+};
+
+const broadcasts = {
+  columnsClear: {
+    bean: 'columnsClear',
+    instance: false,
+  } as IModuleConfigBroadcast,
+};
+
 export const config = _app => {
-  const startups = {
-    workerAlive: {
-      bean: 'workerAlive',
-    } as IModuleConfigStartup,
-    databaseInit: {
-      bean: 'databaseInit',
-      debounce: true,
-    } as IModuleConfigStartup,
-    databaseName: {
-      bean: 'databaseName',
-    } as IModuleConfigStartup,
-    instanceInit: {
-      bean: 'instanceInit',
-      instance: true,
-      debounce: true,
-    } as IModuleConfigStartup,
-  };
-
-  const broadcasts = {
-    columnsClear: {
-      bean: 'columnsClear',
-      instance: false,
-    } as IModuleConfigBroadcast,
-  };
-
   return {
     startups,
     broadcasts,
