9f1a99c5870cc99e9cdcbdabe6e0002fa7700c81
zhennann <zhen.nann@icloud.com>
2024-01-25 10:07:41 +0800

    Update modules.js

--- scripts/modules.js
+++ scripts/modules.js
@@ -69,64 +69,74 @@ async function _suiteHandle({ modules, suite, processHelper }) {
 
 //
 
-async function _moduleHandle_({ module, processHelper }) {
-  const file = `${module.root}/src/config/locales.ts`;
+async function _moduleHandle({ module, processHelper }) {
+  const file = `${module.root}/src/resource/locals.ts`;
   if (!fse.existsSync(file)) {
     console.log('---- not changed: ', module.info.relativeName);
     return;
   }
   const contentOld = (await fse.readFile(file)).toString();
-  const regexp = /'(.*)': require\('\.\/(.*)\.js'\)/g;
+  const matchExport = contentOld.match(/export /);
+  if (matchExport) {
+    console.log('---- not changed: ', module.info.relativeName);
+    return;
+  }
+  const regexp = /const (.*?) =.*service\/(.*?)\.js/g;
   const matches = contentOld.matchAll(regexp);
-  const routesNew1 = [];
-  const routesNew2 = [];
+  const outputNew1 = [];
+  const outputNew2 = [];
+  const outputNew3 = [];
   for (const match of matches) {
     const classNameOld = match[1];
     const classPath = match[2];
-    console.log(classNameOld, classPath);
-    routesNew1.push(`import ${classNameOld.replace('-', '_')} from './${classPath}.js';`);
-    routesNew2.push(`'${classNameOld}': ${classNameOld.replace('-', '_')},`);
-    //
-    const classFile = `${module.root}/src/config/${classPath}.ts`;
+    if (classNameOld.indexOf('.') > -1) {
+      console.log('has . :', module.info.relativeName);
+      return;
+    }
+    const classNameNew = classPathToClassName('Local', classPath);
+    // console.log(classNameOld, classPath, classNameNew);
+    // locals.ts
+    outputNew1.push(`export * from '../local/${classPath}.js';`);
+    outputNew2.push(`import { ${classNameNew} } from '../local/${classPath}.js';`);
+    outputNew3.push(`${classNameOld}: ${classNameNew};`);
+    // local
+    const classFile = `${module.root}/src/local/${classPath}.ts`;
     // console.log(classFile);
     const classContent = (await fse.readFile(classFile)).toString();
     // const classNameNew = classPathToClassName('Controller', classPath);
     // const beanName = parseBeanName(classNameNew, 'Controller');
     // console.log(classNameNew, classNameOld);
     // 替换内容
-    const contentMatches = classContent.match(/([\s\S\n]*)module\.exports = (\{[\s\S\n]*)/);
+    const contentMatches = classContent.match(/([\s\S\n]*)module\.exports = class ([\S]*) (\{[\s\S\n]*)/);
     if (!contentMatches) {
       console.log('---- not matched: ', module.info.relativeName);
       return;
     }
-    // console.log(contentMatches);
+    console.log(contentMatches);
     const contentNew = `
+import { BeanBase, Local } from '@cabloy/core';
+
 ${contentMatches[1]}
-export default ${contentMatches[2]}
+
+@Local()
+export class ${classNameNew} extends BeanBase ${contentMatches[3]}
     `;
-    // console.log(contentNew);
-    await fse.outputFile(classFile, contentNew);
-    await processHelper.formatFile({ fileName: classFile });
-
-    // 别忘了替换routes中的controller name
-    // const routesFile = `${module.root}/src/routes.ts`;
-    // let routesContent = (await fse.readFile(routesFile)).toString();
-    // routesContent = routesContent.replaceAll(`controller: '${classNameOld}'`, `controller: '${beanName}'`);
-    // console.log(routesContent, beanName);
-    // await fse.outputFile(routesFile, routesContent);
-    // await processHelper.formatFile({ fileName: routesFile });
+    console.log(contentNew);
+    // await fse.outputFile(classFile, contentNew);
+    // await processHelper.formatFile({ fileName: classFile });
   }
-  // routes
-  const routesContentNew = `
-${routesNew1.join('\n')}
+  const outputNew = `
+${outputNew1.join('\n')}
 
-export const locales = {
-  ${routesNew2.join('\n')}
-};
+${outputNew2.join('\n')}
+
+export interface IModuleLocal {
+  ${outputNew3.join('\n')}
+}
   `;
-  console.log(routesContentNew);
-  await fse.outputFile(file, routesContentNew);
-  await processHelper.formatFile({ fileName: file });
+  // console.log(outputNew);
+  // await fse.outputFile(file, outputNew);
+  // await processHelper.formatFile({ fileName: file });
 }
 
 function getScopeModuleName(moduleName) {
