476b3d32d33d3c1baea555215e7dd1debfd49170
zhennann <zhen.nann@icloud.com>
2024-01-25 21:05:44 +0800

    feat: version

--- src/suite/a-base/modules/a-socketio/src/local/messageClass.ts
+++
@@ -1,8 +0,0 @@
-import { BeanBase, Local } from '@cabloy/core';
-
-@Local()
-export class LocalMessageClass extends BeanBase {
-  async messageClass({ messageClass }) {
-    return await this.ctx.bean.io.messageClass.get(messageClass);
-  }
-}
