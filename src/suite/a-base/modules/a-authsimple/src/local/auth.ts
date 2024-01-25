7563c3a77c237aaed7d981d6924f796db11bd3a1
zhennann <zhen.nann@icloud.com>
2024-01-25 10:36:00 +0800

    feat: update

--- src/suite/a-base/modules/a-authsimple/src/local/auth.ts
+++ src/suite/a-base/modules/a-authsimple/src/local/auth.ts
@@ -1,42 +1,40 @@
-module.exports =
-  // const moduleInfo = module.info;
-  class Auth {
-    // mobile: not use
-    async signup({ user, state = 'login', userName, realName, email, /* mobile,*/ password }) {
-      return await this.ctx.bean.authSimple.signup({ user, state, userName, realName, email, /* mobile,*/ password });
-    }
+module.exports = class Auth {
+  // mobile: not use
+  async signup({ user, state = 'login', userName, realName, email, /* mobile,*/ password }) {
+    return await this.ctx.bean.authSimple.signup({ user, state, userName, realName, email, /* mobile,*/ password });
+  }
 
-    // data: { auth, password, rememberMe }
-    async signin({ data, state = 'login' }) {
-      return await this.ctx.bean.authSimple.signin({ data, state });
-    }
+  // data: { auth, password, rememberMe }
+  async signin({ data, state = 'login' }) {
+    return await this.ctx.bean.authSimple.signin({ data, state });
+  }
 
-    async add({ userId, password }) {
-      return await this.ctx.bean.authSimple.add({ userId, password });
-    }
+  async add({ userId, password }) {
+    return await this.ctx.bean.authSimple.add({ userId, password });
+  }
 
-    async passwordChange({ passwordOld, passwordNew, userId }) {
-      return await this.ctx.bean.authSimple.passwordChange({ passwordOld, passwordNew, userId });
-    }
+  async passwordChange({ passwordOld, passwordNew, userId }) {
+    return await this.ctx.bean.authSimple.passwordChange({ passwordOld, passwordNew, userId });
+  }
 
-    async passwordReset({ passwordNew, token }) {
-      return await this.ctx.bean.authSimple.passwordReset({ passwordNew, token });
-    }
+  async passwordReset({ passwordNew, token }) {
+    return await this.ctx.bean.authSimple.passwordReset({ passwordNew, token });
+  }
 
-    async passwordForgot({ email }) {
-      return await this.ctx.bean.authSimple.passwordForgot({ email });
-    }
+  async passwordForgot({ email }) {
+    return await this.ctx.bean.authSimple.passwordForgot({ email });
+  }
 
-    async emailConfirm({ email, user }) {
-      return await this.ctx.bean.authSimple.emailConfirm({ email, user });
-    }
+  async emailConfirm({ email, user }) {
+    return await this.ctx.bean.authSimple.emailConfirm({ email, user });
+  }
 
-    // invoke by user clicking the link
-    async emailConfirmation({ token }) {
-      return await this.ctx.bean.authSimple.emailConfirmation({ token });
-    }
+  // invoke by user clicking the link
+  async emailConfirmation({ token }) {
+    return await this.ctx.bean.authSimple.emailConfirmation({ token });
+  }
 
-    async checkStatus({ user }) {
-      return await this.ctx.bean.authSimple.checkStatus({ user });
-    }
-  };
+  async checkStatus({ user }) {
+    return await this.ctx.bean.authSimple.checkStatus({ user });
+  }
+};
