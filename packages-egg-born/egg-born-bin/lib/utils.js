556b75447b21e0264c88cf72c76985fd4a9265e4
zhennann <zhen.nann@icloud.com>
2024-01-29 21:04:34 +0800

    feat: gateway

--- packages-egg-born/egg-born-bin/lib/utils.js
+++ packages-egg-born/egg-born-bin/lib/utils.js
@@ -4,7 +4,7 @@ const urllib = require('urllib');
 const semver = require('semver');
 const chalk = require('chalk');
 const boxen = require('boxen');
-const extend = require('@cabloy/extend');
+const extend = require('@cabloy/extend').default;
 const eggBornUtils = require('egg-born-utils');
 
 const boxenOptions = { padding: 1, margin: 1, align: 'center', borderColor: 'yellow', borderStyle: 'round' };
