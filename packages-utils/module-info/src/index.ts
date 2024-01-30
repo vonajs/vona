416fb880045c81bef109a58af2691d4409e0cc94
zhennann <zhen.nann@icloud.com>
2024-01-30 16:19:42 +0800

    feat: update

--- packages-utils/module-info/src/index.ts
+++ packages-utils/module-info/src/index.ts
@@ -1,5 +1,5 @@
-import fs from 'fs';
-import path from 'path';
+import * as fs from 'fs';
+import * as path from 'path';
 import StackUtils from 'stack-utils';
 import { IModuleInfo } from './interface.js';
 export * from './interface.js';
