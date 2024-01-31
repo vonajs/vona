eaa3c8d80d0b0229cfdd114ef598e93b62572443
zhennann <zhen.nann@icloud.com>
2024-01-31 15:29:16 +0800

    feat: update

--- packages-egg-born/egg-born-bin/mock.js
+++ packages-egg-born/egg-born-bin/mock.js
@@ -1,12 +1,19 @@
 const path = require('path');
 const utils = require('./lib/utils.js');
 
+// env
+process.env.EGG_BASE_DIR = path.join(process.cwd(), 'src/backend');
+process.env.EGG_FRAMEWORK = utils.getModulePath('egg-born-backend');
+process.env.NODE_ENV = 'unittest';
+
+// spec
 let spec = utils.combineTestPattern({
   baseDir: path.join(process.cwd(), 'src/backend'),
   env: 'unittest',
   pattern: null,
 });
 spec = ['src/suite/test-party/modules/test-party/test/controller/test.test.ts'];
+
 module.exports = {
   extension: ['ts'],
   spec,
