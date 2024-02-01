42cb1f1569dbdd4ba5f690cea0e851a7ed98727f
zhennann <zhen.nann@icloud.com>
2024-02-01 23:17:18 +0800

    feat: model app

--- src/suite/a-base/modules/a-app/src/model/app.ts
+++ src/suite/a-base/modules/a-app/src/model/app.ts
@@ -1,4 +1,4 @@
 import { BeanModelBase, Model } from '@cabloy/core';
 
-@Model({ table: 'aApp', options: { disableDeleted: false } })
+@Model({ name: '_app', table: 'aApp', options: { disableDeleted: false } })
 export class ModelApp extends BeanModelBase {}
