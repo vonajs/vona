967c081dc4efa90a62f6ad71abe6b812848740da
zhennann <zhen.nann@icloud.com>
2024-02-01 23:07:57 +0800

    feat: bean.local

--- packages-cabloy/core/src/lib/bean/beanLocal.ts
+++ packages-cabloy/core/src/lib/bean/beanLocal.ts
@@ -5,4 +5,9 @@ export class BeanLocal extends BeanModuleScopeBase {
   protected __get__(prop) {
     return this.bean._getBean(`${this.moduleScope}.local.${prop}`);
   }
+
+  // beanLocal has no __beanFullName__
+  module(moduleScope: string): typeof this {
+    return this.bean._newBeanScope(BeanLocal, moduleScope) as this;
+  }
 }
