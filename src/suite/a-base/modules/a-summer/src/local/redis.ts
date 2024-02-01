30c1ba71cb0f8201b0cb347d1629a78cb4ca01eb
zhennann <zhen.nann@icloud.com>
2024-02-01 22:19:54 +0800

    Update redis.ts

--- src/suite/a-base/modules/a-summer/src/local/redis.ts
+++ src/suite/a-base/modules/a-summer/src/local/redis.ts
@@ -1,3 +1,4 @@
+import { IModuleConfigSummerCacheBase } from '../config/types.js';
 import { CacheBase } from './cacheBase.js';
 import { Local } from '@cabloy/core';
 
@@ -5,7 +6,7 @@ import { Local } from '@cabloy/core';
 export class LocalRedis extends CacheBase {
   _redisSummer: any;
 
-  constructor({ cacheBase }: any) {
+  constructor({ cacheBase }: { cacheBase: IModuleConfigSummerCacheBase }) {
     super({ cacheBase });
     this._redisSummer = null;
   }
