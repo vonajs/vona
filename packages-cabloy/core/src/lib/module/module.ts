6b3937b03fe46421574e09cdbb021ec1783f09be
zhennann <zhen.nann@icloud.com>
2024-02-04 18:48:46 +0800

    feat: development

--- packages-cabloy/core/src/lib/module/module.ts
+++ packages-cabloy/core/src/lib/module/module.ts
@@ -34,7 +34,8 @@ export class ModuleTools extends BeanSimple {
     // 1. import
     const promises: Promise<IModuleResource>[] = [];
     for (const module of app.meta.modulesArray) {
-      promises.push(import(`${module.root}/dist/index.js`));
+      const subPath = app.meta.isTest || app.meta.isLocal ? 'src' : 'dist';
+      promises.push(import(`${module.root}/${subPath}/index.js`));
     }
     const modulesResource = await Promise.all(promises);
     for (let i = 0; i < modulesResource.length; i++) {
