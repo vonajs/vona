976a8ba1fd397e8d80d66a53951bb89a0415a313
zhennann <zhen.nann@icloud.com>
2024-02-16 17:51:12 +0800

    feat: test-flow

--- src/suite/a-flow/modules/a-flowgateway/src/routes.ts
+++ src/suite/a-flow/modules/a-flowgateway/src/routes.ts
@@ -1,6 +1,3 @@
 import { IModuleRoute } from '@cabloy/core';
 
-export const routes: IModuleRoute[] = [
-  //
-  { method: 'post', path: 'flow/start', controller: 'flow', middlewares: 'test' },
-];
+export const routes: IModuleRoute[] = [];
