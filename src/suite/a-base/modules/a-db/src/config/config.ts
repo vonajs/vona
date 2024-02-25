5a89086aa799eef0783dc79f6b2fd384b7e2da91
zhennann <zhen.nann@icloud.com>
2024-02-25 22:18:09 +0800

    Update config.ts

--- src/suite/a-base/modules/a-db/src/config/config.ts
+++ src/suite/a-base/modules/a-db/src/config/config.ts
@@ -4,7 +4,20 @@ export const config = _app => {
   return {
     base: {
       pool: { min: 0, max: 10 },
+      afterCreate,
     } as knex.Knex.Config,
     clients: {} as Record<string, knex.Knex.Config>,
   };
 };
+
+async function afterCreate(conn) {
+  await sessionVariablesSet(conn);
+}
+
+async function sessionVariablesSet(conn) {
+  await sessionVariableSet(conn, 'SET SESSION explicit_defaults_for_timestamp=ON');
+}
+
+async function sessionVariableSet(conn, sql) {
+  await conn.query(sql);
+}
