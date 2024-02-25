81d8721f01478b98e96f1b0db31f6fc79ff6f71d
zhennann <zhen.nann@icloud.com>
2024-02-25 22:08:22 +0800

    Update config.ts

--- src/suite/a-base/modules/a-db/src/config/config.ts
+++ src/suite/a-base/modules/a-db/src/config/config.ts
@@ -2,7 +2,9 @@ import knex from 'knex';
 
 export const config = _app => {
   return {
-    base: {} as knex.Knex.Config,
+    base: {
+      pool: { min: 0, max: 10 },
+    } as knex.Knex.Config,
     clients: {} as Record<string, knex.Knex.Config>,
   };
 };
