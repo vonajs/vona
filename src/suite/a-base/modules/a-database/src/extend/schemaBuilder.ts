d122c8ae2fae813285b0c92e9abbf49b96c4a86e
zhennann <zhen.nann@icloud.com>
2024-02-29 19:48:07 +0800

    Create schemaBuilder.ts

---
+++ src/suite/a-base/modules/a-database/src/extend/schemaBuilder.ts
@@ -0,0 +1,23 @@
+import knex, { Knex } from 'knex';
+import { VirtualDatabaseDialect } from '../bean/virtual.databaseDialect.js';
+import { CabloyApplication, Cast } from '@cabloy/core';
+import { __ThisModule__ } from '../resource/this.js';
+
+export function ExtendSchemaBuilder(app: CabloyApplication) {
+  function getDialect(schemaBuilder: Knex.SchemaBuilder): VirtualDatabaseDialect {
+    const client = Cast<Knex.Client>(Cast(schemaBuilder).client).config.client;
+    const beanFullName = `${__ThisModule__}.database.dialect.${client}`;
+    const dialect = app.bean._newBean(beanFullName, schemaBuilder) as VirtualDatabaseDialect;
+    if (!dialect) {
+      throw new Error(`database dialect not found: ${client}`);
+    }
+    return dialect;
+  }
+
+  ['fetchDatabases'].forEach(function (method) {
+    knex.SchemaBuilder.extend(method, async function (...args) {
+      const dialect = getDialect(this);
+      return await dialect[method](...args);
+    });
+  });
+}
