d1a6909d461c64b10f00e3e9830308fb03656c17
zhennann <zhen.nann@icloud.com>
2024-02-29 11:14:36 +0800

    Update beans.ts

--- src/suite/a-base/modules/a-database/src/resource/beans.ts
+++ src/suite/a-base/modules/a-database/src/resource/beans.ts
@@ -4,13 +4,16 @@ export * from '../bean/database.dialect.mysql.js';
 export * from '../bean/database.dialect.mysql2.js';
 export * from '../bean/database.dialect.pg.js';
 export * from '../bean/virtual.databaseDialect.js';
+export * from '../bean/virtual.model.js';
 
 import { BeanDatabase } from '../bean/bean.database.js';
 import { BeanDatabaseClient } from '../bean/bean.databaseClient.js';
+import { BeanModel } from '../bean/virtual.model.js';
 
 declare module '@cabloy/core' {
   export interface IBeanRecord {
     database: BeanDatabase;
     databaseClient: BeanDatabaseClient;
+    model: BeanModel;
   }
 }
