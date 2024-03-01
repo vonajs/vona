3b7776f94926c4da6561d6b20b5091ff3089e469
zhennann <zhen.nann@icloud.com>
2024-03-01 22:10:36 +0800

    Update update2.ts

--- src/suite/a-base/modules/a-base/src/bean/version.manager/update/update2.ts
+++ src/suite/a-base/modules/a-base/src/bean/version.manager/update/update2.ts
@@ -3,14 +3,14 @@ import { BeanBase } from '@cabloy/core';
 export class VersionUpdate extends BeanBase {
   async run() {
     // add userId 0
-    await this.ctx.model.insert('aUser', {
+    await this.bean.model.builder('aUser').insert({
       id: 0,
       iid: 0,
       userName: 'system',
       realName: 'system',
     });
     // add roleId 0
-    await this.ctx.model.insert('aRole', {
+    await this.bean.model.builder('aRole').insert({
       id: 0,
       iid: 0,
       roleName: 'system',
