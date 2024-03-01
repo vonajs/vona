a4bc4caea25b1f221e58ec7c7c8a2c097933a93d
zhennann <zhen.nann@icloud.com>
2024-03-01 16:13:38 +0800

    Update version.manager.ts

--- src/suite/a-base/modules/a-cache/src/bean/version.manager.ts
+++ src/suite/a-base/modules/a-cache/src/bean/version.manager.ts
@@ -5,21 +5,13 @@ export class VersionManager extends BeanBase {
   async update(options) {
     if (options.version === 1) {
       // create table: aCache
-      const sql = `
-          CREATE TABLE aCache (
-            id int(11) NOT NULL AUTO_INCREMENT,
-            createdAt timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-            updatedAt timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-            deleted int(11) DEFAULT '0',
-            iid int(11) DEFAULT '0',
-            module varchar(255) DEFAULT NULL,
-            name varchar(255) DEFAULT NULL,
-            value json DEFAULT NULL,
-            timeout int(11) DEFAULT '0',
-            PRIMARY KEY (id)
-          )
-        `;
-      await this.ctx.model.query(sql);
+      await this.bean.model.schema.createTable('aCache', function (table) {
+        table.basicFields();
+        table.string('module', 255);
+        table.string('name', 255);
+        table.json('value');
+        table.integer('timeout').defaultTo(0);
+      });
     }
 
     if (options.version === 2) {
