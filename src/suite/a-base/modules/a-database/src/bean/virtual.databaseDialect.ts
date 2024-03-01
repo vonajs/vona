865177d3f84f0713b9da0d77667cc0a8e5848e11
zhennann <zhen.nann@icloud.com>
2024-03-01 09:45:20 +0800

    Update virtual.databaseDialect.ts

--- src/suite/a-base/modules/a-database/src/bean/virtual.databaseDialect.ts
+++ src/suite/a-base/modules/a-database/src/bean/virtual.databaseDialect.ts
@@ -36,7 +36,39 @@ export class VirtualDatabaseDialect<T = unknown> extends BeanBase {
     throw new Error('Not Implemented');
   }
 
-  coerceColumn(_column: Knex.ColumnInfo): ITableColumn {
-    throw new Error('Not Implemented');
+  coerceColumn(column: Knex.ColumnInfo): ITableColumn {
+    // result
+    const result = { type: column.type } as ITableColumn;
+    // coerce
+    result.default = this._coerceColumnValue(column.type, column.defaultValue);
+    // ok
+    return result;
+  }
+
+  protected _coerceColumnValue(type: string, value) {
+    // null
+    if (value === null) return value;
+    // type
+    if (['bit', 'bool', 'boolean'].includes(type)) return Boolean(value);
+    if (['int'].includes(type)) return this._safeNumber(value);
+    if (this._columnTypePrefixes(type, ['timestamp']) && value === 'CURRENT_TIMESTAMP') return new Date();
+    if (this._columnTypePrefixes(type, ['float', 'double'])) return this._safeNumber(value);
+    if (this._columnTypePrefixes(type, ['tinyint', 'smallint', 'mediumint', 'bigint', 'numeric', 'integer'])) {
+      return this._safeNumber(value);
+    }
+    // pg: NULL::character varying
+    if (value.indexOf('NULL::') === 0) return null;
+    // others
+    return value;
+  }
+
+  // pg: nextval
+  protected _safeNumber(value) {
+    const num = Number(value);
+    return isNaN(num) ? null : num;
+  }
+
+  protected _columnTypePrefixes(type: string, prefixes: string[]) {
+    return prefixes.some(prefix => type.indexOf(prefix) > -1);
   }
 }
