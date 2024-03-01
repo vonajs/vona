6874398893b3b5c99c28fbdaecde39da14e61af0
zhennann <zhen.nann@icloud.com>
2024-03-01 16:10:46 +0800

    Update index.ts

--- src/suite/a-base/modules/a-database/src/extend/index.ts
+++ src/suite/a-base/modules/a-database/src/extend/index.ts
@@ -1,8 +1,10 @@
 import { CabloyApplication } from '@cabloy/core';
 import { ExtendSchemaBuilder } from './schemaBuilder.js';
+import { ExtendTableBuilder } from './tableBuilder.js';
 
 export function ExtendKnex(app: CabloyApplication) {
   if (app.meta.inApp) {
     ExtendSchemaBuilder(app);
+    ExtendTableBuilder(app);
   }
 }
