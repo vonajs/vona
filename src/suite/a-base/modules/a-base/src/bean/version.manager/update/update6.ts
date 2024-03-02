f1131bb9caa8ccfd6ccfe59fddc6bc6765572170
zhennann <zhen.nann@icloud.com>
2024-03-02 20:50:03 +0800

    Update update6.ts

--- src/suite/a-base/modules/a-base/src/bean/version.manager/update/update6.ts
+++ src/suite/a-base/modules/a-base/src/bean/version.manager/update/update6.ts
@@ -3,12 +3,10 @@ import { BeanBase } from '@cabloy/core';
 export class VersionUpdate extends BeanBase {
   async run() {
     // aUser
-    const sql = `
-        ALTER TABLE aUser
-          ADD COLUMN activated int(11) DEFAULT '0',
-          ADD COLUMN emailConfirmed int(11) DEFAULT '0',
-          ADD COLUMN mobileVerified int(11) DEFAULT '0'
-                  `;
-    await this.ctx.model.query(sql);
+    await this.bean.model.schema.alterTable('aUser', function (table) {
+      table.int0('activated');
+      table.int0('emailConfirmed');
+      table.int0('mobileVerified');
+    });
   }
 }
