7212d42ac97afa2d5032221846a544a3601afec6
zhennann <zhen.nann@icloud.com>
2024-03-02 20:23:58 +0800

    Update update3.ts

--- src/suite/a-base/modules/a-base/src/bean/version.manager/update/update3.ts
+++ src/suite/a-base/modules/a-base/src/bean/version.manager/update/update3.ts
@@ -3,22 +3,33 @@ import { BeanBase } from '@cabloy/core';
 export class VersionUpdate extends BeanBase {
   async run() {
     // aViewRoleRightAtomClassUser
-    let sql = `
-        create view aViewRoleRightAtomClassUser as
-          select a.iid,a.roleId as roleIdWho,b.atomClassId,b.action,c.userId as userIdWhom from aRoleExpand a
-            inner join aRoleRightRef b on a.roleIdBase=b.roleId
-            inner join aViewUserRoleRef c on b.roleIdScope=c.roleIdParent
-          `;
-    await this.ctx.model.query(sql);
+    await this.bean.model.schema.createView('aViewRoleRightAtomClassUser', view => {
+      view.columns(['iid', 'roleIdWho', 'atomClassId', 'action', 'userIdWhom']);
+      view.as(
+        this.bean.model
+          .builder('aRoleExpand as a')
+          .select(['a.iid', 'a.roleId as roleIdWho', 'b.atomClassId', 'b.action', 'c.userId as userIdWhom'])
+          .innerJoin('aRoleRightRef as b', { 'a.roleIdBase': 'b.roleId' })
+          .innerJoin('aViewUserRoleRef as c', { 'b.roleIdScope': 'c.roleIdParent' }),
+      );
+    });
 
     // aViewRoleRightAtom
-    sql = `
-        create view aViewRoleRightAtom as
-          select a.iid, a.id as atomId,a.userIdCreated as userIdWhom,b.roleIdWho,b.action from aAtom a,aViewRoleRightAtomClassUser b
-            where a.deleted=0 and a.atomEnabled=1
-              and a.atomClassId=b.atomClassId
-              and a.userIdCreated=b.userIdWhom
-          `;
-    await this.ctx.model.query(sql);
+    await this.bean.model.schema.createView('aViewRoleRightAtom', view => {
+      view.columns(['iid', 'atomId', 'userIdWhom', 'roleIdWho', 'action']);
+      view.as(
+        this.bean.model
+          .builder('aAtom as a')
+          .select(['a.iid', 'a.id as atomId', 'a.userIdCreated as userIdWhom', 'b.roleIdWho', 'b.action'])
+          .innerJoin('aViewRoleRightAtomClassUser as b', {
+            'a.atomClassId': 'b.atomClassId',
+            'a.userIdCreated': 'b.userIdWhom',
+          })
+          .where({
+            'a.deleted': 0,
+            'a.atomEnabled': 1,
+          }),
+      );
+    });
   }
 }
