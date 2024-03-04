2b478ceecc72e82b38e79d87a3ba22910f944460
zhennann <zhen.nann@icloud.com>
2024-03-04 18:00:41 +0800

    Update bean.model_inner.ts

--- src/suite/a-base/modules/a-database/src/bean/bean.model/bean.model_inner.ts
+++ src/suite/a-base/modules/a-database/src/bean/bean.model/bean.model_inner.ts
@@ -1,4 +1,33 @@
-import { BeanBase } from '@cabloy/core';
+import { BeanBase, IDecoratorModelOptions, IModelOptions, appResource } from '@cabloy/core';
 
 // @ts-ignore ignore TRecord
-export class BeanModelInner<TRecord extends {}, TResult> extends BeanBase {}
+export class BeanModelInner<TRecord extends {}, TResult> extends BeanBase {
+  protected get __beanOptions() {
+    return appResource.getBean((<any>this).__beanFullName__);
+  }
+
+  protected get __modelOptions() {
+    const beanOptions = this.__beanOptions;
+    return beanOptions?.options as IDecoratorModelOptions;
+  }
+
+  get table(): string {
+    return this.__modelOptions?.table;
+  }
+
+  get options(): IModelOptions {
+    return this.__modelOptions?.options;
+  }
+
+  get disableDeleted() {
+    return this.options?.disableDeleted === undefined
+      ? this.app.config.model.disableDeleted
+      : this.options?.disableDeleted;
+  }
+
+  get disableInstance() {
+    return this.options?.disableInstance === undefined
+      ? this.app.config.model.disableInstance
+      : this.options?.disableInstance;
+  }
+}
