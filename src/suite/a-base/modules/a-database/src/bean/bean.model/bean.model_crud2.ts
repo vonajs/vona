44fd443d058c262687028c16679c0c30145b7faa
zhennann <zhen.nann@icloud.com>
2024-03-06 23:28:21 +0800

    Update bean.model_crud2.ts

--- src/suite/a-base/modules/a-database/src/bean/bean.model/bean.model_crud2.ts
+++ src/suite/a-base/modules/a-database/src/bean/bean.model/bean.model_crud2.ts
@@ -5,20 +5,9 @@ export class BeanModelCrud2<TRecord extends {}> extends BeanModelCrud<TRecord> {
   async create<TRecord2 extends {} = TRecord>(
     data?: Partial<TRecord2>,
     options?: IModelMethodOptionsGeneral,
-  ): Promise<number>;
-  async create<TRecord2 extends {} = TRecord>(
-    table: string,
-    data?: Partial<TRecord2>,
-    options?: IModelMethodOptionsGeneral,
-  ): Promise<number>;
-  async create(table?, data?, options?): Promise<number> {
-    if (typeof table !== 'string') {
-      table = undefined;
-      options = data;
-      data = table;
-    }
+  ): Promise<number> {
     // table
-    table = table || this.table;
+    const table = this.table;
     if (!table) throw new Error('should specify the table name');
     // data
     const data2 = await this.prepareData(data);
@@ -30,20 +19,9 @@ export class BeanModelCrud2<TRecord extends {}> extends BeanModelCrud<TRecord> {
   async write<TRecord2 extends {} = TRecord>(
     data?: Partial<TRecord2>,
     options?: IModelUpdateOptionsGeneral,
-  ): Promise<void>;
-  async write<TRecord2 extends {} = TRecord>(
-    table: string,
-    data?: Partial<TRecord2>,
-    options?: IModelUpdateOptionsGeneral,
-  ): Promise<void>;
-  async write(table?, data?, options?): Promise<void> {
-    if (typeof table !== 'string') {
-      table = undefined;
-      options = data;
-      data = table;
-    }
+  ): Promise<void> {
     // table
-    table = table || this.table;
+    const table = this.table;
     if (!table) throw new Error('should specify the table name');
     // data
     const data2 = await this.prepareData(data);
