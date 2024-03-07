c30af35ef526c9f6a4593dfc24a63fdd47e756b1
zhennann <zhen.nann@icloud.com>
2024-03-07 17:37:43 +0800

    Update update10.ts

--- src/suite/a-base/modules/a-base/src/bean/version.manager/update/update10.ts
+++ src/suite/a-base/modules/a-base/src/bean/version.manager/update/update10.ts
@@ -3,10 +3,8 @@ import { BeanBase } from '@cabloy/core';
 export class VersionUpdate extends BeanBase {
   async run() {
     // aAtom: atomIdArchive -> atomIdFormal
-    const sql = `
-        ALTER TABLE aAtom
-          CHANGE COLUMN atomIdArchive atomIdFormal int(11) DEFAULT '0'
-                  `;
-    await this.ctx.model.query(sql);
+    await this.bean.model.schema.alterTable('aAtom', function (table) {
+      table.renameColumn('atomIdArchive', 'atomIdFormal');
+    });
   }
 }
