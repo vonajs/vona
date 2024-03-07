82fe60139e1dbea7c0d5ef04e09db378ed5a1fdd
zhennann <zhen.nann@icloud.com>
2024-03-07 09:59:27 +0800

    feat: create

--- src/suite/a-base/modules/a-database/src/bean/bean.model/bean.model_crud2.ts
+++ src/suite/a-base/modules/a-database/src/bean/bean.model/bean.model_crud2.ts
@@ -2,15 +2,23 @@ import { IModelMethodOptions, IModelUpdateOptions } from '../../types.js';
 import { BeanModelCrud } from './bean.model_crud.js';
 
 export class BeanModelCrud2<TRecord extends {}> extends BeanModelCrud<TRecord> {
+  async create<TRecord2 extends {} = TRecord>(data?: Partial<TRecord2>, options?: IModelMethodOptions): Promise<number>;
   async create<TRecord2 extends {} = TRecord>(
+    table: string,
     data?: Partial<TRecord2>,
     options?: IModelMethodOptions,
-  ): Promise<number> {
+  ): Promise<number>;
+  async create<TRecord2 extends {} = TRecord>(table?, data?, options?): Promise<number> {
+    if (typeof table !== 'string') {
+      table = undefined;
+      options = data;
+      data = table;
+    }
     // table
-    const table = this.table;
+    table = table || this.table;
     if (!table) throw new Error('should specify the table name');
     // data
-    const data2 = await this.prepareData(data);
+    const data2 = await this.prepareData<TRecord2>(table, data);
     // insert
     const res = await this.insert(table, data2, options);
     return res[0];
