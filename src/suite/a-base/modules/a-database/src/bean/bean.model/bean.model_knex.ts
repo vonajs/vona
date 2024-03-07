0bc9d1c043379d2fddeb9ba9e7cd61c8ec0359b6
zhennann <zhen.nann@icloud.com>
2024-03-07 22:15:01 +0800

    Update bean.model_knex.ts

--- src/suite/a-base/modules/a-database/src/bean/bean.model/bean.model_knex.ts
+++ src/suite/a-base/modules/a-database/src/bean/bean.model/bean.model_knex.ts
@@ -1,6 +1,7 @@
 import { Knex } from 'knex';
 import { BeanModelUtils } from './bean.model_utils.js';
 import { IModelMethodOptionsGeneral } from '../../types.js';
+import { Cast } from '@cabloy/core';
 
 export class BeanModelKnex<TRecord extends {}> extends BeanModelUtils<TRecord> {
   get schema(): Knex.SchemaBuilder {
@@ -42,5 +43,18 @@ export class BeanModelKnex<TRecord extends {}> extends BeanModelUtils<TRecord> {
     this.prepareWhere(builder, table, null, options);
     // ready
     return builder;
+  }
+
+  async createView(viewName: string, callback: (viewBuilder: Knex.ViewBuilder) => any): Promise<void> {
+    let _view: Knex.ViewBuilder | null = null;
+    // create view
+    await this.schema.createView(viewName, view => {
+      callback(view);
+      _view = view;
+    });
+    const sql = Cast(_view).toSQL();
+    // record view
+    const modelViewRecord = this.getScope('a-version').model.viewRecord;
+    await modelViewRecord.insert({ viewName, viewSql: sql[0].sql });
   }
 }
