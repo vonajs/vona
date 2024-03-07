9796fc12d8507af7fccd0590bdb7f9ce2efd1476
zhennann <zhen.nann@icloud.com>
2024-03-07 22:25:07 +0800

    feat: createView

--- src/suite/a-base/modules/a-database/src/bean/bean.model/bean.model_knex.ts
+++ src/suite/a-base/modules/a-database/src/bean/bean.model/bean.model_knex.ts
@@ -49,16 +49,23 @@ export class BeanModelKnex<TRecord extends {}> extends BeanModelUtils<TRecord> {
     return builder;
   }
 
-  async createView(viewName: string, callback: (viewBuilder: Knex.ViewBuilder) => any): Promise<void> {
-    let _view: Knex.ViewBuilder | null = null;
-    // create view
-    await this.schema.createView(viewName, view => {
-      callback(view);
-      _view = view;
-    });
-    const sql = Cast(_view).toSQL();
-    // record view
-    await this.modelViewRecord.insert({ viewName, viewSql: sql[0].sql });
+  async createView(viewName: string, callback?: (viewBuilder: Knex.ViewBuilder) => any): Promise<void> {
+    if (callback) {
+      let _view: Knex.ViewBuilder | null = null;
+      // create view
+      await this.schema.createView(viewName, view => {
+        callback(view);
+        _view = view;
+      });
+      const sql = Cast(_view).toSQL();
+      // record view
+      await this.modelViewRecord.insert({ viewName, viewSql: sql[0].sql });
+    } else {
+      // get record
+      const viewRecord = await this.modelViewRecord.get({ viewName });
+      if (!viewRecord) throw new Error(`not found view record: ${viewName}`);
+      await this.raw(viewRecord.viewSql);
+    }
   }
 
   async dropView(viewName: string, removeRecord?: boolean) {
