04c29275fc4a5548bde55a55ddc6f3779eff55b7
zhennann <zhen.nann@icloud.com>
2024-03-08 21:48:22 +0800

    Update update23.ts

--- src/suite/a-base/modules/a-base/src/bean/version.manager/update/update23.ts
+++ src/suite/a-base/modules/a-base/src/bean/version.manager/update/update23.ts
@@ -3,10 +3,8 @@ import { BeanBase } from '@cabloy/core';
 export class VersionUpdate extends BeanBase {
   async run() {
     // aAtomClass: drop atomClassIdParent
-    const sql = `
-        ALTER TABLE aAtomClass
-          DROP COLUMN atomClassIdParent
-                  `;
-    await this.ctx.model.query(sql);
+    await this.bean.model.alterTable('aAtomClass', function (table) {
+      table.dropColumn('atomClassIdParent');
+    });
   }
 }
