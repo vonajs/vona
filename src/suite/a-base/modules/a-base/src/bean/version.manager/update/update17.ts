e4d4242d380e7a90c60dd3c95a9e5141323dd8ea
zhennann <zhen.nann@icloud.com>
2024-03-08 20:52:56 +0800

    Update update17.ts

--- src/suite/a-base/modules/a-base/src/bean/version.manager/update/update17.ts
+++ src/suite/a-base/modules/a-base/src/bean/version.manager/update/update17.ts
@@ -3,9 +3,6 @@ import { BeanBase } from '@cabloy/core';
 export class VersionUpdate extends BeanBase {
   async run() {
     // aStatus
-    const sql = `
-        delete from aStatus where name like 'user-layoutConfig:%'
-      `;
-    await this.ctx.model.query(sql);
+    await this.bean.model.builder('aStatus').whereILike('name', 'user-layoutConfig:%').delete();
   }
 }
