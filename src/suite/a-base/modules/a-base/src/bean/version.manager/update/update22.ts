c37fc5d119a537edacfa5b65d160b4442ef670e1
zhennann <zhen.nann@icloud.com>
2024-03-08 21:47:11 +0800

    Update update22.ts

--- src/suite/a-base/modules/a-base/src/bean/version.manager/update/update22.ts
+++ src/suite/a-base/modules/a-base/src/bean/version.manager/update/update22.ts
@@ -2,9 +2,6 @@ import { BeanBase } from '@cabloy/core';
 
 export class VersionUpdate extends BeanBase {
   async run() {
-    const sql = `
-        update aAtom set atomFlowId=0 where atomStage in (1,2)
-      `;
-    await this.ctx.model.query(sql);
+    await this.ctx.bean.model.builder('aAtom').update({ atomFlowId: 0 }).whereIn('atomStage', [1, 2]);
   }
 }
