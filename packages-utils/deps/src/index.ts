9ec91bc14496401daa5627b3849f86b3108ebd7e
zhennann <zhen.nann@icloud.com>
2024-03-08 15:31:13 +0800

    feat: deps

--- packages-utils/deps/src/index.ts
+++ packages-utils/deps/src/index.ts
@@ -3,20 +3,27 @@ export interface ISwapDepsItem {
   dependencies: string | string[];
 }
 
-export function swapDeps(items: ISwapDepsItem[]) {
+export interface ISwapDepsOptions {
+  name?: string;
+  dependencies?: string;
+}
+
+export function swapDeps(items: ISwapDepsItem[], options?: ISwapDepsOptions) {
   // eslint-disable-next-line
   while (true) {
-    if (!_swapDeps(items)) break;
+    if (!_swapDeps(items, options)) break;
   }
 }
 
-function _swapDeps(items: ISwapDepsItem[]) {
+function _swapDeps(items: ISwapDepsItem[], options?: ISwapDepsOptions) {
+  const keyDependencies = options?.dependencies || 'dependencies';
+  const keyName = options?.name || 'name';
   let result = false;
   for (const item of items) {
-    let deps = item.dependencies || [];
+    let deps = _getProperty(item, keyDependencies) || [];
     if (typeof deps === 'string') deps = deps.split(',');
     for (const dep of deps) {
-      if (_swapDep(items, dep, item.name)) result = true;
+      if (_swapDep(items, dep, _getProperty(item, keyName))) result = true;
     }
   }
   return result;
@@ -28,4 +35,18 @@ function _swapDep(arr: ISwapDepsItem[], a: string, b: string) {
   if (indexA === -1 || indexB === -1 || indexA < indexB) return false;
   arr.splice(indexB, 0, arr.splice(indexA, 1)[0]);
   return true;
+}
+
+function _getProperty(obj, name) {
+  if (!obj) return undefined;
+  const names = name.split('.');
+  // loop
+  for (const name of names) {
+    if (obj[name] === undefined || obj[name] === null) {
+      obj = obj[name];
+      break;
+    }
+    obj = obj[name];
+  }
+  return obj;
 }
