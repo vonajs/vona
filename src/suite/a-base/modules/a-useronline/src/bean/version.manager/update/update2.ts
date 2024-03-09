d77b128bc442fdab474f6e0d04bf3ca1ca579ccb
zhennann <zhen.nann@icloud.com>
2024-03-09 20:23:42 +0800

    Update update2.ts

--- src/suite/a-base/modules/a-useronline/src/bean/version.manager/update/update2.ts
+++ src/suite/a-base/modules/a-useronline/src/bean/version.manager/update/update2.ts
@@ -3,10 +3,8 @@ import { BeanBase } from '@cabloy/core';
 export class VersionUpdate extends BeanBase {
   async run(_options) {
     // aAtom: drop atomId
-    const sql = `
-      ALTER TABLE aUserOnlineHistory
-        DROP COLUMN atomId
-    `;
-    await this.ctx.model.query(sql);
+    await this.bean.model.alterTable('aUserOnlineHistory', function (table) {
+      table.dropColumn('atomId');
+    });
   }
 }
