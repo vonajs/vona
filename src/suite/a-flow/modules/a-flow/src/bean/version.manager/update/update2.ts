afeccc4ac75deb35e80850f6806bb123b61fd108
zhennann <zhen.nann@icloud.com>
2024-03-09 11:56:02 +0800

    Update update2.ts

--- src/suite/a-flow/modules/a-flow/src/bean/version.manager/update/update2.ts
+++ src/suite/a-flow/modules/a-flow/src/bean/version.manager/update/update2.ts
@@ -2,30 +2,24 @@ import { BeanBase } from '@cabloy/core';
 
 export class VersionUpdate extends BeanBase {
   async run(_options) {
-    let sql;
     // alter table: aFlow
-    sql = `
-      ALTER TABLE aFlow
-        ADD COLUMN flowHandleStatus int(11) DEFAULT '0'
-                `;
-    await this.ctx.model.query(sql);
+    await this.bean.model.alterTable('aFlow', function (table) {
+      table.int0('flowHandleStatus');
+    });
+
     // alter table: aFlowHistory
-    sql = `
-      ALTER TABLE aFlowHistory
-        ADD COLUMN flowHandleStatus int(11) DEFAULT '0'
-                `;
-    await this.ctx.model.query(sql);
+    await this.bean.model.alterTable('aFlowHistory', function (table) {
+      table.int0('flowHandleStatus');
+    });
+
     // alter table: aFlowNode
-    sql = `
-      ALTER TABLE aFlowNode
-        ADD COLUMN flowNodeHandleStatus int(11) DEFAULT '0'
-                `;
-    await this.ctx.model.query(sql);
+    await this.bean.model.alterTable('aFlowNode', function (table) {
+      table.int0('flowNodeHandleStatus');
+    });
+
     // alter table: aFlowNodeHistory
-    sql = `
-      ALTER TABLE aFlowNodeHistory
-        ADD COLUMN flowNodeHandleStatus int(11) DEFAULT '0'
-                `;
-    await this.ctx.model.query(sql);
+    await this.bean.model.alterTable('aFlowNodeHistory', function (table) {
+      table.int0('flowNodeHandleStatus');
+    });
   }
 }
