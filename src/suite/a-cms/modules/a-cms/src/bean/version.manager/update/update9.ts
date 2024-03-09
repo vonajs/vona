0a17e38506a2a8136889e9a3f675f39f4bf6829f
zhennann <zhen.nann@icloud.com>
2024-03-09 22:10:16 +0800

    Update update9.ts

--- src/suite/a-cms/modules/a-cms/src/bean/version.manager/update/update9.ts
+++ src/suite/a-cms/modules/a-cms/src/bean/version.manager/update/update9.ts
@@ -2,13 +2,13 @@ import { BeanBase } from '@cabloy/core';
 
 export class VersionUpdate extends BeanBase {
   async run() {
-    // drop column: aCmsContent.itemId
-    await this.bean.model.alterTable('aCmsContent', function (table) {
-      table.dropColumn('itemId');
-    });
     // drop view: aCmsArticleViewFull
     await this.bean.model.dropView('aCmsArticleViewFull');
     // drop view: aCmsArticleViewSearch
     await this.bean.model.dropView('aCmsArticleViewSearch');
+    // drop column: aCmsContent.itemId
+    await this.bean.model.alterTable('aCmsContent', function (table) {
+      table.dropColumn('itemId');
+    });
   }
 }
