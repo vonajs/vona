c6a1e74f7d9a244739447bdcaf60dafb36d7c738
zhennann <zhen.nann@icloud.com>
2024-03-09 19:56:54 +0800

    Update version.manager.ts

--- src/suite/a-base/modules/a-mail/src/bean/version.manager.ts
+++ src/suite/a-base/modules/a-mail/src/bean/version.manager.ts
@@ -5,22 +5,13 @@ export class VersionManager extends BeanBase {
   async update(options) {
     if (options.version === 1) {
       // create table: aMail
-      const sql = `
-          CREATE TABLE aMail (
-            id int(11) NOT NULL AUTO_INCREMENT,
-            createdAt timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-            updatedAt timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-            deleted int(11) DEFAULT '0',
-            iid int(11) DEFAULT '0',
-            scene varchar(50) DEFAULT NULL,
-            status int(11) DEFAULT '0',
-            mailTo text DEFAULT NULL,
-            mailSubject text DEFAULT NULL,
-            message LONGTEXT DEFAULT NULL,
-            PRIMARY KEY (id)
-          )
-        `;
-      await this.ctx.model.query(sql);
+      await this.bean.model.createTable('aMail', function (table) {
+        table.string('scene', 50);
+        table.int0('status');
+        table.text('mailTo');
+        table.text('mailSubject');
+        table.text('message', 'longtext');
+      });
     }
   }
 
