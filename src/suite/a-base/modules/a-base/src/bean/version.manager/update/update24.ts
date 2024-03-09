ceb96f81b938e5c8691fa9d5d5166b5084602867
zhennann <zhen.nann@icloud.com>
2024-03-09 10:13:33 +0800

    Update update24.ts

--- src/suite/a-base/modules/a-base/src/bean/version.manager/update/update24.ts
+++ src/suite/a-base/modules/a-base/src/bean/version.manager/update/update24.ts
@@ -195,17 +195,19 @@ export class VersionUpdate extends BeanBase {
     });
 
     // aViewRoleRightAtom
-    await this.ctx.model.query('drop view aViewRoleRightAtom');
-    sql = `
-          create view aViewRoleRightAtom as
-            select a.iid, a.id as atomId,a.userIdCreated as userIdWhom,
-                   b.roleIdWho,b.action
-              from aAtom a,aViewRoleRightAtomClassUser b
-                where a.deleted=0 and a.atomStage>0
-                  and a.atomClassId=b.atomClassId
-                  and a.userIdCreated=b.userIdWhom
-      `;
-    await this.ctx.model.query(sql);
+    await this.bean.model.alterView('aViewRoleRightAtom', view => {
+      view.as(
+        this.bean.model
+          .builder('aAtom as a')
+          .select(['a.iid', 'a.id as atomId', 'a.userIdCreated as userIdWhom', 'b.roleIdWho', 'b.action'])
+          .innerJoin('aViewRoleRightAtomClassUser as b', {
+            'a.atomClassId': 'b.atomClassId',
+            'a.userIdCreated': 'b.userIdWhom',
+          })
+          .where('a.deleted', 0)
+          .where('a.atomStage', '>', 0),
+      );
+    });
 
     // aViewUserRightAtomRole
     await this.ctx.model.query('drop view aViewUserRightAtomRole');
