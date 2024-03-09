25603362fa26d3b12defdb38ab282e726537ef8b
zhennann <zhen.nann@icloud.com>
2024-03-09 10:41:05 +0800

    Update update30.ts

--- src/suite/a-base/modules/a-base/src/bean/version.manager/update/update30.ts
+++ src/suite/a-base/modules/a-base/src/bean/version.manager/update/update30.ts
@@ -3,9 +3,6 @@ import { BeanBase } from '@cabloy/core';
 export class VersionUpdate extends BeanBase {
   async run(_options) {
     // aAtomActin: code:16 name: workflowFormal -> viewWorkflow
-    const sql = `
-        update aAtomAction set name='viewWorkflow' where code=16
-      `;
-    await this.ctx.model.query(sql);
+    await this.bean.model.builder('aAtomAction').update({ name: 'viewWorkflow' }).where({ code: 16 });
   }
 }
