9cb1bdfaf34cebe0c814850c2cf03bfecfb1f06b
zhennann <zhen.nann@icloud.com>
2024-03-09 12:27:53 +0800

    Update version.manager.ts

--- src/suite/a-base/modules/a-authsimple/src/bean/version.manager.ts
+++ src/suite/a-base/modules/a-authsimple/src/bean/version.manager.ts
@@ -4,20 +4,12 @@ import { Bean, BeanBase } from '@cabloy/core';
 export class VersionManager extends BeanBase {
   async update(options) {
     if (options.version === 1) {
-      // create table: aStatus
-      const sql = `
-          CREATE TABLE aAuthSimple (
-            id int(11) NOT NULL AUTO_INCREMENT,
-            createdAt timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-            updatedAt timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-            deleted int(11) DEFAULT '0',
-            iid int(11) DEFAULT '0',
-            userId int(11) DEFAULT '0',
-            hash text DEFAULT NULL,
-            PRIMARY KEY (id)
-          )
-        `;
-      await this.ctx.model.query(sql);
+      // create table: aAuthSimple
+      await this.bean.model.createTable('aAuthSimple', function (table) {
+        table.basicFields();
+        table.userId();
+        table.text('hash');
+      });
     }
   }
 
