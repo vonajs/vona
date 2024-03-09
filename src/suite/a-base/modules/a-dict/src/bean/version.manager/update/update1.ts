a54a885cf6fd31ce804f612135edf753c8298c2f
zhennann <zhen.nann@icloud.com>
2024-03-09 18:12:57 +0800

    Update update1.ts

--- src/suite/a-base/modules/a-dict/src/bean/version.manager/update/update1.ts
+++ src/suite/a-base/modules/a-dict/src/bean/version.manager/update/update1.ts
@@ -3,41 +3,29 @@ import { BeanBase } from '@cabloy/core';
 export class VersionUpdate extends BeanBase {
   async run(_options) {
     // create table: aDict
-    let sql = `
-      CREATE TABLE aDict (
-        id int(11) NOT NULL AUTO_INCREMENT,
-        createdAt timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-        updatedAt timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-        deleted int(11) DEFAULT '0',
-        iid int(11) DEFAULT '0',
-        atomId int(11) DEFAULT '0',
-        description varchar(255) DEFAULT NULL,
-        PRIMARY KEY (id)
-      )
-    `;
-    await this.ctx.model.query(sql);
+    await this.bean.model.createTable('aDict', function (table) {
+      table.basicFields();
+      table.atomId();
+      table.description();
+    });
+
     // create table: aDictContent
-    sql = `
-      CREATE TABLE aDictContent (
-        id int(11) NOT NULL AUTO_INCREMENT,
-        createdAt timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-        updatedAt timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-        deleted int(11) DEFAULT '0',
-        iid int(11) DEFAULT '0',
-        atomId int(11) DEFAULT '0',
-        itemId int(11) DEFAULT '0',
-        dictItems JSON DEFAULT NULL,
-        dictLocales JSON DEFAULT NULL,
-        PRIMARY KEY (id)
-      )
-    `;
-    await this.ctx.model.query(sql);
+    await this.bean.model.createTable('aDictContent', function (table) {
+      table.basicFields();
+      table.atomId();
+      table.itemId();
+      table.json('dictItems');
+      table.json('dictLocales');
+    });
+
     // create view: aDictViewFull
-    sql = `
-      CREATE VIEW aDictViewFull as
-        select a.*,b.dictItems,b.dictLocales from aDict a
-          left join aDictContent b on a.id=b.itemId
-    `;
-    await this.ctx.model.query(sql);
+    await this.bean.model.createView('aDictViewFull', view => {
+      view.as(
+        this.bean.model
+          .builder('aDict as a')
+          .select(['a.*', 'b.dictItems', 'b.dictLocales'])
+          .leftJoin('aDictContent as b', { 'a.id': 'b.itemId' }),
+      );
+    });
   }
 }
