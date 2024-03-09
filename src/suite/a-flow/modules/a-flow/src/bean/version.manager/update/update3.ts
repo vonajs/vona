e9c6688216742c325825aa954e9c091f1b3e6093
zhennann <zhen.nann@icloud.com>
2024-03-09 11:58:10 +0800

    Update update3.ts

--- src/suite/a-flow/modules/a-flow/src/bean/version.manager/update/update3.ts
+++ src/suite/a-flow/modules/a-flow/src/bean/version.manager/update/update3.ts
@@ -2,18 +2,14 @@ import { BeanBase } from '@cabloy/core';
 
 export class VersionUpdate extends BeanBase {
   async run(_options) {
-    let sql;
     // alter table: aFlowNode
-    sql = `
-      ALTER TABLE aFlowNode
-        ADD COLUMN behaviorDefId varchar(255) DEFAULT '' 
-                `;
-    await this.ctx.model.query(sql);
+    await this.bean.model.alterTable('aFlowNode', function (table) {
+      table.string('behaviorDefId', 255).defaultTo('');
+    });
+
     // alter table: aFlowNodeHistory
-    sql = `
-      ALTER TABLE aFlowNodeHistory
-        ADD COLUMN behaviorDefId varchar(255) DEFAULT ''
-                `;
-    await this.ctx.model.query(sql);
+    await this.bean.model.alterTable('aFlowNodeHistory', function (table) {
+      table.string('behaviorDefId', 255).defaultTo('');
+    });
   }
 }
