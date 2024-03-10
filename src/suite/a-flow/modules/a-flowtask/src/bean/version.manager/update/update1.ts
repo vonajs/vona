c6e5290535a350201178492180f0e4ffdcf7238c
zhennann <zhen.nann@icloud.com>
2024-03-10 09:01:30 +0800

    refactor: db & model

--- src/suite/a-flow/modules/a-flowtask/src/bean/version.manager/update/update1.ts
+++ src/suite/a-flow/modules/a-flowtask/src/bean/version.manager/update/update1.ts
@@ -2,7 +2,6 @@ import { BeanBase } from '@cabloy/core';
 
 export class VersionUpdate extends BeanBase {
   async run(_options) {
-    let sql;
     // create table: aFlowNodeStartEventAtomCondition
     await this.bean.model.createTable('aFlowNodeStartEventAtomCondition', function (table) {
       table.basicFields();
@@ -29,28 +28,20 @@ export class VersionUpdate extends BeanBase {
     });
 
     // create table: aFlowTaskHistory
-    sql = `
-        CREATE TABLE aFlowTaskHistory (
-          id int(11) NOT NULL AUTO_INCREMENT,
-          createdAt timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-          updatedAt timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-          deleted int(11) DEFAULT '0',
-          iid int(11) DEFAULT '0',
-          flowId int(11) DEFAULT '0',
-          flowTaskId int(11) DEFAULT '0',
-          flowNodeId int(11) DEFAULT '0',
-          flowTaskStatus int(11) DEFAULT '0',
-          flowTaskHidden int(11) DEFAULT '0',
-          userIdAssignee int(11) DEFAULT '0',
-          specificFlag int(11) DEFAULT '0',
-          handleStatus int(11) DEFAULT '0',
-          handleRemark TEXT DEFAULT NULL,
-          timeClaimed timestamp DEFAULT NULL,
-          timeHandled timestamp DEFAULT NULL,
-          taskVars JSON DEFAULT NULL,
-          PRIMARY KEY (id)
-        )
-      `;
-    await this.ctx.model.query(sql);
+    await this.bean.model.createTable('aFlowTaskHistory', function (table) {
+      table.basicFields();
+      table.int0('flowId');
+      table.int0('flowTaskId');
+      table.int0('flowNodeId');
+      table.int0('flowTaskStatus');
+      table.int0('flowTaskHidden');
+      table.int0('userIdAssignee');
+      table.int0('specificFlag');
+      table.int0('handleStatus');
+      table.text('handleRemark');
+      table.timestamp('timeClaimed');
+      table.timestamp('timeHandled');
+      table.json('taskVars');
+    });
   }
 }
