0bcff544d7c406a1027dd906ef18c436786e91be
zhennann <zhen.nann@icloud.com>
2024-03-11 16:38:05 +0800

    feat: big number

--- packages-cabloy/core/src/index.ts
+++ packages-cabloy/core/src/index.ts
@@ -8,7 +8,6 @@ import Master from 'egg-cluster/lib/master.js';
 moduleAlias.addAlias('koa-static-cache', '@cabloy/koa-static-cache');
 Master.prototype.onReload = Reload;
 
-export * from 'bignumber.js';
 export * from './types/index.js';
 export * from './lib/bean/index.js';
 export * from './lib/core/index.js';
