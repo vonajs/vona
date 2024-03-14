8236694d17526efafb4c2690974cd65b8440d5d7
zhennann <zhen.nann@icloud.com>
2024-03-14 09:31:06 +0800

    Update bean.atomClass.ts

--- src/suite/a-base/modules/a-base/src/bean/bean.atomClass.ts
+++ src/suite/a-base/modules/a-base/src/bean/bean.atomClass.ts
@@ -32,8 +32,7 @@ export class BeanAtomClass extends BeanModuleScopeBase<ScopeModule> {
   async __getRaw(params: AtomClassParams): Promise<EntityAtomClass> {
     if ('id' in params) {
       const res = await this.model.get(params);
-      if (!res) throw new Error(`atomClass not found: ${params.id}`);
-      return res;
+      if (res) return res;
     }
     const module = params.module || this.moduleScope;
     const atomClassName = params.atomClassName;
