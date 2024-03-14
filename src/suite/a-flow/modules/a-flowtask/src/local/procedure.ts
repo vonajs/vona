cfde931746baa800be06a5ca3493c970b6023d26
zhennann <zhen.nann@icloud.com>
2024-03-14 21:13:53 +0800

    Update procedure.ts

--- src/suite/a-flow/modules/a-flowtask/src/local/procedure.ts
+++ src/suite/a-flow/modules/a-flowtask/src/local/procedure.ts
@@ -2,19 +2,19 @@ import { Local, BeanBase } from '@cabloy/core';
 
 @Local()
 export class LocalProcedure extends BeanBase {
-  selectTasks({ iid, userIdWho, where, orders, page, count, history }: any) {
+  async selectTasks({ iid, userIdWho, where, orders, page, count, history }: any) {
     iid = parseInt(iid);
     userIdWho = parseInt(userIdWho);
     history = parseInt(history);
 
     // history
     if (history === 0) {
-      return this._selectTasks_0({ iid, userIdWho, where, orders, page, count });
+      return await this._selectTasks_0({ iid, userIdWho, where, orders, page, count });
     }
-    return this._selectTasks_1({ iid, userIdWho, where, orders, page, count });
+    return await this._selectTasks_1({ iid, userIdWho, where, orders, page, count });
   }
 
-  _selectTasks_0({ iid, userIdWho, where, orders, page, count }: any) {
+  async _selectTasks_0({ userIdWho, where, orders, page, count }: any) {
     // -- tables
     // -- a: aFlowTask
     // -- b: aFlowNode
@@ -23,60 +23,69 @@ export class LocalProcedure extends BeanBase {
     // -- e: aUser(for flowUserId)
 
     // for safe
-    where = where ? this.bean.model._where(where) : null;
-    orders = orders ? this.bean.model._orders(orders) : null;
-    const limit = page ? this.bean.model._limit(page.size, page.index) : null;
-
-    // vars
-    let _userWhere;
-
-    //
-    const _where = where ? `${where} AND` : ' WHERE';
-    const _orders = orders || '';
-    const _limit = limit || '';
+    const _where: any = Object.assign({}, where);
+    const _orders = orders ? orders.concat() : [];
 
     // user
     if (userIdWho !== 0) {
-      _userWhere = ` and a.userIdAssignee=${userIdWho}`;
-    } else {
-      _userWhere = '';
+      _where['a.userIdAssignee'] = userIdWho;
     }
 
-    // fields
-    let _selectFields;
+    // builder
+    const builder = this.bean.model.builderSelect('aFlowTask as a');
+    // count/select:fields
     if (count) {
-      _selectFields = 'count(*) as _count';
+      builder.count();
     } else {
-      _selectFields = `a.*,a.id as flowTaskId,
-            b.flowNodeDefId,b.flowNodeName,b.flowNodeType,0 as flowNodeStatus,
-            c.flowDefId,c.flowDefKey,c.flowDefRevision,c.flowName,c.flowStatus,c.flowAtomId,c.flowAtomClassId,c.flowNodeIdCurrent,c.flowUserId,
-            d.userName,d.avatar,
-            e.userName as flowUserName,e.avatar as flowUserAvatar
-          `;
+      const _selectFields = [
+        'a.*',
+        'a.id as flowTaskId',
+        'b.flowNodeDefId',
+        'b.flowNodeName',
+        'b.flowNodeType',
+        '0 as flowNodeStatus',
+        'c.flowDefId',
+        'c.flowDefKey',
+        'c.flowDefRevision',
+        'c.flowName',
+        'c.flowStatus',
+        'c.flowAtomId',
+        'c.flowAtomClassId',
+        'c.flowNodeIdCurrent',
+        'c.flowUserId',
+        'd.userName',
+        'd.avatar',
+        'e.userName as flowUserName',
+        'e.avatar as flowUserAvatar',
+      ];
+      builder.select(_selectFields);
     }
-
-    // sql
-    const _sql = `select ${_selectFields} from aFlowTask a
-            inner join aFlowNode b on a.flowNodeId=b.id
-            inner join aFlow c on a.flowId=c.id
-            left join aUser d on a.userIdAssignee=d.id
-            left join aUser e on c.flowUserId=e.id
-
-          ${_where}
-           (
-             a.deleted=0 and a.iid=${iid}
-             ${_userWhere}
-           )
-
-          ${count ? '' : _orders}
-          ${count ? '' : _limit}
-        `;
-
-    // ok
-    return _sql;
+    // joins
+    builder
+      .innerJoin('aFlowNode as b', { 'a.flowNodeId': 'b.id' })
+      .innerJoin('aFlow as c', { 'a.flowId': 'c.id' })
+      .leftJoin('aUser as d', { 'a.userIdAssignee': 'd.id' })
+      .leftJoin('aUser as e', { 'c.flowUserId': 'e.id' });
+    // where
+    const wheres = this.bean.model.checkWhere(_where);
+    if (wheres === false) return [];
+    if (wheres !== true) {
+      this.bean.model.buildWhere(builder, wheres);
+    }
+    // orders/page
+    if (!count) {
+      this.bean.model.buildOrders(builder, _orders);
+      this.bean.model.buildPage(builder, page);
+    }
+    // execute
+    const debug = this.app.bean.debug.get('flow:sql');
+    if (debug.enabled) {
+      debug('===== selectTasks =====\n%s', builder.toQuery());
+    }
+    return await builder;
   }
 
-  _selectTasks_1({ iid, userIdWho, where, orders, page, count }: any) {
+  async _selectTasks_1({ iid, userIdWho, where, orders, page, count }: any) {
     // -- tables
     // -- a: aFlowTaskHistory
     // -- b: aFlowNodeHistory
