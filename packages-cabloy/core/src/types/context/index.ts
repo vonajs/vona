c81ac3e1e4650487910c3b2a03a65e76b759d544
zhennann <zhen.nann@icloud.com>
2024-03-17 10:25:46 +0800

    feat: config

--- packages-cabloy/core/src/types/context/index.ts
+++ packages-cabloy/core/src/types/context/index.ts
@@ -5,10 +5,12 @@ import { ContextOthers } from './contextOthers.js';
 import { ContextError } from '../../lib/bean/resource/error/errorContext.js';
 import { CabloyApplication } from '../application/app.js';
 import { BeanContainerLike } from '../../lib/bean/beanContainer.js';
+import { ContextConfig } from './contextConfig.js';
 
 // @ts-ignore ignore the throw type check of 'throw'/'meta'
 export interface CabloyContext extends ContextLocale, ContextError, ContextBase, ContextOthers, EggContext {
   app: CabloyApplication;
   bean: BeanContainerLike;
+  config: ContextConfig;
   [key: string | symbol]: any; // should use any rather than unknown
 }
