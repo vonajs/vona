d6345f63ff77bad2492dab2f8fe5862b2986826b
zhennann <zhen.nann@icloud.com>
2024-03-23 14:31:38 +0800

    feat: compose

--- packages-cabloy/core/src/lib/utils/util.ts
+++ packages-cabloy/core/src/lib/utils/util.ts
@@ -10,6 +10,7 @@ import { CabloyContext, Cast, IModule, PowerPartial } from '../../types/index.js
 import { BeanSimple } from '../bean/beanSimple.js';
 import { IModuleMiddlewareGate } from '../bean/index.js';
 import { appResource } from '../core/resource.js';
+import { compose, composeAsync } from '@cabloy/compose';
 
 const __EnvTests = ['unittest', 'test'];
 
@@ -151,64 +152,11 @@ export class AppUtil extends BeanSimple {
   }
 
   compose(chains, adapter) {
-    if (!chains) chains = [];
-    return function (context, next?) {
-      // last called middleware #
-      let index = -1;
-      return dispatch(0);
-      function dispatch(i) {
-        if (i <= index) return new Error('next() called multiple times');
-        index = i;
-        let receiver;
-        let fn;
-        const chain = chains[i];
-        if (chain) {
-          const obj = adapter(context, chain);
-          if (!obj) return dispatch(i + 1);
-          receiver = obj.receiver;
-          fn = obj.fn;
-          if (!fn) return new Error('fn is not defined');
-        }
-        if (i === chains.length) fn = next;
-        if (!fn) return;
-        return fn.call(receiver, context, function next() {
-          return dispatch(i + 1);
-        });
-      }
-    };
+    return compose(chains, adapter);
   }
 
   composeAsync(chains, adapter) {
-    if (!chains) chains = [];
-    return function (context, next?) {
-      // last called middleware #
-      let index = -1;
-      return dispatch(0);
-      function dispatch(i) {
-        if (i <= index) return Promise.reject(new Error('next() called multiple times'));
-        index = i;
-        let receiver;
-        let fn;
-        const chain = chains[i];
-        if (chain) {
-          const obj = adapter(context, chain);
-          if (!obj) return dispatch(i + 1);
-          receiver = obj.receiver;
-          fn = obj.fn;
-          if (!fn) return Promise.reject(new Error('fn is not defined'));
-        }
-        if (i === chains.length) fn = next;
-        if (!fn) return Promise.resolve();
-        try {
-          const res = fn.call(receiver, context, function next() {
-            return dispatch(i + 1);
-          });
-          return Promise.resolve(res);
-        } catch (err) {
-          return Promise.reject(err);
-        }
-      }
-    };
+    return composeAsync(chains, adapter);
   }
 
   async createAnonymousContext({ locale, subdomain, module, instance }): Promise<CabloyContext> {
