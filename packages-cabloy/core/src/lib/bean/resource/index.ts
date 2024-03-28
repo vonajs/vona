7d42b468eaa634ca994b6676d9dd4429d38d12d5
zhennann <zhen.nann@icloud.com>
2024-03-28 11:51:12 +0800

    feat: update

---
+++ packages-cabloy/core/src/lib/bean/resource/index.ts
@@ -0,0 +1,7 @@
+export * from './config/index.js';
+export * from './constant/index.js';
+export * from './entity/index.js';
+export * from './error/index.js';
+export * from './locale/index.js';
+export * from './model/index.js';
+export * from './route/index.js';
