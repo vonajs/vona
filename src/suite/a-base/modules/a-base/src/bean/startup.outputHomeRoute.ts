5d8d91a7ff402ef886af1a28291f5cd3b914dd8b
zhennann <zhen.nann@icloud.com>
2024-04-03 20:01:10 +0800

    Update startup.outputHomeRoute.ts

--- src/suite/a-base/modules/a-base/src/bean/startup.outputHomeRoute.ts
+++ src/suite/a-base/modules/a-base/src/bean/startup.outputHomeRoute.ts
@@ -4,7 +4,11 @@ import chalk from 'chalk';
 @Bean({ scene: 'startup' })
 export class StartupOutputHomeRoute extends BeanBase {
   async execute() {
-    const url = this.ctx.bean.base.getAbsoluteUrl(`/api/a/home/hello`);
+    const moduleHome = this.app.meta.modules['a-home'];
+    if (!moduleHome) return;
+    const route = moduleHome.resource.routes.find(item => item.method === 'get');
+    if (!route) return;
+    const url = this.ctx.bean.base.getAbsoluteUrl(`/api/a/home/${route.path}`);
     setTimeout(() => {
       console.log(chalk.yellow('\n=== a-home route ==='));
       console.log(chalk.cyan('> ' + url));
