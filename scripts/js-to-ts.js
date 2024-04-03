33752322ca37f1fdf2d64f15049254297f6ff5c1
zhennann <zhen.nann@icloud.com>
2024-04-03 17:44:19 +0800

    feat: js to ts

--- scripts/js-to-ts.js
+++ scripts/js-to-ts.js
@@ -4,6 +4,9 @@ const fs = require('node:fs/promises');
 const eggBornUtils = require('egg-born-utils');
 const argv = require('./lib/parse_argv')('sync');
 
+const extFrom = '.js';
+const extTo = '.ts';
+
 (async function () {
   await main();
 })();
@@ -12,15 +15,15 @@ async function main() {
   // paths
   const paths = argv.args;
   const patterns = paths.map(item => {
-    return `${item}/**/*.js`;
+    return `${item}/**/*${extFrom}`;
   });
   // files
   const files = await eggBornUtils.tools.globbyAsync(patterns);
   // convert
   const filesTo = [];
   for (const file of files) {
-    const pos = String(file).lastIndexOf('.js');
-    const fileTo = String(file).substring(0, pos) + '.ts';
+    const pos = String(file).lastIndexOf(extFrom);
+    const fileTo = String(file).substring(0, pos) + extTo;
     await fs.rename(file, fileTo);
     filesTo.push(fileTo);
   }
