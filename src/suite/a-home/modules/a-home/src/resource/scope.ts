69f2f02239cb7c8397504c1a867a64d0556e5a80
zhennann <zhen.nann@icloud.com>
2024-04-03 15:43:11 +0800

    feat: a-home

---
+++ src/suite/a-home/modules/a-home/src/resource/scope.ts
@@ -0,0 +1,27 @@
+import { BeanScopeBase, Scope, TypeModuleResource } from '@cabloy/core';
+import { IModuleLocal } from './locals.js';
+import { IModuleModel } from './models.js';
+import { config, Errors, locales, constants } from '../config/index.js';
+
+@Scope()
+export class ScopeModuleAHome extends BeanScopeBase {}
+
+export interface ScopeModuleAHome
+  extends TypeModuleResource<
+    IModuleLocal,
+    IModuleModel,
+    typeof config,
+    typeof Errors,
+    typeof locales,
+    typeof constants
+  > {}
+
+declare module '@cabloy/core' {
+  export interface IBeanScopeRecord {
+    'a-home': ScopeModuleAHome;
+  }
+
+  export interface IBeanScopeConfig {
+    'a-home': ReturnType<typeof config>;
+  }
+}
