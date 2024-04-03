bdc92269bf01e64370b1b2988dd0c0e28b855fb6
zhennann <zhen.nann@icloud.com>
2024-04-03 19:47:00 +0800

    feat: update

--- src/suite/a-base/modules/a-base/src/bean/startup.outputHomeRoute.ts
+++ src/suite/a-base/modules/a-base/src/bean/startup.outputHomeRoute.ts
@@ -1,8 +1,10 @@
 import { Bean, BeanBase } from '@cabloy/core';
+import chalk from 'chalk';
 
 @Bean({ scene: 'startup' })
 export class StartupOutputHomeRoute extends BeanBase {
   async execute() {
-    await this.app.meta._loadSchedules({ ctx: this.ctx });
+    const url = this.ctx.bean.base.getAbsoluteUrl(`/api/a/home/hello`);
+    console.log(chalk.cyan(`  ${url}`));
   }
 }
