179f182ee9b0c72765a52eee466168db902129ea
zhennann <zhen.nann@icloud.com>
2024-04-05 10:57:23 +0800

    Update index.ts

--- src/suite/a-base/modules/a-base/src/types/user/index.ts
+++ src/suite/a-base/modules/a-base/src/types/user/index.ts
@@ -4,6 +4,7 @@ export interface StateUser {
   op: { id: number } & Partial<EntityUser>;
   agent: { id: number } & Partial<EntityUser>;
   provider: {
+    id: number;
     module: string;
     providerName: string;
     providerScene: string;
