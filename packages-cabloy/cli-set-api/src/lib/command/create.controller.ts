556ddf23c6b2f2f14b6c538537c08a916cf627c8
zhennann <zhen.nann@icloud.com>
2024-04-09 17:45:22 +0800

    Update create.controller.ts

--- packages-cabloy/cli-set-api/src/lib/command/create.controller.ts
+++ packages-cabloy/cli-set-api/src/lib/command/create.controller.ts
@@ -3,7 +3,7 @@ export default {
   info: {
     version: '5.0.0',
     title: 'Cli: Create Controller',
-    usage: 'npm run cabloy :create:controller controllerName -- [--module=]',
+    usage: 'npm run cabloy api:create:controller controllerName -- [--module=]',
   },
   options: {
     module: {
