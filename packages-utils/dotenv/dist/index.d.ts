34d42218fc49d96f09d6d7bafefdce28c3bb9c51
zhennann <zhen.nann@icloud.com>
2024-04-24 23:27:44 +0800

    feat: dotenv

--- packages-utils/dotenv/dist/index.d.ts
+++ packages-utils/dotenv/dist/index.d.ts
@@ -1,4 +1,5 @@
-export declare function loadEnvs(meta: object, dir: string, prefix?: string, postfix?: string): object | undefined;
+import { DotenvParseOutput } from 'dotenv';
+export declare function loadEnvs(meta: object, dir: string, prefix?: string, postfix?: string): DotenvParseOutput | undefined;
 export declare function metaToScope(meta: object): {};
 export declare function getEnvFiles(meta: object, dir: string, prefix: string, postfix?: string): string[] | undefined;
 //# sourceMappingURL=index.d.ts.map
