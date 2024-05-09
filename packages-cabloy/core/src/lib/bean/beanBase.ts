1e21452afbea404f25058a368f97c443eb1c49f0
zhennann <zhen.nann@icloud.com>
2024-05-09 12:00:31 +0800

    feat: locales

--- packages-cabloy/core/src/lib/bean/beanBase.ts
+++ packages-cabloy/core/src/lib/bean/beanBase.ts
@@ -1,7 +1,17 @@
 import { BeanBaseSimple } from './beanBaseSimple.js';
+import { IModuleLocaleText } from './resource/locale/type.js';
 import { IBeanScopeRecord, TypeBeanScopeRecordKeys } from './type.js';
 
+const SymbolText = Symbol('SymbolText');
+
 export class BeanBase<TScopeModule = unknown> extends BeanBaseSimple {
+  protected get $text(): IModuleLocaleText {
+    if (!this[SymbolText]) {
+      this[SymbolText] = this.ctx.meta.locale.createLocaleText(this.moduleBelong);
+    }
+    return this[SymbolText];
+  }
+
   protected get scope() {
     return this.getScope() as TScopeModule;
   }
