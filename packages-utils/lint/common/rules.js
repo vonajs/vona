6bc6463094e2e1c57d97e11e5233e3444381fd27
zhennann <zhen.nann@icloud.com>
2024-08-09 09:25:46 +0800

    feat: no-unused-vars

--- packages-utils/lint/common/rules.js
+++ packages-utils/lint/common/rules.js
@@ -82,7 +82,13 @@ const rules = {
   '@typescript-eslint/no-unused-vars': [
     'error',
     {
+      args: 'all',
       argsIgnorePattern: '^_',
+      caughtErrors: 'all',
+      caughtErrorsIgnorePattern: '^_',
+      destructuredArrayIgnorePattern: '^_',
+      varsIgnorePattern: '^_',
+      ignoreRestSiblings: true,
     },
   ],
   '@typescript-eslint/no-this-alias': 'off',
