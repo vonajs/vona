cb6246f6f8b33dc93858349de9c31c164f8cb763
zhennann <zhen.nann@icloud.com>
2024-10-21 19:52:47 +0800

    chore: update

--- build/config.js
+++ build/config.js
@@ -12,32 +12,6 @@ const backend = {
   maintenance: true,
 };
 
-/** front
- * @property {object} front
- * @property {object} front.build - npm run build:front
- * @property {boolean} front.build.productionSourceMap=true
- * @property {boolean} front.build.uglify=true
- * @property {object} front.dev - npm run dev:front
- * @property {string} front.dev.hostname='localhost'
- * @property {number} front.dev.port=9192
- * @property {string} front.dev.proxyBaseURL='http://localhost:7102'
- */
-const front = {
-  build: {
-    productionSourceMap: true,
-    uglify: true,
-  },
-  dev: {
-    // hostname: 'localhost',
-    // hostname: 'b.test.com',
-    // hostname: '192.168.0.100',
-    // hostname: '192.168.0.102',
-    port: 9192,
-    // proxyBaseURL: 'http://localhost:7102',
-  },
-};
-
 module.exports = {
-  front,
   backend,
 };
