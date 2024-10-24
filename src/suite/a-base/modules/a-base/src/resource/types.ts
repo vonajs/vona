fb17c68fe26332823f0cfc319a4105135a55fef7
zhennann <zhen.nann@icloud.com>
2024-10-24 14:36:45 +0800

    feat: update

--- src/suite/a-base/modules/a-base/src/resource/types.ts
+++
@@ -1,5 +0,0 @@
-declare module 'vona' {
-  export interface CtxMeta {
-    validateHost: { atomClass; key; options; user };
-  }
-}
--- src/suite/a-base/modules/a-base/src/types.ts
+++ src/suite/a-base/modules/a-base/src/types.ts
@@ -1,3 +1,9 @@
 export * from './types/atom/index.js';
 export * from './types/atomClass/index.js';
 export * from './types/user/index.js';
+
+declare module 'vona' {
+  export interface CtxMeta {
+    validateHost: { atomClass; key; options; user };
+  }
+}
