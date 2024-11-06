f742dcc237a8cf7b257585eb3f3c6cbf8039a7e1
zhennann <zhen.nann@icloud.com>
2024-11-06 16:33:16 +0800

    feat: create entity

--- packages-cli/cli-set-api/src/lib/commands.ts
+++ packages-cli/cli-set-api/src/lib/commands.ts
@@ -2,6 +2,7 @@ import defaultList from './command/default.list.js';
 import createSuite from './command/create.suite.js';
 import createModule from './command/create.module.js';
 import createBean from './command/create.bean.js';
+import createEntity from './command/create.entity.js';
 import initMonkey from './command/init.monkey.js';
 import initMain from './command/init.main.js';
 import toolsDeps from './command/tools.deps.js';
@@ -16,6 +17,7 @@ export const commands = {
     suite: createSuite,
     module: createModule,
     bean: createBean,
+    entity: createEntity,
   },
   init: {
     monkey: initMonkey,
