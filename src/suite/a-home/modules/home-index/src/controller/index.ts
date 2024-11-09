478cc469d781286bc982865b071f69853a289407
zhennann <zhen.nann@icloud.com>
2024-11-09 16:56:42 +0800

    Update index.ts

--- src/suite/a-home/modules/home-index/src/controller/index.ts
+++ src/suite/a-home/modules/home-index/src/controller/index.ts
@@ -1,12 +1,9 @@
-import { BeanBase, Controller, Get, useMiddlewareGlobal } from 'vona';
+import { BeanBase, Controller, Get } from 'vona';
 import { ScopeModule } from '../.metadata/this.js';
 
 @Controller()
-@useMiddlewareGlobal('a-core:guard', { enable: false, test: 'from controller' })
-@useMiddlewareGlobal('a-core:pipe', { enable: true })
 export class ControllerIndex extends BeanBase<ScopeModule> {
   @Get('//')
-  @useMiddlewareGlobal('a-core:guard', { enable: true, test: 'from action' })
   index() {
     return 'Hello Vona';
   }
