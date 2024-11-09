ec271c04feb2db1908479152c7dc8aeafea4d280
zhennann <zhen.nann@icloud.com>
2024-11-09 18:16:49 +0800

    Update index.ts

--- src/suite/a-home/modules/home-index/src/controller/index.ts
+++ src/suite/a-home/modules/home-index/src/controller/index.ts
@@ -1,11 +1,9 @@
-import { BeanBase, Controller, Get, UseMiddleware } from 'vona';
+import { BeanBase, Controller, Get } from 'vona';
 import { ScopeModule } from '../.metadata/this.js';
 
 @Controller()
-@UseMiddleware('a-core:development', { local: false })
 export class ControllerIndex extends BeanBase<ScopeModule> {
   @Get('//')
-  @UseMiddleware('a-core:development', { local: true })
   index() {
     return 'Hello Vona';
   }
