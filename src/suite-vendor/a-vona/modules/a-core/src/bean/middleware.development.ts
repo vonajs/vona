c65f555df88fbb23ccdf1690da105435663782f7
zhennann <zhen.nann@icloud.com>
2024-11-09 17:24:08 +0800

    Update middleware.development.ts

--- src/suite-vendor/a-vona/modules/a-core/src/bean/middleware.development.ts
+++ src/suite-vendor/a-vona/modules/a-core/src/bean/middleware.development.ts
@@ -1,8 +1,11 @@
 import { BeanBase, IDecoratorMiddlewareOptions, IMiddlewareExecute, Middleware, Next } from 'vona';
 
-export interface IMiddlewareOptionsDevelopment extends IDecoratorMiddlewareOptions {}
+export interface IMiddlewareOptionsDevelopment extends IDecoratorMiddlewareOptions {
+  test?: boolean;
+  local?: boolean;
+}
 
-@Middleware({} as IMiddlewareOptionsDevelopment)
+@Middleware({ test: true, local: true } as IMiddlewareOptionsDevelopment)
 export class MiddlewareDevelopment extends BeanBase implements IMiddlewareExecute {
   async execute(_options: IMiddlewareOptionsDevelopment, next: Next) {
     if (this.app.meta.isProd) this.ctx.throw(403);
