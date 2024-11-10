b15bb5fdc55cd40e56a003f2f8a94d4329463376
zhennann <zhen.nann@icloud.com>
2024-11-10 22:48:36 +0800

    Update metadata.ts

--- packages-vona/vona/src/types/config/metadata.ts
+++ packages-vona/vona/src/types/config/metadata.ts
@@ -1,5 +1,7 @@
+import { IGuardRecord } from '../interface/guard.js';
 import { IMiddlewareRecord } from '../interface/middleware.js';
 
 export interface ConfigMetadata {
   middleware: IMiddlewareRecord;
+  guard: IGuardRecord;
 }
