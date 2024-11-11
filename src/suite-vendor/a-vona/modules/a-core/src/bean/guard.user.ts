c3b67418a576c1b96b607349b2d8879fd5f763e6
zhennann <zhen.nann@icloud.com>
2024-11-11 09:17:07 +0800

    Update guard.user.ts

--- src/suite-vendor/a-vona/modules/a-core/src/bean/guard.user.ts
+++ src/suite-vendor/a-vona/modules/a-core/src/bean/guard.user.ts
@@ -8,9 +8,13 @@ export interface IGuardOptionsUser extends IDecoratorGuardOptions {
 export class GuardUser extends BeanBase implements IGuardExecute {
   async execute(options: IGuardOptionsUser, next: Next) {
     // check user
+    // todo: middleware.auth.ts
     if (!options.public) {
       const user = this.ctx.state.user?.op;
-      if (!user || (user.id !== 0 && user.anonymous)) return false;
+      if (!user || (user.id !== 0 && user.anonymous)) {
+        //return false;
+        return this.ctx.throw(401);
+      }
     }
     // next
     return next();
