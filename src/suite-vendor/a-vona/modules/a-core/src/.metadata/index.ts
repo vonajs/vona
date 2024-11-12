1117cf1e239270810acda3f5e45804c9e077067f
zhennann <zhen.nann@icloud.com>
2024-11-12 17:42:53 +0800

    feat: middleware

--- src/suite-vendor/a-vona/modules/a-core/src/.metadata/index.ts
+++ src/suite-vendor/a-vona/modules/a-core/src/.metadata/index.ts
@@ -1,22 +1,10 @@
 /** middlewares: begin */
 export * from '../bean/middleware.development.js';
-export * from '../bean/middleware.guard.js';
-export * from '../bean/middleware.interceptor.js';
-export * from '../bean/middleware.pipe.js';
 export * from '../bean/middleware.transaction.js';
 import { IMiddlewareOptionsDevelopment } from '../bean/middleware.development.js';
-import { IMiddlewareOptionsGuard } from '../bean/middleware.guard.js';
-import { IMiddlewareOptionsInterceptor } from '../bean/middleware.interceptor.js';
-import { IMiddlewareOptionsPipe } from '../bean/middleware.pipe.js';
 import { IMiddlewareOptionsTransaction } from '../bean/middleware.transaction.js';
 import 'vona';
 declare module 'vona' {
-  export interface IMiddlewareRecordGlobal {
-    'a-core:guard': IMiddlewareOptionsGuard;
-    'a-core:interceptor': IMiddlewareOptionsInterceptor;
-    'a-core:pipe': IMiddlewareOptionsPipe;
-  }
-
   export interface IMiddlewareRecordLocal {
     'a-core:development': IMiddlewareOptionsDevelopment;
     'a-core:transaction': IMiddlewareOptionsTransaction;
