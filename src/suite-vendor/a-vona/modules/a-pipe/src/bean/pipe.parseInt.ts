8646e7896aff05893fa3cdc577ee4bfc62a8742b
zhennann <zhen.nann@icloud.com>
2024-11-13 06:25:37 +0800

    feat: parse int

--- src/suite-vendor/a-vona/modules/a-pipe/src/bean/pipe.parseInt.ts
+++ src/suite-vendor/a-vona/modules/a-pipe/src/bean/pipe.parseInt.ts
@@ -9,19 +9,24 @@ import {
   ArgumentPipeResult,
   ArgumentPipeResultFn,
 } from 'vona';
+import { ScopeModule } from '../.metadata/this.js';
 
 export interface IPipeOptionsParseInt extends IDecoratorPipeOptions {
   optional?: boolean;
 }
 
 @Pipe<IPipeOptionsParseInt>()
-export class PipeParseInt extends BeanBase implements IPipeTransform {
+export class PipeParseInt extends BeanBase<ScopeModule> implements IPipeTransform {
   async transform(value, _metadata: RouteHandlerArgumentMeta, options: IPipeOptionsParseInt) {
     if (isNil(value) && options?.optional) {
       return value;
     }
     if (!this.isNumeric(value)) {
-      this.ctx.throw(422, 'sss'); // Validation failed (numeric string is expected)'
+      if (this.app.meta.isProd) {
+        this.ctx.throw(422, this.scope.locale.ValidationFailedPipeParseInt());
+      } else {
+        //this.ctx.throw(422, this.scope.locale.ValidationFailedPipeParseIntDev(metadata.));
+      }
     }
     return parseInt(value, 10);
   }
--- src/suite-vendor/a-vona/modules/a-pipe/src/config/locale/en-us.ts
+++ src/suite-vendor/a-vona/modules/a-pipe/src/config/locale/en-us.ts
@@ -1,3 +1,4 @@
 export default {
   ValidationFailedPipeParseInt: 'Validation failed (numeric string is expected)',
+  ValidationFailedPipeParseIntDev: 'Validation failed (numeric string is expected): method: %s, argument: %d',
 };
