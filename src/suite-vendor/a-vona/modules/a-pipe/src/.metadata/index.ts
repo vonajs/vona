b3493b1e9ed8071a7296c38af3c04d2fc39dee40
zhennann <zhen.nann@icloud.com>
2024-11-12 21:16:49 +0800

    feat: locale

--- src/suite-vendor/a-vona/modules/a-pipe/src/.metadata/index.ts
+++ src/suite-vendor/a-vona/modules/a-pipe/src/.metadata/index.ts
@@ -8,13 +8,22 @@ declare module 'vona' {
   }
 }
 /** pipes: end */
+/** locale: begin */
+import locale_en_us from '../config/locale/en-us.js';
+import locale_zh_cn from '../config/locale/zh-cn.js';
+export const locales = {
+  'en-us': locale_en_us,
+  'zh-cn': locale_zh_cn,
+};
+/** locale: end */
 /** scope: begin */
-import { BeanScopeBase, Scope, TypeModuleResource } from 'vona';
+import { BeanScopeBase, Scope, TypeLocaleBase, TypeModuleResource } from 'vona';
 
 @Scope()
 export class ScopeModuleAPipe extends BeanScopeBase {}
 
-export interface ScopeModuleAPipe extends TypeModuleResource<any, any, any, any, any, any> {}
+export interface ScopeModuleAPipe
+  extends TypeModuleResource<any, any, (typeof locales)[TypeLocaleBase], any, any, any> {}
 
 import 'vona';
 declare module 'vona' {
@@ -24,6 +33,10 @@ declare module 'vona' {
 
   export interface IBeanScopeContainer {
     pipe: ScopeModuleAPipe;
+  }
+
+  export interface IBeanScopeLocale {
+    'a-pipe': (typeof locales)[TypeLocaleBase];
   }
 }
 /** scope: end */
