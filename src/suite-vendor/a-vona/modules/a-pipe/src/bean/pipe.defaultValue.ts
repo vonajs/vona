fd598d6a00fee5141f58ccf92de50ded4d8f5c5e
zhennann <zhen.nann@icloud.com>
2024-11-16 15:45:31 +0800

    feat: default value

--- src/suite-vendor/a-vona/modules/a-pipe/src/bean/pipe.defaultValue.ts
+++ src/suite-vendor/a-vona/modules/a-pipe/src/bean/pipe.defaultValue.ts
@@ -13,7 +13,7 @@ export type IPipeOptionsDefaultValue = any;
 @Pipe<IPipeOptionsDefaultValue>()
 export class PipeDefaultValue extends BeanBase implements IPipeTransform<any, IPipeOptionsDefaultValue> {
   async transform(value: any, _metadata: RouteHandlerArgumentMeta, options: IPipeOptionsDefaultValue) {
-    if (isNil(value) || (isNumber(value) && isNaN(value as unknown as number)) || (value === '' && isNumber(options))) {
+    if (isNil(value) || (isNumber(value) && isNaN(value as unknown as number)) || (value === '' && !isNil(options))) {
       return options;
     }
     return value;
