c7e86288173490c4fa980e55ac218c027a49200c
zhennann <zhen.nann@icloud.com>
2024-11-18 10:28:13 +0800

    Update zod-pro.ts

--- src/suite-vendor/a-vona/modules/a-validator/src/decorator/zod-pro.ts
+++ src/suite-vendor/a-vona/modules/a-validator/src/decorator/zod-pro.ts
@@ -1,17 +1,40 @@
 import { isNil } from 'vona';
-import { z, ZodNumber } from 'zod';
+import { z } from 'zod';
+
+/////////////////////////////////////////
+/////////////////////////////////////////
+//////////                     //////////
+//////////      ZodString      //////////
+//////////                     //////////
+/////////////////////////////////////////
+/////////////////////////////////////////
+
+const _parseString = z.ZodString.prototype._parse;
+z.ZodString.prototype._parse = function (this: z.ZodString, input) {
+  _coerce(this, input, () => {
+    input.data = String(input.data);
+  });
+  return _parseString.call(this, input);
+};
 
 const _parseNumber = z.ZodNumber.prototype._parse;
-z.ZodNumber.prototype._parse = function (this: ZodNumber, input) {
-  if (this._def.coerce !== false) {
-    if (this._def.coerce === true) this._def.coerce = undefined as any;
-    if (!isNil(input.data)) {
-      if (input.data === '') {
-        input.data = undefined;
-      } else {
-        input.data = Number(input.data);
-      }
+z.ZodNumber.prototype._parse = function (this: z.ZodNumber, input) {
+  _coerce(this, input, () => {
+    if (input.data === '') {
+      input.data = undefined;
+    } else {
+      input.data = Number(input.data);
     }
-  }
+  });
   return _parseNumber.call(this, input);
 };
+
+/** coerce */
+function _coerce(instance, input, fn) {
+  if (instance._def.coerce !== false) {
+    if (instance._def.coerce === true) instance._def.coerce = undefined as any;
+    if (!isNil(input.data)) {
+      fn();
+    }
+  }
+}
