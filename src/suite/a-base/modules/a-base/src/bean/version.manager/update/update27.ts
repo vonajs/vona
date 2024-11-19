21cc26d0afc87d8cc21e0a3e01e30eff7b4551aa
zhennann <zhen.nann@icloud.com>
2024-11-19 15:53:16 +0800

    Update update27.ts

--- src/suite/a-base/modules/a-base/src/bean/version.manager/update/update27.ts
+++ src/suite/a-base/modules/a-base/src/bean/version.manager/update/update27.ts
@@ -21,13 +21,12 @@ export class VersionUpdate extends BeanBase<ScopeModule> {
 
   async _adjustRoleExpands(_options) {
     // all instances
-    const instances = await this.ctx.bean.instance.list({ where: {} });
+    const instances = await this.ctx.bean.instance.list();
     for (const instance of instances) {
       await this.ctx.meta.util.executeBean({
         subdomain: instance.name,
-        fn: async ({ ctx }) => {
-          const selfInstance = ctx.bean._newBean(VersionUpdate);
-          await selfInstance._adjustRoleExpandsInstance();
+        fn: async () => {
+          await this._adjustRoleExpandsInstance();
         },
       });
     }
