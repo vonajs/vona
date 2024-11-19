dc2d71bfe53415ff047aa84570974db13e2f24e9
zhennann <zhen.nann@icloud.com>
2024-11-19 20:57:16 +0800

    Update bean.test.ts

--- src/suite/test-party/modules/test-party/test/controller/test/feat/bean.test.ts
+++ src/suite/test-party/modules/test-party/test/controller/test/feat/bean.test.ts
@@ -1,24 +1,26 @@
 // eslint-disable-next-line
 import { app, mockUrl, mockInfo, assert } from 'egg-born-mock';
 
-describe('test/controller/test/feat/bean.test.js', () => {
+describe.only('test/controller/test/feat/bean.test.js', () => {
   it('action:bean', async () => {
     // ctx
-    const ctx = await app.meta.mockUtil.mockCtx();
-    await ctx.meta.util.performAction({
-      innerAccess: false,
-      method: 'get',
-      url: mockUrl('test/feat/beanTest', false),
+    await app.meta.mockUtil.mockRunInAnonymousContextScope(async ctx => {
+      await ctx.meta.util.performAction({
+        innerAccess: false,
+        method: 'get',
+        url: mockUrl('test/feat/beanTest', false),
+      });
     });
   });
 
   it('action:service', async () => {
     // ctx
-    const ctx = await app.meta.mockUtil.mockCtx();
-    await ctx.meta.util.performAction({
-      innerAccess: false,
-      method: 'get',
-      url: mockUrl('test/feat/bean/serviceTest', false),
+    await app.meta.mockUtil.mockRunInAnonymousContextScope(async ctx => {
+      await ctx.meta.util.performAction({
+        innerAccess: false,
+        method: 'get',
+        url: mockUrl('test/feat/bean/serviceTest', false),
+      });
     });
   });
 });
