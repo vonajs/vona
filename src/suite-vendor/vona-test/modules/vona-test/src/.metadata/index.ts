b93c0e95efba5f729643f9bff4ebff960db35aee
zhennann <zhen.nann@icloud.com>
2024-11-22 21:28:53 +0800

    feat: test

--- src/suite-vendor/vona-test/modules/vona-test/src/.metadata/index.ts
+++ src/suite-vendor/vona-test/modules/vona-test/src/.metadata/index.ts
@@ -1,10 +1,19 @@
+/** locale: begin */
+import locale_en_us from '../config/locale/en-us.js';
+import locale_zh_cn from '../config/locale/zh-cn.js';
+export const locales = {
+  'en-us': locale_en_us,
+  'zh-cn': locale_zh_cn,
+};
+/** locale: end */
 /** scope: begin */
-import { BeanScopeBase, Scope, TypeModuleResource } from 'vona';
+import { BeanScopeBase, Scope, TypeLocaleBase, TypeModuleResource } from 'vona';
 
 @Scope()
 export class ScopeModuleVonaTest extends BeanScopeBase {}
 
-export interface ScopeModuleVonaTest extends TypeModuleResource<never, never, never, never, never, never> {}
+export interface ScopeModuleVonaTest
+  extends TypeModuleResource<never, never, (typeof locales)[TypeLocaleBase], never, never, never> {}
 
 import 'vona';
 declare module 'vona' {
@@ -15,5 +24,13 @@ declare module 'vona' {
   export interface IBeanScopeContainer {
     vonaTest: ScopeModuleVonaTest;
   }
+
+  export interface IBeanScopeLocale {
+    'vona-test': (typeof locales)[TypeLocaleBase];
+  }
+}
+
+export function locale<K extends keyof (typeof locales)[TypeLocaleBase]>(key: K): K {
+  return key;
 }
 /** scope: end */
--- src/suite-vendor/vona-test/modules/vona-test/src/test/locale.test.ts
+++ src/suite-vendor/vona-test/modules/vona-test/src/test/locale.test.ts
@@ -4,7 +4,10 @@ describe.only('locale.test.ts', () => {
   it('action:locale', async () => {
     const moduleInfo = mockModuleInfo();
     await app.meta.mockUtil.mockCtx(async ctx => {
+      // getText
       assert.equal(app.meta.locale.getText(false, moduleInfo.relativeName, ctx.locale, 'TestLocale'), 'TestLocale');
+      // scope
+      const scopeTest = app.bean.scope('');
     });
   });
 });
