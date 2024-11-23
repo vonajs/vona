1e5b6d25ce363d103a38df3b72073839b380eaf2
zhennann <zhen.nann@icloud.com>
2024-11-23 11:16:51 +0800

    feat: test locale

--- src/suite-vendor/vona-test/modules/vona-test/src/config/locale/zh-cn.ts
+++ src/suite-vendor/vona-test/modules/vona-test/src/config/locale/zh-cn.ts
@@ -2,6 +2,6 @@ export default {
   TestHelloWorld: '您好，世界',
   TestApples: '%d个苹果',
   TestApples_0: '没有苹果',
-  TestNameApples_: '$s有%d个苹果',
-  TestNameApples_0_1: '$s没有苹果',
+  TestNameApples_: '%s有%d个苹果',
+  TestNameApples_0_1: '%s没有苹果',
 };
