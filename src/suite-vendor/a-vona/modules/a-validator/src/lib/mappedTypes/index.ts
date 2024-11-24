7ea3372296c1c7f8737a257479e645000b4adfc8
zhennann <zhen.nann@icloud.com>
2024-11-24 17:01:54 +0800

    feat: pick type

--- src/suite-vendor/a-vona/modules/a-validator/src/lib/mappedTypes/index.ts
+++ src/suite-vendor/a-vona/modules/a-validator/src/lib/mappedTypes/index.ts
@@ -1,1 +1,2 @@
 export * from './omitType.js';
+export * from './pickType.js';
---
+++ src/suite-vendor/a-vona/modules/a-validator/src/lib/mappedTypes/pickType.ts
@@ -0,0 +1,17 @@
+import { appMetadata, Type } from 'vona';
+import { SymbolDecoratorRule } from '../decorator/rule.js';
+
+export function pickType<T, K extends keyof T>(classRef: Type<T>, keys: K[]): Type<Omit<T, (typeof keys)[number]>> {
+  abstract class PickTypeClass {}
+  const rules = appMetadata.getOwnMetadataMap(SymbolDecoratorRule, classRef.prototype);
+  const rulesNew = {};
+  if (rules) {
+    for (const key in rules) {
+      if (keys.includes(key as any)) {
+        rulesNew[key] = rules[key];
+      }
+    }
+  }
+  appMetadata.defineMetadata(SymbolDecoratorRule, rulesNew, PickTypeClass.prototype);
+  return PickTypeClass as any;
+}
