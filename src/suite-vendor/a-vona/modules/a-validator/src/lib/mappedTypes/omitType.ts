ac4935ebf45aea72837ef15f104c10832d42489c
zhennann <zhen.nann@icloud.com>
2024-11-24 16:30:01 +0800

    feat: omitType

--- src/suite-vendor/a-vona/modules/a-validator/src/lib/mappedTypes/omitType.ts
+++ src/suite-vendor/a-vona/modules/a-validator/src/lib/mappedTypes/omitType.ts
@@ -1,12 +1,8 @@
 import { appMetadata, Type } from 'vona';
 import { SymbolDecoratorRule } from '../decorator/rule.js';
 
-export function omitType<T, K extends keyof T>(
-  classRef: Type<T>,
-  keys: readonly K[],
-): Type<Omit<T, (typeof keys)[number]>> {
+export function omitType<T, K extends keyof T>(classRef: Type<T>, keys: K[]): Type<Omit<T, (typeof keys)[number]>> {
   const omitedType: any = function () {};
-  omitedType.prototype = classRef.prototype;
   const rules = appMetadata.getOwnMetadataMap(SymbolDecoratorRule, classRef);
   appMetadata.defineMetadata(SymbolDecoratorRule, rules, omitedType);
   return omitedType;
--- src/suite-vendor/vona-test/modules/vona-test/src/test/mappedTypes.test.ts
+++ src/suite-vendor/vona-test/modules/vona-test/src/test/mappedTypes.test.ts
@@ -2,6 +2,7 @@ import { app } from 'egg-born-mock';
 import { DtoUser } from '../dto/user.js';
 import { catchError } from 'vona';
 import assert from 'assert';
+import { omitType } from 'vona-module-a-validator';
 
 describe('mappedTypes.test.ts', () => {
   it('action:mappedTypes', async () => {
@@ -9,11 +10,15 @@ describe('mappedTypes.test.ts', () => {
       const serviceValidator = app.bean._getBean('a-validator.service.validator');
       const data = { id: 1, name: 'tom', married: true };
       // original
-      const [dataNew] = await catchError(async () => {
+      const [dataNew, err] = await catchError(async () => {
         return await serviceValidator.validate(DtoUser, data, { strict: true });
       });
-      assert.deepEqual(dataNew, data);
+      assert.deepEqual(dataNew, data, JSON.stringify(err, null, 2));
       // omitType
+      const [dataNew2, err2] = await catchError(async () => {
+        return await serviceValidator.validate(omitType(DtoUser, ['married']), data, { strict: true });
+      });
+      assert.deepEqual(dataNew2, data, JSON.stringify(err2, null, 2));
     });
   });
 });
