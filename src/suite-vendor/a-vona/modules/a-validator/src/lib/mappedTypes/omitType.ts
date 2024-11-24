ff3c3aa7ed55c698c788d2a5a7c64a2a3301f314
zhennann <zhen.nann@icloud.com>
2024-11-24 16:48:41 +0800

    Update omitType.ts

--- src/suite-vendor/a-vona/modules/a-validator/src/lib/mappedTypes/omitType.ts
+++ src/suite-vendor/a-vona/modules/a-validator/src/lib/mappedTypes/omitType.ts
@@ -2,8 +2,16 @@ import { appMetadata, Type } from 'vona';
 import { SymbolDecoratorRule } from '../decorator/rule.js';
 
 export function omitType<T, K extends keyof T>(classRef: Type<T>, keys: K[]): Type<Omit<T, (typeof keys)[number]>> {
-  const omitedType: any = function () {};
-  const rules = appMetadata.getOwnMetadataMap(SymbolDecoratorRule, classRef);
-  appMetadata.defineMetadata(SymbolDecoratorRule, rules, omitedType);
-  return omitedType;
+  abstract class OmitTypeClass {}
+  const rules = appMetadata.getOwnMetadataMap(SymbolDecoratorRule, classRef.prototype);
+  const rulesNew = {};
+  if (rules) {
+    for (const key in rules) {
+      if (!keys.includes(key as any)) {
+        rulesNew[key] = rules[key];
+      }
+    }
+  }
+  appMetadata.defineMetadata(SymbolDecoratorRule, rulesNew, OmitTypeClass.prototype);
+  return OmitTypeClass as any;
 }
