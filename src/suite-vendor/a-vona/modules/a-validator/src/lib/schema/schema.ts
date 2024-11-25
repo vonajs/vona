a0426528b93ce6a863be9097b090da950283fb92
zhennann <zhen.nann@icloud.com>
2024-11-25 11:48:47 +0800

    Update schema.ts

--- src/suite-vendor/a-vona/modules/a-validator/src/lib/schema/schema.ts
+++ src/suite-vendor/a-vona/modules/a-validator/src/lib/schema/schema.ts
@@ -3,7 +3,17 @@ import { ValidatorOptions } from '../types/validatorOptions.js';
 import { z } from 'zod';
 import { SymbolDecoratorRule } from '../decorator/rule.js';
 
-export function schema<T>(classType: Constructable<T>, options?: Partial<ValidatorOptions>): z.ZodSchema<T> {
+export function schema(classType: StringConstructor): z.ZodNumber;
+export function schema(classType: NumberConstructor): z.ZodNumber;
+export function schema(classType: BooleanConstructor): z.ZodNumber;
+export function schema(classType: DateConstructor): z.ZodNumber;
+export function schema<T>(classType: Constructable<T>, options?: Partial<ValidatorOptions>): z.ZodSchema<T>;
+export function schema(classType: any, options?: Partial<ValidatorOptions>): any {
+  if (classType.name === 'String') return z.string();
+  if (classType.name === 'Number') return z.number();
+  if (classType.name === 'Boolean') return z.boolean();
+  if (classType.name === 'Date') return z.date();
+  // object
   const rules = appMetadata.getMetadata(SymbolDecoratorRule, classType.prototype);
   let schema = z.object((rules as z.ZodRawShape) || {});
   if (options?.passthrough) schema = schema.passthrough() as any;
