bbaee671df1323e1d805b8a2091d4f471ea4077e
zhennann <zhen.nann@icloud.com>
2024-11-26 14:45:09 +0800

    Update index.ts

--- src/suite/a-base/modules/a-socketio/src/.metadata/index.ts
+++ src/suite/a-base/modules/a-socketio/src/.metadata/index.ts
@@ -8,7 +8,6 @@ export * from '../bean/queue.push.js';
 export * from '../bean/queue.pushDirect.js';
 export * from '../bean/version.manager.js';
 import { BeanIo } from '../bean/bean.io.js';
-import { BeanIoMessageBase } from '../bean/bean.ioMessageBase_.js';
 import { BroadcastSocketEmit } from '../bean/broadcast.socketEmit.js';
 import { QueueDelivery } from '../bean/queue.delivery.js';
 import { QueueProcess } from '../bean/queue.process.js';
@@ -22,7 +21,6 @@ declare module 'vona' {
   }
 
   export interface IBeanRecordGeneral {
-    ioMessageBase: BeanIoMessageBase;
     'a-socketio.broadcast.socketEmit': BroadcastSocketEmit;
     'a-socketio.queue.delivery': QueueDelivery;
     'a-socketio.queue.process': QueueProcess;
@@ -136,4 +134,5 @@ declare module 'vona' {
     'a-socketio': ReturnType<typeof config>;
   }
 }
+
 /** scope: end */
