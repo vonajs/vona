3eea5130c9f4f3975906b9723c22966d579da8f1
zhennann <zhen.nann@icloud.com>
2024-12-04 15:27:36 +0800

    Update utils.ts

--- packages-vona/vona/src/lib/mappedClass/utils.ts
+++ packages-vona/vona/src/lib/mappedClass/utils.ts
@@ -1,4 +1,5 @@
 import { appMetadata, MetadataKey } from '../core/metadata.js';
+import { Constructable } from '../decorator/index.js';
 import { IMappedClassMetadataOptions, MappedClassMetadataKeys, SymbolMappedClassMetadataKeys } from './type.js';
 
 export function registerMappedClassMetadataKey(
@@ -20,11 +21,46 @@ export function getMappedClassMetadataKeys(target: object): MappedClassMetadataK
   return appMetadata.getMetadata<MappedClassMetadataKeys>(SymbolMappedClassMetadataKeys, target);
 }
 
+export function copyPropertiesOfClasses(target: Constructable, sources: Constructable[], filter?: Function) {
+  for (const source of sources) {
+    copyProperties(target, source, ['constructor', 'prototype', 'name'], filter); // copy static
+    copyProperties(target.prototype, source.prototype, ['constructor', 'prototype'], filter); // copy prototype
+  }
+}
+
 export function copyProperties(target: object, source: object, keysIgnore: MetadataKey[], filter?: Function) {
   for (const key of Reflect.ownKeys(source)) {
     if (keysIgnore.includes(key)) continue;
     if (filter && !filter(key)) continue;
     const desc = Object.getOwnPropertyDescriptor(source, key)!;
     Object.defineProperty(target, key, desc);
+  }
+}
+
+export function copyMetadataOfClasses(target: object, sources: object[], transform?: Function) {
+  //
+  const metadataKeys = {};
+  for (const source of sources) {
+    const _metadataKeys = getMappedClassMetadataKeys(source);
+    if (_metadataKeys) {
+      Object.assign(metadataKeys, _metadataKeys);
+    }
+  }
+  //
+  for (const metadataKey of Object.getOwnPropertySymbols(metadataKeys)) {
+    const metadataKeyOptions = metadataKeys[metadataKey];
+    const rulesNew = {};
+    for (const source of sources) {
+      const rules = appMetadata.getMetadata(metadataKey, source);
+      if (!rules) continue;
+      if (!transform) {
+        Object.assign(rulesNew, rules);
+      } else {
+        for (const key in rules) {
+          rulesNew[key] = transform(rules, key, metadataKeyOptions);
+        }
+      }
+    }
+    appMetadata.defineMetadata(metadataKey, rulesNew, target);
   }
 }
