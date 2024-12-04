0849435bd02abc139135a912938818437f71a17d
zhennann <zhen.nann@icloud.com>
2024-12-04 11:12:59 +0800

    Update instance.ts

--- src/suite/a-base/modules/a-instance/src/entity/instance.ts
+++ src/suite/a-base/modules/a-instance/src/entity/instance.ts
@@ -1,5 +1,5 @@
 import { ConfigInstanceBase, Entity, omitClass } from 'vona';
-import { EntityBase } from 'vona-module-a-core';
+import { EntityBase } from 'vona-module-a-database';
 import { Rule } from 'vona-module-a-validator';
 import { z } from 'zod';
 
