259043e472bfda366d0682d4e20fd312613ddb2c
zhennann <zhen.nann@icloud.com>
2024-12-04 11:28:31 +0800

    feat: entity base

--- src/suite/a-base/modules/a-database/src/types/index.ts
+++ src/suite/a-base/modules/a-database/src/types/index.ts
@@ -1,6 +1,6 @@
 export * from './config.js';
 export * from './database.js';
-export * from './entity.js';
+export * from './entityBase.js';
 export * from './model.js';
 export * from './tableIdentity.js';
 export * from './transaction.js';
