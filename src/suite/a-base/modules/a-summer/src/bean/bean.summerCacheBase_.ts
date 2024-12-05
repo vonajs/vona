1c7bd970697c30ee43bca12b235b4d7c25799336
zhennann <zhen.nann@icloud.com>
2024-12-05 20:32:47 +0800

    Update bean.summerCacheBase_.ts

--- src/suite/a-base/modules/a-summer/src/bean/bean.summerCacheBase_.ts
+++ src/suite/a-base/modules/a-summer/src/bean/bean.summerCacheBase_.ts
@@ -4,18 +4,18 @@ import { IModuleConfigSummerCacheBase } from '../config/types.js';
 import { CacheBase } from '../common/cacheBase.js';
 
 @Virtual({ scene: 'bean' })
-export class BeanSummerCacheBase<TScopeModule = unknown> extends CacheBase<TScopeModule> {
+export class BeanSummerCacheBase<TScopeModule = unknown, KEY = any, DATA = any> extends CacheBase<TScopeModule> {
   constructor({ cacheBase }: { cacheBase: IModuleConfigSummerCacheBase }) {
     super({ cacheBase });
   }
 
-  async get(key, options?) {
+  async get(key: KEY, options?): Promise<DATA | null | undefined> {
     const keyHash = this.__getKeyHash(key);
     const layered = this.__getLayered(options);
     return await layered.get(keyHash, key, options);
   }
 
-  async mget(keys, options?) {
+  async mget(keys: KEY[], options?): Promise<DATA[]> {
     if (!keys || keys.length === 0) {
       return [];
     }
@@ -24,13 +24,13 @@ export class BeanSummerCacheBase<TScopeModule = unknown> extends CacheBase<TScop
     return await layered.mget(keysHash, keys, options);
   }
 
-  async del(key, options?) {
+  async del(key: KEY, options?) {
     const keyHash = this.__getKeyHash(key);
     const layered = this.__getLayered(options);
     return await layered.del(keyHash, key, options);
   }
 
-  async mdel(keys, options?) {
+  async mdel(keys: KEY[], options?) {
     if (!keys || keys.length === 0) {
       return [];
     }
@@ -44,13 +44,13 @@ export class BeanSummerCacheBase<TScopeModule = unknown> extends CacheBase<TScop
     return await layered.clear(options);
   }
 
-  async peek(key, options?) {
+  async peek(key: KEY, options?): Promise<DATA | null | undefined> {
     const keyHash = this.__getKeyHash(key);
     const layered = this.__getLayered(options);
     return await layered.peek(keyHash, key, options);
   }
 
-  __getLayered(options?) {
+  private __getLayered(options?) {
     if (!this.__getOptionsEnabled(options)) {
       return this.localFetch;
     }
@@ -61,19 +61,20 @@ export class BeanSummerCacheBase<TScopeModule = unknown> extends CacheBase<TScop
     return this.localRedis;
   }
 
-  __getKeyHash(key) {
+  private __getKeyHash(key: KEY): string {
     if (key === undefined || key === null) {
       throw new Error('key is required');
     }
     if (Array.isArray(key) || typeof key === 'object') {
-      key = objectHash(key, { respectType: false });
-    } else if (typeof key !== 'string') {
-      key = String(key);
+      return objectHash(key, { respectType: false });
+    }
+    if (typeof key !== 'string') {
+      return String(key);
     }
     return key;
   }
 
-  __getKeysHash(keys) {
+  private __getKeysHash(keys: KEY[]): string[] {
     return keys.map(key => this.__getKeyHash(key));
   }
 }
