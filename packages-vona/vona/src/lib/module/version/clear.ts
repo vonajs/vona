ecf820fb7ed9272cf93389bb60a0874b140def66
zhennann <zhen.nann@icloud.com>
2024-12-07 07:22:26 +0800

    Update clear.ts

--- packages-vona/vona/src/lib/module/version/clear.ts
+++ packages-vona/vona/src/lib/module/version/clear.ts
@@ -1,21 +1,23 @@
 import path from 'path';
 import fse from 'fs-extra';
+import { VonaApplication } from '../../../types/index.js';
 
-export default async function (app) {
+export default async function (app: VonaApplication) {
   if (!app.meta.isTest) return;
   // clear keys
   await _clearRedisKeys(app.redis.get('limiter'), `b_${app.name}:*`);
   await _clearRedisKeys(app.redis.get('queue'), `bull_${app.name}:*`);
   // broadcast channel has subscribed
   // await _clearRedisKeys(app.redis.get('broadcast'), `broadcast_${app.name}:*`);
-  await _clearRedisKeys(app.redis.get('cache'), `cache_${app.name}:*`);
-  await _clearRedisKeys(app.redis.get('io'), `io_${app.name}:*`);
-  await _clearRedisKeys(app.redis.get('auth'), `auth_${app.name}:*`);
-  await _clearRedisKeys(app.redis.get('summer'), `summer_${app.name}:*`);
-  await _clearRedisKeys(app.redis.get('model'), `model_${app.name}:*`);
   // redlock
   for (const clientName of app.config.queue.redlock.clients) {
     await _clearRedisKeys(app.redis.get(clientName), `redlock_${app.name}:*`);
+  }
+  for (const clientName in app.config.redis.clients) {
+    if (['redlock', 'limiter', 'queue', 'broadcast'].includes(clientName)) continue;
+    if (clientName.includes('redlock')) continue;
+    const client = app.config.redis.clients[clientName];
+    await _clearRedisKeys(app.redis.get(clientName), `${client.keyPrefix}*`);
   }
   // src/backend/app/public
   await fse.remove(path.join(app.options.baseDir, 'app/public/1'));
