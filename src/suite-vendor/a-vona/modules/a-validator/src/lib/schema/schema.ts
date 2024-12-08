7933de7b597d59eebda5dd1209ec6eb5f2e2aba8
zhennann <zhen.nann@icloud.com>
2024-12-08 11:23:40 +0800

    Update schema.ts

--- src/suite-vendor/a-vona/modules/a-validator/src/lib/schema/schema.ts
+++ src/suite-vendor/a-vona/modules/a-validator/src/lib/schema/schema.ts
@@ -3,16 +3,18 @@ import { ValidatorOptions } from '../types/validatorOptions.js';
 import { z } from 'zod';
 import { SymbolDecoratorRule } from '../decorator/rule.js';
 
-export function schema(classType: StringConstructor): z.ZodNumber;
+export function schema(classType: StringConstructor): z.ZodString;
 export function schema(classType: NumberConstructor): z.ZodNumber;
-export function schema(classType: BooleanConstructor): z.ZodNumber;
-export function schema(classType: DateConstructor): z.ZodNumber;
+export function schema(classType: BooleanConstructor): z.ZodBoolean;
+export function schema(classType: DateConstructor): z.ZodDate;
+export function schema(classType: BigIntConstructor): z.ZodBigInt;
 export function schema<T>(classType: Constructable<T>, options?: Partial<ValidatorOptions>): z.ZodSchema<T>;
 export function schema(classType: any, options?: Partial<ValidatorOptions>): any {
   if (classType.name === 'String') return z.string();
   if (classType.name === 'Number') return z.number();
   if (classType.name === 'Boolean') return z.boolean();
   if (classType.name === 'Date') return z.date();
+  if (classType.name === 'BigInt') return z.bigint();
   // object
   const rules = appMetadata.getMetadata(SymbolDecoratorRule, classType.prototype);
   let schema = z.object((rules as z.ZodRawShape) || {});
