a49c07b042bad3fc23f4d39d4ef388a142388f2b
zhennann <zhen.nann@icloud.com>
2024-12-11 16:19:44 +0800

    Update schedule.ts

--- src/suite-vendor/a-vona/modules/a-schedule/src/types/schedule.ts
+++ src/suite-vendor/a-vona/modules/a-schedule/src/types/schedule.ts
@@ -1,8 +1,8 @@
 import * as Bull from 'bullmq';
-import { TypeQueueScheduleJobData } from '../bean/queue.schedule.js';
+import { TypeQueueScheduleJobData, TypeQueueScheduleJobResult } from '../bean/queue.schedule.js';
 import { IQueueJobContext } from 'vona-module-a-queue';
 
-export type TypeScheduleJob = Bull.Job<IQueueJobContext<TypeQueueScheduleJobData>, void>;
+export type TypeScheduleJob = Bull.Job<IQueueJobContext<TypeQueueScheduleJobData>, TypeQueueScheduleJobResult>;
 
 export interface IScheduleExecute {
   execute(job?: TypeScheduleJob): Promise<void>;
