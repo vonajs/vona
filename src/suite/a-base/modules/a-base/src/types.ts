87a82bc3c7df60c620e8038ded00b7d1252d2c9e
zhennann <zhen.nann@icloud.com>
2024-12-12 11:35:20 +0800

    feat: scope

--- src/suite/a-base/modules/a-base/src/types.ts
+++ src/suite/a-base/modules/a-base/src/types.ts
@@ -1,8 +1,13 @@
+import { BeanAtomBase } from './bean/bean.atomBase_.js';
+
 export * from './types/atom/index.js';
 export * from './types/atomClass/index.js';
 export * from './types/user/index.js';
 
 declare module 'vona' {
+  export interface IBeanRecordGlobal {
+    atomBase: BeanAtomBase;
+  }
   export interface CtxMeta {
     validateHost: { atomClass; key; options; user };
   }
