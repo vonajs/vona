e01e7607ad17992513cdc0d22b8d49c8c5d2c438
zhennann <zhen.nann@icloud.com>
2024-12-12 12:18:44 +0800

    Update test.ts

--- src/suite/a-flow/modules/a-flow/src/bean/version.manager/test/test.ts
+++ src/suite/a-flow/modules/a-flow/src/bean/version.manager/test/test.ts
@@ -2,18 +2,14 @@ import { BeanBase } from 'vona';
 import { __ThisModule__ } from '../../../.metadata/this.js';
 
 export class VersionTest extends BeanBase {
-  constructor() {
-    super(__ThisModule__);
-  }
-
   async run() {
     // why add these test codes
     //   - for force flowHistory.id !== flow.id
     // flowHistory
-    let res = await this.scope.model.flowHistory.insert({});
-    await this.scope.model.flowHistory.delete({ id: res[0] });
+    let res = await this.$scope.flow.model.flowHistory.insert({});
+    await this.$scope.flow.model.flowHistory.delete({ id: res[0] });
     // flowNodeHistory
-    res = await this.scope.model.flowNodeHistory.insert({});
-    await this.scope.model.flowNodeHistory.delete({ id: res[0] });
+    res = await this.$scope.flow.model.flowNodeHistory.insert({});
+    await this.$scope.flow.model.flowNodeHistory.delete({ id: res[0] });
   }
 }
