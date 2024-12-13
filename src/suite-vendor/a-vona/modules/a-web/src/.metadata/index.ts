0cce96afb7d5c6d0df5efb3abf69ad4f7b7fc6ae
zhennann <zhen.nann@icloud.com>
2024-12-13 17:17:48 +0800

    feat: router

--- src/suite-vendor/a-vona/modules/a-web/src/.metadata/index.ts
+++ src/suite-vendor/a-vona/modules/a-web/src/.metadata/index.ts
@@ -1,3 +1,32 @@
+/** service: begin */
+export * from '../service/router.js';
+
+import 'vona';
+declare module 'vona' {
+  export interface IServiceRecord {
+    'a-web:router': never;
+  }
+}
+declare module 'vona-module-a-web' {
+  export interface ServiceRouter {
+    get scope(): ScopeModuleAWeb;
+  }
+}
+/** service: end */
+/** service: begin */
+import { ServiceRouter } from '../service/router.js';
+export interface IModuleService {
+  router: ServiceRouter;
+}
+/** service: end */
+/** service: begin */
+import 'vona';
+declare module 'vona' {
+  export interface IBeanRecordGeneral {
+    'a-web.service.router': ServiceRouter;
+  }
+}
+/** service: end */
 /** scope: begin */
 import { BeanScopeBase, Scope, TypeModuleBean, BeanScopeUtil } from 'vona';
 
@@ -7,6 +36,7 @@ export class ScopeModuleAWeb extends BeanScopeBase {}
 export interface ScopeModuleAWeb {
   _bean: TypeModuleBean;
   util: BeanScopeUtil;
+  service: IModuleService;
 }
 
 import 'vona';
