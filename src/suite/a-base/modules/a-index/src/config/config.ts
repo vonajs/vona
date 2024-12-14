4c835473b8c0d87f9e747e6ddc99e65db45367d2
zhennann <zhen.nann@icloud.com>
2024-12-14 20:32:36 +0800

    feat: meta

--- src/suite/a-base/modules/a-index/src/config/config.ts
+++ src/suite/a-base/modules/a-index/src/config/config.ts
@@ -1,4 +1,5 @@
-import { MetaOptionsIndexModuleIndexes, VonaApplication } from 'vona';
+import { VonaApplication } from 'vona';
+import { MetaOptionsIndexModuleIndexes } from 'vona-module-a-meta';
 
 export type ConfigModulesIndexes = Record<string, MetaOptionsIndexModuleIndexes>;
 export const config = (_app: VonaApplication) => {
