35d9c130f68736c81db67a5873ccfe49f15b07e9
zhennann <zhen.nann@icloud.com>
2024-12-15 17:09:45 +0800

    feat: onionName

--- src/suite-vendor/a-vona/modules/a-aspect/src/lib/use/useOnionBase.ts
+++ src/suite-vendor/a-vona/modules/a-aspect/src/lib/use/useOnionBase.ts
@@ -1,15 +1,15 @@
 import { appMetadata, MetadataKey } from 'vona';
 import { SymbolUseOnionLocal, SymbolUseOnionOptions } from 'vona-module-a-onion';
 
-export function UseOnionBase(sceneName: string, middlewareName, options?): ClassDecorator & MethodDecorator {
+export function UseOnionBase(sceneName: string, onionName: string, options?: any): ClassDecorator & MethodDecorator {
   return function (target: object, prop?: MetadataKey, descriptor?: PropertyDescriptor) {
     const middlewaresOptions = appMetadata.getOwnMetadataMap(false, SymbolUseOnionOptions, target, prop);
     const middlewaresLocal = appMetadata.getOwnMetadataMap<string, string[]>(false, SymbolUseOnionLocal, target, prop);
     //
-    const beanFullName = (middlewareName as string).replace(':', `.${sceneName}.`);
+    const beanFullName = onionName.replace(':', `.${sceneName}.`);
     middlewaresOptions[beanFullName] = options;
     if (!middlewaresLocal[sceneName]) middlewaresLocal[sceneName] = [];
-    middlewaresLocal[sceneName].push(middlewareName);
+    middlewaresLocal[sceneName].push(onionName);
     return descriptor;
   } as any;
 }
--- src/suite-vendor/a-vona/modules/a-aspect/src/lib/use/useOnionGlobalBase.ts
+++ src/suite-vendor/a-vona/modules/a-aspect/src/lib/use/useOnionGlobalBase.ts
@@ -1,10 +1,14 @@
 import { appMetadata, MetadataKey } from 'vona';
 import { SymbolUseOnionOptions } from 'vona-module-a-onion';
 
-export function UseOnionGlobalBase(sceneName: string, middlewareName, options?): ClassDecorator & MethodDecorator {
+export function UseOnionGlobalBase(
+  sceneName: string,
+  onionName: string,
+  options?: any,
+): ClassDecorator & MethodDecorator {
   return function (target: object, prop?: MetadataKey, descriptor?: PropertyDescriptor) {
     const middlewaresOptions = appMetadata.getOwnMetadataMap(false, SymbolUseOnionOptions, target, prop);
-    const beanFullName = (middlewareName as string).replace(':', `.${sceneName}.`);
+    const beanFullName = onionName.replace(':', `.${sceneName}.`);
     middlewaresOptions[beanFullName] = options;
     return descriptor;
   } as any;
