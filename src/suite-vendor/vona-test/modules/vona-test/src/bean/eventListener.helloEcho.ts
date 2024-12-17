178f9ae5a2df807507c4f464004c913ddc0e6af5
zhennann <zhen.nann@icloud.com>
2024-12-17 10:25:56 +0800

    Update eventListener.helloEcho.ts

--- src/suite-vendor/vona-test/modules/vona-test/src/bean/eventListener.helloEcho.ts
+++ src/suite-vendor/vona-test/modules/vona-test/src/bean/eventListener.helloEcho.ts
@@ -1,5 +1,5 @@
-import { BeanBase, Next } from 'vona';
-import { EventListener, IEventExecute } from 'vona-module-a-event';
+import { BeanBase } from 'vona';
+import { EventListener, IEventExecute, NextEvent } from 'vona-module-a-event';
 import { TypeEventHelloEchoArgs, TypeEventHelloEchoResult } from './event.helloEcho.js';
 
 @EventListener({ match: 'vona-test:helloEcho' })
@@ -7,7 +7,10 @@ export class EventListenerHelloEcho
   extends BeanBase
   implements IEventExecute<TypeEventHelloEchoArgs, TypeEventHelloEchoResult>
 {
-  async execute(_args: TypeEventHelloEchoArgs, next: Next): Promise<TypeEventHelloEchoResult> {
+  async execute(
+    _args: TypeEventHelloEchoArgs,
+    next: NextEvent<TypeEventHelloEchoResult>,
+  ): Promise<TypeEventHelloEchoResult> {
     // next
     return next();
   }
