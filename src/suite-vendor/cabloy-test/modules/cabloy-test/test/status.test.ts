48e0f559c986bbe8d6cce344f12b3d5d4a357d37
zhennann <zhen.nann@icloud.com>
2024-12-20 19:16:59 +0800

    Update status.test.ts

--- src/suite-vendor/cabloy-test/modules/cabloy-test/test/status.test.ts
+++ src/suite-vendor/cabloy-test/modules/cabloy-test/test/status.test.ts
@@ -3,7 +3,7 @@ import { app } from 'vona-mock';
 describe('status.test.ts', () => {
   it('action:status', async () => {
     await app.bean.executor.mockCtx(async () => {
-      await app.bean.executor.performAction('post', '/vona/test/status');
+      await app.bean.executor.performAction('post', '/cabloy/test/status');
     });
   });
 });
