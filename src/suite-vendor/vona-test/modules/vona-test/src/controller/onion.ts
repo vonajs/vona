953c0a2b2e97a71f3723b7fb6a7a6c149c410a35
zhennann <zhen.nann@icloud.com>
2024-12-20 21:59:34 +0800

    Update onion.ts

--- src/suite-vendor/vona-test/modules/vona-test/src/controller/onion.ts
+++ src/suite-vendor/vona-test/modules/vona-test/src/controller/onion.ts
@@ -3,7 +3,7 @@ import { Body, Query, v } from 'vona-module-a-validation';
 import { z } from 'zod';
 import { DtoUser } from '../dto/user.js';
 import { Controller, Get, Post } from 'vona-module-a-web';
-import { UseFilterGlobal, UseMiddleware } from 'vona-module-a-aspect';
+import { UseFilterGlobal, UseGuardGlobal, UseMiddleware, UseMiddlewareGlobal } from 'vona-module-a-aspect';
 import { Transaction } from 'vona-module-a-database';
 import { Gate } from 'vona-module-a-core';
 
@@ -11,14 +11,14 @@ import { Gate } from 'vona-module-a-core';
 export class ControllerOnion extends BeanBase {
   @Get('/')
   @UseMiddleware('a-database:transaction')
-  // @UseGuardGlobal('home-base:user', { public: true })
+  // @UseGuardGlobal('a-core:user', { public: true })
   index() {
     //return 'Hello Vona';
   }
 
   @Get('//echo')
-  // @UseGuardGlobal('home-base:user', { public: true })
-  //@UseMiddlewareGlobal('a-core:gate', { gate: { mode: 'local' } })
+  @UseGuardGlobal('a-core:user', { public: true })
+  @UseMiddlewareGlobal('a-core:gate', { gate: { mode: 'local' } })
   @Gate({ gate: { mode: 'local' } })
   @UseMiddleware('a-database:transaction', { isolationLevel: 'serializable', readOnly: true })
   @Transaction({ isolationLevel: 'read committed', readOnly: false })
@@ -31,7 +31,7 @@ export class ControllerOnion extends BeanBase {
   }
 
   @Get('echo2/:userId/:userName')
-  // @UseGuardGlobal('home-base:user', { public: true })
+  // @UseGuardGlobal('a-core:user', { public: true })
   //async echo2(@Query(v.object(DtoUser, { passthrough: false, strict: false })) book: Partial<DtoUser>) {
   async echo2(@Query(DtoUser) book: Partial<DtoUser>) {
     //const ctx = this.app.currentContext;
@@ -40,7 +40,7 @@ export class ControllerOnion extends BeanBase {
   }
 
   @Get('echo3/:userId')
-  // @UseGuardGlobal('home-base:user', { public: true })
+  // @UseGuardGlobal('a-core:user', { public: true })
   async echo3(@Query('id', v.optional()) id: number) {
     //this.scope.util.combineApiPath
     console.log(this.ctx.path);
@@ -50,14 +50,14 @@ export class ControllerOnion extends BeanBase {
   }
 
   @Post('echo4')
-  // @UseGuardGlobal('home-base:user', { public: true })
+  // @UseGuardGlobal('a-core:user', { public: true })
   @UseFilterGlobal('a-core:error', { enable: true, logs: { 422: true } })
   async echo4(@Body(v.array(DtoUser)) users: DtoUser[]) {
     return users;
   }
 
   @Get('echo5')
-  // @UseGuardGlobal('home-base:user', { public: true })
+  // @UseGuardGlobal('a-core:user', { public: true })
   async echo5(@Query('ids', v.default([1]), v.array(Number, { separator: '-' })) ids: number[]) {
     //const ctx = this.app.currentContext;
     //console.log(ctx === this.ctx);
