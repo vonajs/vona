478a73cea2128b15e6668bb943ff554b2b8419b3
zhennann <zhen.nann@icloud.com>
2024-12-20 21:40:28 +0800

    Update bean.user.ts

--- src/suite/a-home/modules/home-user/src/bean/bean.user.ts
+++ src/suite/a-home/modules/home-user/src/bean/bean.user.ts
@@ -1,5 +1,37 @@
 import { Bean } from 'vona-module-a-bean';
-import { BeanUserBase } from 'vona-module-a-core';
+import { BeanUserBase, IUserBase } from 'vona-module-a-core';
+import { TableIdentity } from 'vona-module-a-database';
+
+export interface IUser extends IUserBase {
+  id: number;
+  name: string;
+  avatar?: string;
+}
+
+const userAdmin: IUser = {
+  id: 1,
+  name: 'admin',
+};
 
 @Bean()
-export class BeanUser extends BeanUserBase {}
+export class BeanUser extends BeanUserBase {
+  public get isAuthenticated(): boolean {
+    return this.current !== undefined && TableIdentity.isValid(this.userId);
+  }
+
+  public get current(): IUser | undefined {
+    return userAdmin;
+  }
+
+  public get userId(): TableIdentity {
+    return userAdmin.id;
+  }
+
+  public get userName(): string {
+    return userAdmin.name;
+  }
+
+  public get userAvatar(): string | undefined {
+    return userAdmin.avatar;
+  }
+}
