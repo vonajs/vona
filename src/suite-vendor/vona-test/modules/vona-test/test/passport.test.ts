8014325508679113b9874a4a4b990707e5b33b19
zhennann <zhen.nann@icloud.com>
2024-12-21 16:28:47 +0800

    Create passport.test.ts

---
+++ src/suite-vendor/vona-test/modules/vona-test/test/passport.test.ts
@@ -0,0 +1,13 @@
+import assert from 'assert';
+import { app } from 'vona-mock';
+
+describe('passport.test.ts', () => {
+  it('action:passport', async () => {
+    await app.bean.executor.mockCtx(async () => {
+      await app.bean.passport.signinMock();
+      assert.equal(app.bean.passport.isAuthenticated, true);
+      await app.bean.passport.signout();
+      assert.equal(app.bean.passport.isAuthenticated, false);
+    });
+  });
+});
