11889024afc0284f36da6910b4824fe5b63ad10e
zhennann <zhen.nann@icloud.com>
2024-12-21 10:55:29 +0800

    feat: update

--- packages-vona/vona/src/lib/core/resource.ts
+++ packages-vona/vona/src/lib/core/resource.ts
@@ -29,10 +29,6 @@ export class AppResource extends BeanSimple {
     let { module, scene, name, beanClass, virtual } = options;
     // name
     name = this._parseBeanName(beanClass!, scene, name);
-    // todo: remove
-    if (scene === ('bean' as any) && name === 'base2') {
-      name = 'base';
-    }
     // module
     if (!module) throw new Error(`module name not parsed for bean: ${scene}.${name}`);
     // beanFullName
