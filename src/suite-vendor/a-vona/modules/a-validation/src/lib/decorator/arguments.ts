0aab5dcdbebea9a7620e873823054aa2ee87ac83
zhennann <zhen.nann@icloud.com>
2024-12-21 17:17:28 +0800

    Update arguments.ts

--- src/suite-vendor/a-vona/modules/a-validation/src/lib/decorator/arguments.ts
+++ src/suite-vendor/a-vona/modules/a-validation/src/lib/decorator/arguments.ts
@@ -1,6 +1,13 @@
 import { SchemaLike } from '../types/decorator.js';
 import { createPipesArgumentDecorator } from './pipesArgument.js';
 
+export function Param(): ParameterDecorator;
+export function Param(...schemaLikes: SchemaLike[]): ParameterDecorator;
+export function Param(property: string, ...schemaLikes: SchemaLike[]): ParameterDecorator;
+export function Param(property?: string | SchemaLike, ...schemaLikes: SchemaLike[]): ParameterDecorator {
+  return createPipesArgumentDecorator('param')(property, ...schemaLikes);
+}
+
 export function Query(): ParameterDecorator;
 export function Query(...schemaLikes: SchemaLike[]): ParameterDecorator;
 export function Query(property: string, ...schemaLikes: SchemaLike[]): ParameterDecorator;
