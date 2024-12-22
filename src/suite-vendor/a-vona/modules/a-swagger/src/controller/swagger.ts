ab804fa322fb8de89d7e0ea54a6bfa7c4edac3f8
zhennann <zhen.nann@icloud.com>
2024-12-22 10:51:54 +0800

    Update swagger.ts

--- src/suite-vendor/a-vona/modules/a-swagger/src/controller/swagger.ts
+++ src/suite-vendor/a-vona/modules/a-swagger/src/controller/swagger.ts
@@ -2,12 +2,35 @@ import { BeanBase } from 'vona';
 import { Public } from 'vona-module-a-user';
 import { Controller, Get, Res } from 'vona-module-a-web';
 
+const __SWAGGER_HTML__ = `<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+  <meta name="description" content="SwaggerUI" />
+  <title>SwaggerUI</title>
+  <link rel="stylesheet" href="https://unpkg.com/swagger-ui-dist@5.11.0/swagger-ui.css" />
+</head>
+<body>
+<div id="swagger-ui"></div>
+<script src="https://unpkg.com/swagger-ui-dist@5.11.0/swagger-ui-bundle.js" crossorigin></script>
+<script>
+  window.onload = () => {
+    window.ui = SwaggerUIBundle({
+      url: 'https://petstore3.swagger.io/api/v3/openapi.json',
+      dom_id: '#swagger-ui',
+    });
+  };
+</script>
+</body>
+</html>`;
+
 @Controller('//swagger')
 export class ControllerSwagger extends BeanBase {
   @Get()
   @Public()
   @Res.contentType('text/html')
   index() {
-    return 'Hello Swagger';
+    return __SWAGGER_HTML__;
   }
 }
