a252486565c153390e29f0c80a16ebcefea14d4b
zhennann <zhen.nann@icloud.com>
2024-12-23 15:11:16 +0800

    Update passport.ts

--- src/suite-vendor/vona-test/modules/vona-test/src/controller/passport.ts
+++ src/suite-vendor/vona-test/modules/vona-test/src/controller/passport.ts
@@ -1,10 +1,11 @@
 import assert from 'assert';
 import { BeanBase } from 'vona';
 import { userName, type IUserBase } from 'vona-module-a-user';
-import { Param, User } from 'vona-module-a-openapi';
+import { Api, Param, User } from 'vona-module-a-openapi';
 import { Controller, Get } from 'vona-module-a-web';
 
 @Controller('passport')
+@Api.exclude()
 export class ControllerPassport extends BeanBase {
   @Get('echo/:name')
   echo(@Param('name') name: string, @User() user: IUserBase) {
