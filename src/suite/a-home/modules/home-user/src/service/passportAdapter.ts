daad7132e0d2b1ab6f1831dcd0ac9a233384d24c
zhennann <zhen.nann@icloud.com>
2024-12-24 15:11:48 +0800

    Update passportAdapter.ts

--- src/suite/a-home/modules/home-user/src/service/passportAdapter.ts
+++ src/suite/a-home/modules/home-user/src/service/passportAdapter.ts
@@ -16,7 +16,23 @@ export class ServicePassportAdapter extends BeanBase implements IPassportAdapter
   }
 
   async getUser(user: Partial<IUser>): Promise<IUserBase | undefined> {
-    if (user.id !== undefined) return __UsersDemo.find(item => item.id === user.id);
-    if (user.name !== undefined) return __UsersDemo.find(item => item.name === user.name);
+    const usersDemo = await this._getUsersDemo();
+    if (!usersDemo) return;
+    if (user.id !== undefined) return usersDemo.find(item => item.id === user.id);
+    if (user.name !== undefined) return usersDemo.find(item => item.name === user.name);
+  }
+
+  private async _getUsersDemo() {
+    if (this.app.meta.isProd) return;
+    const redis = this.bean.redis.get('cache');
+    const key = this._getCacheKey('usersDemo');
+    const usersDemo = await redis.get(key);
+    if (usersDemo) return JSON.parse(usersDemo);
+    await redis.set(key, JSON.stringify(__UsersDemo));
+    return __UsersDemo;
+  }
+
+  private _getCacheKey(name: string) {
+    return `${this.ctx.instance ? this.ctx.instance.id : 0}:a-startup:${name}`;
   }
 }
