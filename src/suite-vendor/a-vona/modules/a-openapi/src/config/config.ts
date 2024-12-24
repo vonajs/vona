ed282e95192c85453ed6c711f0527af77b8c3b42
zhennann <zhen.nann@icloud.com>
2024-12-24 11:06:18 +0800

    Update config.ts

--- src/suite-vendor/a-vona/modules/a-openapi/src/config/config.ts
+++ src/suite-vendor/a-vona/modules/a-openapi/src/config/config.ts
@@ -1,15 +1,27 @@
-import { OpenAPIObjectConfig } from '@asteasolutions/zod-to-openapi/dist/v3.0/openapi-generator.js';
+import { OpenAPIObjectConfig as OpenAPIObjectConfigV30 } from '@asteasolutions/zod-to-openapi/dist/v3.0/openapi-generator.js';
+import { OpenAPIObjectConfigV31 } from '@asteasolutions/zod-to-openapi/dist/v3.1/openapi-generator.js';
 import { VonaApplication } from 'vona';
 
 export const config = (_app: VonaApplication) => {
   return {
+    defaultVersion: '31',
     generateDocument: {
-      openapi: '3.0.0',
-      info: {
-        version: '5.0.0',
-        title: 'Vona',
-        description: 'Vona API',
-      },
-    } as OpenAPIObjectConfig,
+      30: {
+        openapi: '3.0.0',
+        info: {
+          version: '5.0.0',
+          title: 'Vona',
+          description: 'Vona API',
+        },
+      } as OpenAPIObjectConfigV30,
+      31: {
+        openapi: '3.1.0',
+        info: {
+          version: '5.0.0',
+          title: 'Vona',
+          description: 'Vona API',
+        },
+      } as OpenAPIObjectConfigV31,
+    },
   };
 };
