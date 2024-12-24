de51f5e6a0d7e65323eb684136479c48c30ad118
zhennann <zhen.nann@icloud.com>
2024-12-24 12:00:54 +0800

    Update swagger.ts

--- src/suite-vendor/a-cabloy/modules/a-swagger/src/controller/swagger.ts
+++ src/suite-vendor/a-cabloy/modules/a-swagger/src/controller/swagger.ts
@@ -3,28 +3,6 @@ import { Api, Query, TypeOpenApiVersion, v } from 'vona-module-a-openapi';
 import { Public } from 'vona-module-a-user';
 import { apiPath, Controller, Get } from 'vona-module-a-web';
 
-// const __SWAGGER_HTML__ = `<!DOCTYPE html>
-// <html lang="en">
-// <head>
-//   <meta charset="utf-8" />
-//   <meta name="viewport" content="width=device-width, initial-scale=1" />
-//   <meta name="description" content="SwaggerUI" />
-//   <title>SwaggerUI</title>
-//   <link rel="stylesheet" href="https://unpkg.com/swagger-ui-dist@5.11.0/swagger-ui.css" />
-// </head>
-// <body>
-// <div id="swagger-ui"></div>
-// <script src="https://unpkg.com/swagger-ui-dist@5.11.0/swagger-ui-bundle.js" crossorigin></script>
-// <script>
-//   window.onload = () => {
-//     window.ui = SwaggerUIBundle({
-//       url: '__SWAGGER_JSON__',
-//       dom_id: '#swagger-ui',
-//     });
-//   };
-// </script>
-// </body>
-// </html>`;
 const __SWAGGER_HTML__ = `<!DOCTYPE html>
 <html lang="en">
 <head>
