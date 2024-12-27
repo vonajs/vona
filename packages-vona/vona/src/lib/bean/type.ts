f6655133fa81d37c29b0e77d9c0eb2c39e4168bd
zhennann <zhen.nann@icloud.com>
2024-12-27 10:03:56 +0800

    Update type.ts

--- packages-vona/vona/src/lib/bean/type.ts
+++ packages-vona/vona/src/lib/bean/type.ts
@@ -1,11 +1,14 @@
 /** bean merge: bean.instance */
 
+import { IBeanSceneRecord } from '../decorator/interface/beanOptions.js';
+
 export interface IBeanRecordGlobal {}
 export interface IBeanRecordGeneral {}
-export type TypeBeanRecordGeneralSelector<SCENE extends string> = {
+export type TypeBeanRecordGeneralSelector<SCENE extends keyof IBeanSceneRecord> = {
   [K in keyof IBeanRecordGeneral as K extends `${string}.${SCENE}.${string}` ? K : never]: IBeanRecordGeneral[K];
 };
-export type TypeBeanRecordGeneralSelectorKeys<SCENE extends string> = keyof TypeBeanRecordGeneralSelector<SCENE>;
+export type TypeBeanRecordGeneralSelectorKeys<SCENE extends keyof IBeanSceneRecord> =
+  keyof TypeBeanRecordGeneralSelector<SCENE>;
 
 export type IBeanRecord = IBeanRecordGlobal & IBeanRecordGeneral;
 export type TypeBeanRecordKeys = keyof IBeanRecord;
