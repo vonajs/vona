75ef0b6e2cabdb7edaa22a366510b3fc5939bc5c
zhennann <zhen.nann@icloud.com>
2024-12-28 20:23:28 +0800

    feat: update

--- src/suite-vendor/a-cabloy/modules/a-swagger/src/bean/meta.printApiPath.ts
+++ src/suite-vendor/a-cabloy/modules/a-swagger/src/bean/meta.printApiPath.ts
@@ -1,10 +1,16 @@
 import { BeanBase } from 'vona';
 import { Meta } from 'vona-module-a-meta';
 import { IMetaPrintApiPathExecute, IMetaPrintApiPathInfo } from 'vona-module-a-printapipath';
+import { apiPath } from 'vona-module-a-web';
 
 @Meta()
 export class MetaPrintApiPath extends BeanBase implements IMetaPrintApiPathExecute {
   async execute(): Promise<IMetaPrintApiPathInfo | IMetaPrintApiPathInfo[]> {
-    return [{ title: 'swagger api ' }];
+    // apiPath
+    const _apiPath = this.scope.util.combineApiPath(apiPath('//swagger'));
+    return [
+      { title: 'swagger', path: `http://localhost:${process.env.SERVER_LISTEN_PORT}${_apiPath}` },
+      { title: 'swagger30', path: `http://localhost:${process.env.SERVER_LISTEN_PORT}${_apiPath}?version=30` },
+    ];
   }
 }
