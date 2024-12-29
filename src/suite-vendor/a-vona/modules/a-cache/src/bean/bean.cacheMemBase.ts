5ffe80136feccd61f2e035c3bff3dca910ec9250
zhennann <zhen.nann@icloud.com>
2024-12-29 15:09:44 +0800

    Update bean.cacheMemBase.ts

--- src/suite-vendor/a-vona/modules/a-cache/src/bean/bean.cacheMemBase.ts
+++ src/suite-vendor/a-vona/modules/a-cache/src/bean/bean.cacheMemBase.ts
@@ -58,49 +58,66 @@ export class BeanCacheMemBase<KEY = any, DATA = any> extends BeanBase {
     return layered.lruCache;
   }
 
-  private __getKeyHash(key: KEY): string {
+  private __getKeyHash(key?: KEY): string {
     const cache = this.__cacheInstance;
-    if (!cache) return '';
-    return cast(cache).__getKeyHash(key);
+    if (!cache) throw new Error('cache not enabled');
+    return cast(cache).__getKeyHash(key ?? 'default');
   }
 
-  public get(key: KEY): DATA | undefined {
+  public get(key?: KEY): DATA | undefined {
     const cacheMem = this.__cacheInstanceMem;
     if (!cacheMem) return undefined;
     const keyHash = this.__getKeyHash(key);
     return cacheMem.get(keyHash);
   }
 
-  public set(key: KEY, value: DATA, timeout?: number) {
+  public set(value: DATA): void;
+  public set(key: KEY | undefined, value: DATA): void;
+  public set(key?: KEY | DATA, value?: DATA): void {
     const cacheMem = this.__cacheInstanceMem;
     if (!cacheMem) return;
+    if (arguments.length === 1) {
+      value = key as DATA;
+      key = undefined;
+    } else {
+      key = key as KEY;
+    }
     const keyHash = this.__getKeyHash(key);
-    cacheMem.set(keyHash, value, { ttl: timeout || 0 });
+    cacheMem.set(keyHash, value);
   }
 
-  public getset(key: KEY, value: DATA, timeout?: number) {
+  public getset(value: DATA): DATA | undefined;
+  public getset(key: KEY, value: DATA): DATA | undefined;
+  public getset(key?: KEY | DATA, value?: DATA): DATA | undefined {
     const cacheMem = this.__cacheInstanceMem;
     if (!cacheMem) return;
+    if (arguments.length === 1) {
+      value = key as DATA;
+      key = undefined;
+    } else {
+      key = key as KEY;
+      value = value as DATA;
+    }
     const valueOld = this.get(key);
-    this.set(key, value, timeout);
+    this.set(key, value);
     return valueOld;
   }
 
-  has(key: KEY) {
+  has(key?: KEY): boolean {
     const cacheMem = this.__cacheInstanceMem;
     if (!cacheMem) return false;
     const keyHash = this.__getKeyHash(key);
     return cacheMem.has(keyHash);
   }
 
-  remove(key: KEY) {
+  remove(key?: KEY): void {
     const cache = this.__cacheInstance;
     if (!cache) return;
     // del on this worker + broadcast
-    cache.del(key);
+    cache.del(key ?? ('default' as any));
   }
 
-  clear() {
+  clear(): void {
     const cache = this.__cacheInstance;
     if (!cache) return;
     // del on this worker + broadcast
