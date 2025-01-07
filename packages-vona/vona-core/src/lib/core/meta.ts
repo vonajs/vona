a3c1e525281e46b8c6d7625fcd809e0693c549fa
zhennann <zhen.nann@icloud.com>
2025-01-07 15:36:52 +0800

    Update meta.ts

--- packages-vona/vona-core/src/lib/core/meta.ts
+++ packages-vona/vona-core/src/lib/core/meta.ts
@@ -54,7 +54,7 @@ export class AppMeta extends BeanSimple {
     this.locale = this.bean._newBean(AppLocale);
 
     // text
-    this.text = this.locale.createLocaleText();
+    this.app.text = this.text = this.locale.createLocaleText();
 
     // scopeContainer
     this.scopeContainer = this.bean._newBean(BeanScopeContainer);
