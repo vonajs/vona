733ce79083f6d000c72839701ec632acea6f1739
zhennann <zhen.nann@icloud.com>
2025-02-09 15:54:55 +0800

    feat: remove cluster reload

--- packages-vona/vona-core/src/index.ts
+++ packages-vona/vona-core/src/index.ts
@@ -1,10 +1,6 @@
 import '@cabloy/set';
 import '@cabloy/json5';
 import 'regenerator-runtime';
-import Reload from '@cabloy/cluster-reload';
-import Master from 'egg-cluster/lib/master.js';
-
-Master.prototype.onReload = Reload;
 
 export * from './lib/index.js';
 export * from './types/index.js';
