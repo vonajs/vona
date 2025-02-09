8e6f8b8e19014a012b02270b268933f68e19e8be
zhennann <zhen.nann@icloud.com>
2025-02-09 13:36:41 +0800

    feat: elect

--- src/suite-vendor/a-vona/modules/a-development/src/bean/meta.election.ts
+++ src/suite-vendor/a-vona/modules/a-development/src/bean/meta.election.ts
@@ -2,7 +2,7 @@ import { Meta } from 'vona-module-a-meta';
 import { BeanElectionBase, IElectionElectOptions } from 'vona-module-a-election';
 
 export interface MetaElection {
-  elect(resource: 'development', fn: Function, options?: IElectionElectOptions);
+  elect(resource: 'development', fn: Function, options?: IElectionElectOptions): void;
 }
 
 @Meta()
