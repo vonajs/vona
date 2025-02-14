9000d7f121eafc977bc01ac9a33efb8aae7898cb
zhennann <zhen.nann@icloud.com>
2025-02-14 18:06:40 +0800

    feat: eslint

--- packages-utils/lint/common/rules.js
+++ packages-utils/lint/common/rules.js
@@ -1,3 +1,6 @@
 export const rules = {
   'prefer-promise-reject-errors': 'off',
+  '@stylistic/max-len': ['error', { code: 120, tabWidth: 2 }],
+  '@stylistic/arrow-parens': ['error', 'as-needed'],
+  '@stylistic/brace-style': ['error', '1tbs', { allowSingleLine: true }],
 };
