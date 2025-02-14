84f672a74313a12b001190e45d01e3496a6c8aaf
zhennann <zhen.nann@icloud.com>
2025-02-14 17:45:40 +0800

    Update eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,10 +3,10 @@ import EslintConfigs from '@cabloy/lint/api/eslint.js';
 export default [
   ...EslintConfigs,
   {
-      files: ['**/*.ts', '**/*.tsx'],
-      ignores: [
-        'node_modules',
-        'dist',
-      ],
-  }
-]
+    files: ['**/*.ts', '**/*.tsx'],
+    ignores: [
+      'node_modules',
+      'dist',
+    ],
+  },
+];
