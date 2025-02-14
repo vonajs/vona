0952985a0fa4d5ae726ae47e96c3da7abe9905d5
zhennann <zhen.nann@icloud.com>
2025-02-14 20:47:33 +0800

    Update rules.js

--- packages-utils/lint/common/rules.js
+++ packages-utils/lint/common/rules.js
@@ -1,9 +1,33 @@
 export const rules = {
   'prefer-promise-reject-errors': 'off',
-  'no-console': 'error',
+  'no-console': 'warn',
+  'no-debugger': 'warn',
+  'prefer-const': [
+    'error',
+    {
+      destructuring: 'all',
+      ignoreReadBeforeAssign: true,
+    },
+  ],
+  'no-undef': [
+    'error',
+    {
+      typeof: false,
+    },
+  ],
   '@typescript-eslint/consistent-type-imports': ['error', { prefer: 'type-imports' }],
   '@typescript-eslint/explicit-function-return-type': 'off',
-  '@stylistic/max-len': ['error', { code: 130, tabWidth: 2 }],
+  '@typescript-eslint/no-namespace': 'off',
+  '@typescript-eslint/no-explicit-any': 'off',
+  '@stylistic/max-len': ['error', {
+    code: 130, tabWidth: 2,
+    ignoreComments: true,
+    ignoreTrailingComments: true,
+    ignoreUrls: true,
+    ignoreStrings: true,
+    ignoreTemplateLiterals: true,
+    ignoreRegExpLiterals: true,
+  }],
   '@stylistic/arrow-parens': ['error', 'as-needed'],
   '@stylistic/brace-style': ['error', '1tbs', { allowSingleLine: true }],
   '@stylistic/quotes': ['error', 'single', { avoidEscape: true }],
