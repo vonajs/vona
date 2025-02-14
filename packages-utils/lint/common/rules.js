3ca0a0d18e97c96c25fa4c3a142463dec4ba90e1
zhennann <zhen.nann@icloud.com>
2025-02-14 20:52:30 +0800

    feat: eslint

--- packages-utils/lint/common/rules.js
+++ packages-utils/lint/common/rules.js
@@ -15,10 +15,79 @@ export const rules = {
       typeof: false,
     },
   ],
+  'array-bracket-spacing': ['error', 'never'],
+  'no-empty': [
+    'error',
+    {
+      allowEmptyCatch: true,
+    },
+  ],
+  'no-empty-function': [
+    'error',
+    {
+      allow: [
+        'functions',
+        'arrowFunctions',
+        'generatorFunctions',
+        'methods',
+        'generatorMethods',
+        'getters',
+        'setters',
+        'constructors',
+        'asyncFunctions',
+        'asyncMethods',
+      ],
+    },
+  ],
+  'no-constant-condition': [
+    'error',
+    {
+      checkLoops: false,
+    },
+  ],
+  'one-var-declaration-per-line': [0],
+  'space-before-function-paren': [0],
+  'generator-star-spacing': [0],
+  'newline-per-chained-call': [0],
+  'prefer-rest-params': 'off',
+  // 'no-setter-return': 'off', // no effect for typescript check
   '@typescript-eslint/consistent-type-imports': ['error', { prefer: 'type-imports' }],
   '@typescript-eslint/explicit-function-return-type': 'off',
   '@typescript-eslint/no-namespace': 'off',
   '@typescript-eslint/no-explicit-any': 'off',
+  '@typescript-eslint/no-var-requires': 'off',
+  '@typescript-eslint/no-unused-vars': [
+    'error',
+    {
+      args: 'all',
+      argsIgnorePattern: '^_',
+      caughtErrors: 'all',
+      caughtErrorsIgnorePattern: '^_',
+      destructuredArrayIgnorePattern: '^_',
+      varsIgnorePattern: '^_',
+      ignoreRestSiblings: true,
+    },
+  ],
+  '@typescript-eslint/no-this-alias': 'off',
+  '@typescript-eslint/no-unsafe-declaration-merging': 'off',
+  '@typescript-eslint/ban-ts-comment': [
+    'error',
+    {
+      'ts-expect-error': 'allow-with-description',
+      'ts-ignore': 'allow-with-description',
+    },
+  ],
+  '@typescript-eslint/ban-types': [
+    'error',
+    {
+      types: {
+        // un-ban a type that's banned by default
+        '{}': false,
+        'Function': false,
+      },
+      extendDefaults: true,
+    },
+  ],
   '@stylistic/max-len': ['error', {
     code: 130, tabWidth: 2,
     ignoreComments: true,
--- packages-utils/lint/common/rulesVue.js
+++ packages-utils/lint/common/rulesVue.js
@@ -1,4 +1,4 @@
 export const rulesVue = {
   'vue/multi-word-component-names': 'off',
   'vue/prefer-import-from-vue': 'off',
-}
+};
