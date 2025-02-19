34c4ecec942d017f8e58b29ab91a3b5969dd7b87
zhennann <zhen.nann@icloud.com>
2025-02-19 22:43:15 +0800

    feat: update

--- packages-utils/module-info-pro/dist/index.d.ts
+++
@@ -1,5 +0,0 @@
-import { IModuleInfo } from '@cabloy/module-info';
-export declare const ParseModuleNameLevelInit = 1;
-export declare function parseModuleName(level?: number): string | undefined;
-export declare function parseModuleInfo(level?: number): IModuleInfo | undefined;
-//# sourceMappingURL=index.d.ts.map
--- packages-utils/module-info/dist/index.d.ts
+++
@@ -1,6 +0,0 @@
-export * from './interface.js';
-export * from './onionScenesMeta.js';
-export * from './utils.js';
-export * from './vona.js';
-export * from './zova.js';
-//# sourceMappingURL=index.d.ts.map
