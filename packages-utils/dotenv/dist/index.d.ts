cc4d258757b70082258c4248c389809e3f7c555e
zhennann <zhen.nann@icloud.com>
2025-02-19 22:40:34 +0800

    feat: update

--- packages-utils/dotenv/dist/index.d.ts
+++
@@ -1,4 +0,0 @@
-import type { DotenvParseOutput } from 'dotenv';
-export declare function loadEnvs(meta: object, dir: string, prefix?: string, postfix?: string): DotenvParseOutput | undefined;
-export declare function metaToScope(meta: object): {};
-export declare function getEnvFiles(meta: object, dir: string, prefix: string, postfix?: string): string[] | undefined;
