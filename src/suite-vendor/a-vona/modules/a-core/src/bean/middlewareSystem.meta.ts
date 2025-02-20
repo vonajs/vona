ff15e0be77cc039a0fdf6c2e9dbb383008629623
zhennann <zhen.nann@icloud.com>
2025-02-20 15:46:06 +0800

    Update middlewareSystem.meta.ts

--- src/suite-vendor/a-vona/modules/a-core/src/bean/middlewareSystem.meta.ts
+++ src/suite-vendor/a-vona/modules/a-core/src/bean/middlewareSystem.meta.ts
@@ -12,12 +12,14 @@ export interface IMiddlewareSystemOptionsMeta extends IDecoratorMiddlewareSystem
 export class MiddlewareSystemMeta extends BeanBase implements IMiddlewareSystemExecute {
   async execute(options: IMiddlewareSystemOptionsMeta, next: Next) {
     const ctx = this.ctx;
-
     if (options.logging) {
+      // todo: log
       ctx.coreLogger.info('[meta] request started, host: %s, user-agent: %s', ctx.host, ctx.header['user-agent']);
     }
+    // start
+    ctx.starttime = Date.now();
     await next();
-    // total response time header
+    // end
     if (ctx.performanceStarttime) {
       ctx.set('x-readtime', (Math.floor((performance.now() - ctx.performanceStarttime) * 1000) / 1000).toString());
     } else {
