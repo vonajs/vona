8dd09e2c8d04faaa4667448df43371aa1aa8a8ac
zhennann <zhen.nann@icloud.com>
2025-02-22 16:43:49 +0800

    Update cli.bin.build.ts

--- packages-cli/cli-set-api/src/lib/bean/cli.bin.build.ts
+++ packages-cli/cli-set-api/src/lib/bean/cli.bin.build.ts
@@ -1,4 +1,7 @@
+import type { VonaConfigMeta, VonaMetaFlavor, VonaMetaMode } from '@cabloy/module-info';
+import type { VonaBinConfigOptions } from './toolsBin/types.ts';
 import { BeanCliBase } from '@cabloy/cli';
+import { generateVonaMeta } from './toolsBin/generateVonaMeta.ts';
 
 declare module '@cabloy/cli' {
   interface ICommandArgv {
@@ -12,42 +15,19 @@ export class CliBinBuild extends BeanCliBase {
     // super
     await super.execute();
     const projectPath = argv.projectPath;
-    const force = argv.force;
-    await this._build(projectPath, force);
+    await this._build(projectPath);
   }
 
-  async _build(_projectPath: string, force: boolean) {
-    // count
-    const count =
-      Object.keys(this.modulesMeta.suites).length + this.modulesMeta.modulesArray.filter(item => !item.suite).length;
-    // begin
-    let counter = 0;
-    const timeBegin = new Date();
-    // eslint-disable-next-line
-    console.log('===> build begin');
-    // args
-    const tscArgs = ['-b'];
-    if (force) {
-      tscArgs.push('--force');
-    }
-    // suites
-    for (const key in this.modulesMeta.suites) {
-      const suite = this.modulesMeta.suites[key];
-      // eslint-disable-next-line
-      console.log(`===>  ${++counter}/${count} suite: ${suite.info.originalName}`);
-      await this.helper.processHelper.tsc(tscArgs, { cwd: suite.root });
-    }
-    // modules
-    for (const module of this.modulesMeta.modulesArray) {
-      if (!module.suite) {
-        // eslint-disable-next-line
-        console.log(`===>  ${++counter}/${count} module: ${module.info.originalName}`);
-        await this.helper.processHelper.tsc(tscArgs, { cwd: module.root });
-      }
-    }
-    // end
-    const timeEnd = new Date();
-    // eslint-disable-next-line
-    console.log(`===> build end: ${(timeEnd.valueOf() - timeBegin.valueOf()) / 1000}s`);
+  async _build(projectPath: string) {
+    const { argv } = this.context;
+    const mode: VonaMetaMode = 'prod';
+    const flavor: VonaMetaFlavor = argv.flavor || 'normal';
+    const configMeta: VonaConfigMeta = { flavor, mode };
+    const configOptions: VonaBinConfigOptions = {
+      appDir: projectPath,
+      runtimeDir: '.vona',
+      workers: argv.workers,
+    };
+    await generateVonaMeta(configMeta, configOptions);
   }
 }
