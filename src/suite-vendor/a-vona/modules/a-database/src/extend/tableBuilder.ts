0ed3e6e2731a94b6eb5d86db6f8bb0c5b62f0495
zhennann <zhen.nann@icloud.com>
2025-02-26 10:12:04 +0800

    Update tableBuilder.ts

--- src/suite-vendor/a-vona/modules/a-database/src/extend/tableBuilder.ts
+++ src/suite-vendor/a-vona/modules/a-database/src/extend/tableBuilder.ts
@@ -1,5 +1,6 @@
 import type { VonaApplication } from 'vona';
 import knex from 'knex';
+import TableBuilder from 'knex/lib/schema/tablebuilder.js';
 
 export interface IBasicFieldsOptions {
   id?: boolean;
@@ -9,6 +10,7 @@ export interface IBasicFieldsOptions {
 }
 
 export function ExtendTableBuilder(_app: VonaApplication) {
+  delete TableBuilder.prototype.basicFields;
   knex.TableBuilder.extend('basicFields', function (options?: IBasicFieldsOptions) {
     options = options || ({} as IBasicFieldsOptions);
     if (options.id !== false) this.increments();
@@ -19,26 +21,32 @@ export function ExtendTableBuilder(_app: VonaApplication) {
     if (options.iid !== false) this.integer('iid').defaultTo(0);
     return this;
   });
+  delete TableBuilder.prototype.id0;
   knex.TableBuilder.extend('id0', function (columnName) {
     return this.integer(columnName).defaultTo(0);
   });
+  delete TableBuilder.prototype.int0;
   knex.TableBuilder.extend('int0', function (columnName) {
     return this.integer(columnName).defaultTo(0);
   });
+  delete TableBuilder.prototype.int1;
   knex.TableBuilder.extend('int1', function (columnName) {
     return this.integer(columnName).defaultTo(1);
   });
   ['atomId', 'itemId', 'userId', 'atomClassId', 'atomIdMain', 'atomClassIdMain'].forEach(method => {
+    delete TableBuilder.prototype[method];
     knex.TableBuilder.extend(method, function () {
       return this.integer(method).defaultTo(0);
     });
   });
   ['description'].forEach(method => {
+    delete TableBuilder.prototype[method];
     knex.TableBuilder.extend(method, function (length = 255) {
       return this.string(method, length);
     });
   });
   ['content'].forEach(method => {
+    delete TableBuilder.prototype[method];
     knex.TableBuilder.extend(method, function (useText) {
       return useText ? this.text(method) : this.json(method);
     });
