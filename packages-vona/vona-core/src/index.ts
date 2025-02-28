09848a857747762e38074a388faf14ace924143f
zhennann <zhen.nann@icloud.com>
2025-02-28 09:51:55 +0800

    feat: update

--- packages-vona/vona-core/src/index.ts
+++ packages-vona/vona-core/src/index.ts
@@ -1,6 +1,5 @@
 import '@cabloy/set';
 import '@cabloy/json5';
-import 'regenerator-runtime';
 
 export * from './lib/index.ts';
 export * from './types/index.ts';
--- packages-vona/vona/src/index.ts
+++ packages-vona/vona/src/index.ts
@@ -1,2 +1,1 @@
 export * from 'vona-core';
-export * from 'vona-shared';
