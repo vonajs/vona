4b8c7ebc9324e81680e83b6eb8e878ee555b7483
zhennann <zhen.nann@icloud.com>
2025-03-01 22:38:57 +0800

    Update useAopMethod.ts

--- src/suite-vendor/a-vona/modules/a-aspect/src/lib/use/useAopMethod.ts
+++ src/suite-vendor/a-vona/modules/a-aspect/src/lib/use/useAopMethod.ts
@@ -1,9 +1,26 @@
+import type { MetadataKey } from 'vona';
 import type { IAopMethodRecord } from '../../types/aopMethod.ts';
-import { UseOnionBase } from './useOnionBase.ts';
+import { appMetadata, registerMappedClassMetadataKey } from 'vona';
+
+export const SymbolDecoratorUseAopMethod = Symbol('SymbolDecoratorUseAopMethod');
+export interface IUseAopMethodPropMetadata<T extends keyof IAopMethodRecord> {
+  aopMethodName: T;
+  options?: Partial<IAopMethodRecord[T]>;
+}
 
 export function UseAopMethod<T extends keyof IAopMethodRecord>(
   aopMethodName: T,
   options?: Partial<IAopMethodRecord[T]>,
 ): PropertyDescriptor & MethodDecorator {
-  return UseOnionBase('aopMethod', aopMethodName, options);
+  return function (target: object, prop: MetadataKey, descriptor?: PropertyDescriptor) {
+    registerMappedClassMetadataKey(target, SymbolDecoratorUseAopMethod);
+    const uses = appMetadata.getOwnMetadataMap<MetadataKey, IUseAopMethodPropMetadata<T>[]>(
+      true,
+      SymbolDecoratorUseAopMethod,
+      target,
+    );
+    if (!uses[prop]) uses[prop] = [];
+    uses[prop].push({ aopMethodName, options });
+    return descriptor;
+  } as any;
 }
