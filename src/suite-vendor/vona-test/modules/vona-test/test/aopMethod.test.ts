a1fc933862877224b01b4535dd998dec6b37a746
zhennann <zhen.nann@icloud.com>
2025-03-02 10:35:45 +0800

    Update aopMethod.test.ts

--- src/suite-vendor/vona-test/modules/vona-test/test/aopMethod.test.ts
+++ src/suite-vendor/vona-test/modules/vona-test/test/aopMethod.test.ts
@@ -2,7 +2,7 @@ import assert from 'node:assert';
 import { describe, it } from 'node:test';
 import { app } from 'vona-mock';
 
-describe('aopMethod.test.ts', () => {
+describe.only('aopMethod.test.ts', () => {
   it('action:aopMethod', async () => {
     await app.bean.executor.mockCtx(async () => {
       // scope
