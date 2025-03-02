f2a2b420746ab9f27e416b1565ad1f6cf234d398
zhennann <zhen.nann@icloud.com>
2025-03-02 19:47:12 +0800

    Update transaction.ts

--- src/suite-vendor/a-vona/modules/a-database/src/lib/transaction.ts
+++ src/suite-vendor/a-vona/modules/a-database/src/lib/transaction.ts
@@ -1,3 +1,4 @@
+import type { IAopMethodOptionsTransaction } from '../bean/aopMethod.transaction.ts';
 import type { IMiddlewareOptionsTransaction } from '../bean/middleware.transaction.ts';
 import { UseAopMethod, UseMiddleware } from 'vona-module-a-aspect';
 
@@ -5,6 +6,6 @@ export function TransactionMiddleware(options?: Partial<IMiddlewareOptionsTransa
   return UseMiddleware('a-database:transaction', options);
 }
 
-export function Transaction(options?: Partial<IMiddlewareOptionsTransaction>): MethodDecorator {
+export function Transaction(options?: Partial<IAopMethodOptionsTransaction>): MethodDecorator {
   return UseAopMethod('a-database:transaction', options);
 }
