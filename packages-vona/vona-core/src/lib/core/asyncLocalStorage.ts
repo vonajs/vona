07c1c6439c0cd39b1c0156cf77ca0f03d284caf1
zhennann <zhen.nann@icloud.com>
2025-03-04 17:10:46 +0800

    Update asyncLocalStorage.ts

--- packages-vona/vona-core/src/lib/core/asyncLocalStorage.ts
+++ packages-vona/vona-core/src/lib/core/asyncLocalStorage.ts
@@ -9,15 +9,15 @@ export class VonaAsyncLocalStorage<T> extends AsyncLocalStorage<T> {
     this.app = app;
   }
 
-  run<R>(store: T, callback: () => R): R;
-  run<R, TArgs extends any[]>(store: T, callback: (...args: TArgs) => R, ...args: TArgs): R {
+  async run<R>(store: T, callback: () => R): Promise<R>;
+  async run<R, TArgs extends any[]>(store: T, callback: (...args: TArgs) => R, ...args: TArgs): Promise<R> {
     if (store === this.app.currentContext) {
-      return callback(...args);
+      return await callback(...args);
     }
-    return super.run(store, (...args) => {
+    return super.run(store, async (...args) => {
       try {
         this.app.meta.ctxCounter.increment();
-        return callback(...args);
+        return await callback(...args);
       } finally {
         this.app.meta.ctxCounter.decrement();
       }
