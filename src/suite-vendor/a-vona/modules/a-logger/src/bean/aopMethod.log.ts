30d0f8d43d6f395c73262b178522a6c6aad529a2
zhennann <zhen.nann@icloud.com>
2025-03-05 19:37:24 +0800

    Update aopMethod.log.ts

--- src/suite-vendor/a-vona/modules/a-logger/src/bean/aopMethod.log.ts
+++ src/suite-vendor/a-vona/modules/a-logger/src/bean/aopMethod.log.ts
@@ -1,6 +1,7 @@
 import type { ILoggerClientChildRecord, ILoggerClientRecord, LoggerLevel, Next, NextSync } from 'vona';
 import type { IAopMethodExecute, IDecoratorAopMethodOptions } from 'vona-module-a-aspect';
 import type winston from 'winston';
+import { evaluate } from '@cabloy/utils';
 import { BeanAopMethodBase, SymbolBeanFullName } from 'vona';
 import { AopMethod } from 'vona-module-a-aspect';
 
@@ -11,7 +12,7 @@ export interface IAopMethodOptionsLog extends IDecoratorAopMethodOptions {
   auto?: boolean;
   args?: boolean;
   result?: boolean;
-  // context?:Record<
+  context?: Record<string, any>;
 }
 
 @AopMethod<IAopMethodOptionsLog>({
@@ -19,72 +20,85 @@ export interface IAopMethodOptionsLog extends IDecoratorAopMethodOptions {
 })
 export class AopMethodLog extends BeanAopMethodBase implements IAopMethodExecute {
   get(options: IAopMethodOptionsLog, next: NextSync, receiver: any, prop: string): string {
+    const context = this._getContext(options, receiver);
     const message = `${receiver[SymbolBeanFullName]}#get ${prop}`;
     const logger = this.app.meta.logger.child(options.childName, options.clientName);
     // begin
-    (!options.auto) && logger.log(options.level, message);
+    (!options.auto) && logger.log(options.level, message, { context });
     const profiler = logger.startTimer();
     // next
     try {
       const res = next();
-      this._logResult(profiler, res, options, message);
+      this._logResult(profiler, context, res, options, message);
       return res;
     } catch (err: any) {
-      this._logError(profiler, err, options, message);
+      this._logError(profiler, context, err, options, message);
       throw err;
     }
   }
 
   set(options: IAopMethodOptionsLog, value: string, next: NextSync, receiver: any, prop: string): boolean {
+    const context = this._getContext(options, receiver);
     const message = `${receiver[SymbolBeanFullName]}#set ${prop}`;
     const logger = this.app.meta.logger.child(options.childName, options.clientName);
     // begin
-    logger.log(options.level, `${message} value: ${JSON.stringify(value)}`);
+    logger.log(options.level, `${message} value: ${JSON.stringify(value)}`, { context });
     const profiler = logger.startTimer();
     // next
     try {
       const res = next();
-      (!options.auto) && this._logResult(profiler, undefined, options, message);
+      (!options.auto) && this._logResult(profiler, context, undefined, options, message);
       return res;
     } catch (err: any) {
-      this._logError(profiler, err, options, message);
+      this._logError(profiler, context, err, options, message);
       throw err;
     }
   }
 
   execute(options: IAopMethodOptionsLog, _args: [], next: Next | NextSync, receiver: any, prop: string): Promise<any> | any {
+    const context = this._getContext(options, receiver);
     const message = `${receiver[SymbolBeanFullName]}#${prop}`;
     const logger = this.app.meta.logger.child(options.childName, options.clientName);
     // begin
-    options.args !== false && logger.log(options.level, `${message} args: ${JSON.stringify(_args)}`);
+    options.args !== false && logger.log(options.level, `${message} args: ${JSON.stringify(_args)}`, { context });
     const profiler = logger.startTimer();
     // next
     try {
       const res = next();
       if (res?.then) {
         return res.then((res: any) => {
-          options.result !== false && this._logResult(profiler, res, options, message);
+          options.result !== false && this._logResult(profiler, context, res, options, message);
           return res;
         }).catch((err: Error) => {
-          this._logError(profiler, err, options, message);
+          this._logError(profiler, context, err, options, message);
           throw err;
         });
       }
-      options.result !== false && this._logResult(profiler, res, options, message);
+      options.result !== false && this._logResult(profiler, context, res, options, message);
       return res;
     } catch (err: any) {
-      this._logError(profiler, err, options, message);
+      this._logError(profiler, context, err, options, message);
       throw err;
     }
   }
 
-  _logResult(profiler: winston.Profiler, res: any, options: IAopMethodOptionsLog, message: string) {
-    const textResult = res !== undefined ? ` result: ${JSON.stringify(res)}` : '';
-    profiler.done({ level: options.level, message: `${message}${textResult}` });
+  _getContext(options: IAopMethodOptionsLog, receiver: any) {
+    const context = options.context;
+    if (!context) return;
+    const result = {};
+    for (const key in context) {
+      result[key] = evaluate(context[key], receiver);
+    }
+    return result;
   }
 
-  _logError(profiler: winston.Profiler, err: Error, _options: IAopMethodOptionsLog, message: string) {
+  _logResult(profiler: winston.Profiler, context: any, res: any, options: IAopMethodOptionsLog, message: string) {
+    const textResult = res !== undefined ? ` result: ${JSON.stringify(res)}` : '';
+    profiler.done({ level: options.level, message: `${message}${textResult}`, context });
+  }
+
+  _logError(profiler: winston.Profiler, context: any, err: Error, _options: IAopMethodOptionsLog, message: string) {
     const textError = ` error: ${err.message}`;
-    profiler.done({ level: 'error', message: `${message}${textError}` });
+    profiler.done({ level: 'error', message: `${message}${textError}`, context });
   }
 }
