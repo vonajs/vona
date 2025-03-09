8a0c7e0b88b5b926c0407437400895d4873291b4
zhennann <zhen.nann@icloud.com>
2025-03-09 18:39:58 +0800

    Update jwt.ts

--- src/suite-vendor/vona-test/modules/vona-test/src/controller/jwt.ts
+++ src/suite-vendor/vona-test/modules/vona-test/src/controller/jwt.ts
@@ -2,12 +2,14 @@ import type { IJwtToken } from 'vona-module-a-jwt';
 import { BeanBase } from 'vona';
 import { DtoJwtToken } from 'vona-module-a-jwt';
 import { Api, Body, v } from 'vona-module-a-openapi';
+import { Public } from 'vona-module-a-user';
 import { Controller, Post } from 'vona-module-a-web';
 
 @Controller('jwt')
 export class ControllerJwt extends BeanBase {
   @Post('login')
   @Api.body(v.object(DtoJwtToken))
+  @Public()
   async login(@Body('name') name: string): Promise<IJwtToken> {
     const payloadData = await this.bean.passport.signinMock(name);
     const jwt = await this.bean.jwt.create(payloadData);
