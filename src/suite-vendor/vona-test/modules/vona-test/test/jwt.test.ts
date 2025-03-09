b5fec0529937a46645247d6044a3947383801054
zhennann <zhen.nann@icloud.com>
2025-03-09 19:37:32 +0800

    Update jwt.test.ts

--- src/suite-vendor/vona-test/modules/vona-test/test/jwt.test.ts
+++ src/suite-vendor/vona-test/modules/vona-test/test/jwt.test.ts
@@ -5,13 +5,20 @@ import { app } from 'vona-mock';
 describe('jwt.test.ts', () => {
   it('action:jwt', async () => {
     await app.bean.executor.mockCtx(async () => {
+      // login
       const jwt = await app.bean.executor.performAction('post', '/vona/test/jwt/login', {
         body: {
           name: 'admin',
         },
       });
       assert.equal(!!jwt?.accessToken, true);
-      const isAuthenticated = await app.bean.executor.performAction('get', '/vona/test/jwt/isAuthenticated');
+      // isAuthenticated
+      let isAuthenticated = await app.bean.executor.performAction('get', '/vona/test/jwt/isAuthenticated');
+      assert.equal(isAuthenticated, true);
+      // isAuthenticated: isolate
+      isAuthenticated = await app.bean.executor.newCtxIsolate(async () => {
+        return await app.bean.executor.performAction('get', '/vona/test/jwt/isAuthenticated');
+      });
       assert.equal(isAuthenticated, true);
     });
   });
