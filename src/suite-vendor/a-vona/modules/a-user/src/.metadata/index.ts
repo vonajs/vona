da9a7e0c001d758e26eb7237afa36bb8ceab3846
zhennann <zhen.nann@icloud.com>
2025-03-11 12:03:37 +0800

    Update index.ts

--- src/suite-vendor/a-vona/modules/a-user/src/.metadata/index.ts
+++ src/suite-vendor/a-vona/modules/a-user/src/.metadata/index.ts
@@ -94,7 +94,6 @@ export interface IModuleEvent {
   signin: EventSignin;
   signout: EventSignout;
 }
-export * from '../bean/meta.printTip1.ts';
 /** event: end */
 /** meta: begin */
 export * from '../bean/meta.printTip.ts';
@@ -102,18 +101,12 @@ declare module 'vona' {
 
   export interface IMetaRecord {
     'a-user:printTip': never;
-    'a-user:printTip1': never;
   }
 
 }
 declare module 'vona-module-a-user' {
 
   export interface MetaPrintTip {
-    /** @internal */
-    get scope(): ScopeModuleAUser;
-  }
-
-  export interface MetaPrintTip1 {
     /** @internal */
     get scope(): ScopeModuleAUser;
   }
