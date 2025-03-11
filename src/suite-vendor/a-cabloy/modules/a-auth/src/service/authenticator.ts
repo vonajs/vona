981d060a066345601c0bb604e11a9695416567b2
zhennann <zhen.nann@icloud.com>
2025-03-11 22:01:44 +0800

    feat: update

--- src/suite-vendor/a-cabloy/modules/a-auth/src/service/authenticator.ts
+++ src/suite-vendor/a-cabloy/modules/a-auth/src/service/authenticator.ts
@@ -2,4 +2,6 @@ import { BeanBase } from 'vona';
 import { Service } from 'vona-module-a-web';
 
 @Service()
-export class ServiceAuthenticator extends BeanBase {}
+export class ServiceAuthenticator extends BeanBase {
+
+}
---
+++ src/suite-vendor/cabloy-test/modules/cabloy-test/test/authSimple.test.ts
@@ -0,0 +1,11 @@
+import assert from 'node:assert';
+import { describe, it } from 'node:test';
+import { app } from 'vona-mock';
+
+describe.only('authSimple.test.ts.test.ts', () => {
+  it('action:authSimple.test.ts', async () => {
+    await app.bean.executor.mockCtx(async () => {
+      // await app.bean.auth.authenticate;
+    });
+  });
+});
