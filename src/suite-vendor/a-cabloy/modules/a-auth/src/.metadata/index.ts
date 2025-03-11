5e98276cdf861faf48e8cd45f6218b536e73daf7
zhennann <zhen.nann@icloud.com>
2025-03-11 19:29:51 +0800

    feat: authenticator

--- src/suite-vendor/a-cabloy/modules/a-auth/src/.metadata/index.ts
+++ src/suite-vendor/a-cabloy/modules/a-auth/src/.metadata/index.ts
@@ -2,11 +2,20 @@ import type { BeanScopeUtil } from 'vona';
 /** bean: end */
 /** bean: begin */
 import type { BeanAuth } from '../bean/bean.auth.ts';
-/** bean: end */
+/** service: end */
+/** service: begin */
+import type { ServiceAuthenticator } from '../service/authenticator.ts';
+/** service: end */
 /** scope: begin */
 import { BeanScopeBase } from 'vona';
+
 import { Scope } from 'vona-module-a-bean';
 /** bean: begin */
+import 'vona';
+/** service: end */
+/** service: begin */
+
+import 'vona';
 import 'vona';
 import 'vona';
 
@@ -28,12 +37,38 @@ declare module 'vona' {
     auth: BeanAuth;
   }
 }
+/** bean: end */
+/** service: begin */
+export * from '../service/authenticator.ts';
+declare module 'vona-module-a-web' {
+
+  export interface IServiceRecord {
+    'a-auth:authenticator': never;
+  }
+
+}
+declare module 'vona-module-a-auth' {
+
+  export interface ServiceAuthenticator {
+    /** @internal */
+    get scope(): ScopeModuleAAuth;
+  }
+}
+export interface IModuleService {
+  authenticator: ServiceAuthenticator;
+}
+declare module 'vona' {
+  export interface IBeanRecordGeneral {
+    'a-auth.service.authenticator': ServiceAuthenticator;
+  }
+}
 
 @Scope()
 export class ScopeModuleAAuth extends BeanScopeBase {}
 
 export interface ScopeModuleAAuth {
   util: BeanScopeUtil;
+  service: IModuleService;
 }
 declare module 'vona' {
   export interface IBeanScopeRecord {
