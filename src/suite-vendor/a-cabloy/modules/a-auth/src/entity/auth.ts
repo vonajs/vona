78a6fe9b800c5c343caa62a3a89373b586f80a55
zhennann <zhen.nann@icloud.com>
2025-03-13 15:10:16 +0800

    feat: auth provider

--- src/suite-vendor/a-cabloy/modules/a-auth/src/entity/auth.ts
+++ src/suite-vendor/a-cabloy/modules/a-auth/src/entity/auth.ts
@@ -1,6 +1,17 @@
-import { Entity, EntityBase } from 'vona-module-a-database';
+import { Entity, EntityProBase, TableIdentity } from 'vona-module-a-database';
+import { Rule, v } from 'vona-module-a-openapi';
 
 @Entity('aAuth')
-export class EntityAuth extends EntityBase {
+export class EntityAuth extends EntityProBase {
+  @Rule(v.tableIdentity())
+  userId: TableIdentity;
 
+  @Rule()
+  authProviderId: number;
+
+  @Rule()
+  profileId: string;
+
+  @Rule()
+  profile: string;
 }
--- src/suite-vendor/a-cabloy/modules/a-auth/src/entity/authProvider.ts
+++ src/suite-vendor/a-cabloy/modules/a-auth/src/entity/authProvider.ts
@@ -1,4 +1,21 @@
+import type { IAuthProviderClientOptions } from '../types/authProvider.ts';
 import { Entity, EntityBase } from 'vona-module-a-database';
+import { Rule } from 'vona-module-a-openapi';
 
 @Entity('aAuthProvider')
-export class EntityAuthProvider extends EntityBase {}
+export class EntityAuthProvider extends EntityBase {
+  @Rule()
+  disabled: boolean;
+
+  @Rule()
+  module: string;
+
+  @Rule()
+  providerName: string;
+
+  @Rule()
+  clientName: string;
+
+  @Rule()
+  clientOptions: IAuthProviderClientOptions;
+}
