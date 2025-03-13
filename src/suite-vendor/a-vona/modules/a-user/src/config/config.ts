8647fc3043d70f5baeb607847a7f6815b9f56da9
zhennann <zhen.nann@icloud.com>
2025-03-13 19:21:07 +0800

    Update config.ts

--- src/suite-vendor/a-vona/modules/a-user/src/config/config.ts
+++ src/suite-vendor/a-vona/modules/a-user/src/config/config.ts
@@ -1,6 +1,4 @@
-import type knex from 'knex';
 import type { VonaApplication } from 'vona';
-import type { TableIdentityType } from 'vona-module-a-database';
 import type { IServiceRecord } from 'vona-module-a-web';
 
 export function config(_app: VonaApplication) {
@@ -9,50 +7,6 @@ export function config(_app: VonaApplication) {
       refreshAuthToken: {
         recreate: true,
         refresh: true,
-      },
-    },
-    tableIdentity: {
-      current: {
-        user: 'string' as TableIdentityType,
-        auth: 'string' as TableIdentityType,
-      },
-      fields: {
-        user: {
-          number: {
-            userIdPrimary(this: knex.Knex.TableBuilder) {
-              return this.increments();
-            },
-            userId(this: knex.Knex.TableBuilder, columnName: string) {
-              return this.integer(columnName ?? 'userId'); // default is null
-            },
-          },
-          string: {
-            userIdPrimary(this: knex.Knex.TableBuilder) {
-              return this.bigIncrements();
-            },
-            userId(this: knex.Knex.TableBuilder, columnName: string) {
-              return this.bigInteger(columnName ?? 'userId'); // default is null
-            },
-          },
-        },
-        auth: {
-          number: {
-            authIdPrimary(this: knex.Knex.TableBuilder) {
-              return this.increments();
-            },
-            authId(this: knex.Knex.TableBuilder, columnName: string) {
-              return this.integer(columnName ?? 'authId');
-            },
-          },
-          string: {
-            authIdPrimary(this: knex.Knex.TableBuilder) {
-              return this.bigIncrements();
-            },
-            authId(this: knex.Knex.TableBuilder, columnName: string) {
-              return this.bigInteger(columnName ?? 'authId');
-            },
-          },
-        },
       },
     },
     adapter: {
