7a8c4ac0a5a9803f1b89fdfc41390eb6e22b19d6
zhennann <zhen.nann@icloud.com>
2025-03-13 15:34:22 +0800

    Update meta.version.ts

--- src/suite-vendor/a-cabloy/modules/a-auth/src/bean/meta.version.ts
+++ src/suite-vendor/a-cabloy/modules/a-auth/src/bean/meta.version.ts
@@ -4,5 +4,14 @@ import { Meta } from 'vona-module-a-meta';
 
 @Meta()
 export class MetaVersion extends BeanBase implements IMetaVersionUpdate {
-  async update(_options: IMetaVersionUpdateOptions) {}
+  async update(options: IMetaVersionUpdateOptions) {
+    if (options.version === 1) {
+      // aAuth
+      const entity = this.scope.entity.auth;
+      await this.bean.model.createTable(entity.table, table => {
+        table.basicFields();
+        table.userId();
+      });
+    }
+  }
 }
