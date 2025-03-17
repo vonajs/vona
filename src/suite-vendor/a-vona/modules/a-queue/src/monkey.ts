3bebe115e19cae0673ebd2b46523ddecabb79892
zhennann <zhen.nann@icloud.com>
2025-03-17 10:59:35 +0800

    Update monkey.ts

--- src/suite-vendor/a-vona/modules/a-queue/src/monkey.ts
+++ src/suite-vendor/a-vona/modules/a-queue/src/monkey.ts
@@ -1,11 +1,15 @@
-import type { IMonkeyAppClosed } from 'vona';
+import type { IMonkeyAppClose, IMonkeyAppClosed } from 'vona';
 import { BeanSimple } from 'vona';
 import { __ThisModule__ } from './.metadata/this.ts';
 
-export class Monkey extends BeanSimple implements IMonkeyAppClosed {
-  async appClosed() {
+export class Monkey extends BeanSimple implements IMonkeyAppClose, IMonkeyAppClosed {
+  async appClose() {
     const scopeSelf = this.bean.scope(__ThisModule__);
     await scopeSelf.service.queue.clearWorkers();
+  }
+
+  async appClosed() {
+    const scopeSelf = this.bean.scope(__ThisModule__);
     await scopeSelf.service.queue.clearQueues();
   }
 }
