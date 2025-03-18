cb9454c7e686a32d720468374d9a46a9e09e55e8
zhennann <zhen.nann@icloud.com>
2025-03-18 10:48:33 +0800

    Update passport.ts

--- src/suite-vendor/a-cabloy/modules/a-auth/src/controller/passport.ts
+++ src/suite-vendor/a-cabloy/modules/a-auth/src/controller/passport.ts
@@ -2,7 +2,6 @@ import { BeanBase } from 'vona';
 import { Api } from 'vona-module-a-openapi';
 import { Public } from 'vona-module-a-user';
 import { Controller, Get } from 'vona-module-a-web';
-import { __ThisModule__ } from '../.metadata/this.ts';
 
 @Controller('passport')
 export class ControllerPassport extends BeanBase {
