4d33613b2662f06183f1daf5e8c0d8c2201af277
zhennann <zhen.nann@icloud.com>
2025-03-19 09:43:04 +0800

    Update meta.version.ts

--- src/suite/a-home/modules/home-user/src/bean/meta.version.ts
+++ src/suite/a-home/modules/home-user/src/bean/meta.version.ts
@@ -4,14 +4,16 @@ import { Meta } from 'vona-module-a-meta';
 
 @Meta()
 export class MetaVersion extends BeanBase implements IMetaVersionUpdate {
-  async update(_options: IMetaVersionUpdateOptions) {
-    // homeUser
-    const entityUser = this.scope.entity.user;
-    await this.bean.model.createTable(entityUser.$table, table => {
-      table.basicFields();
-      table.string(entityUser.$column('name'), 255);
-      table.string(entityUser.$column('avatar'), 255);
-      table.string(entityUser.$column('locale'), 255);
-    });
+  async update(options: IMetaVersionUpdateOptions) {
+    if (options.version === 1) {
+      // homeUser
+      const entityUser = this.scope.entity.user;
+      await this.bean.model.createTable(entityUser.$table, table => {
+        table.basicFields();
+        table.string(entityUser.$column('name'), 255);
+        table.string(entityUser.$column('avatar'), 255);
+        table.string(entityUser.$column('locale'), 255);
+      });
+    }
   }
 }
