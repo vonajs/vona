669a4b754dd04c930ddcc4f88f1933f5a31f7c08
zhennann <zhen.nann@icloud.com>
2025-03-19 22:37:24 +0800

    Update socket.test.ts

--- src/suite-vendor/cabloy-test/modules/cabloy-test/test/socket.test.ts
+++ src/suite-vendor/cabloy-test/modules/cabloy-test/test/socket.test.ts
@@ -11,9 +11,7 @@ describe.only('socket.test.ts', () => {
 function test() {
   return new Promise(resolve => {
     const ws = new WebSocket(`ws://${app.config.server.listen.hostname}:${app.config.server.listen.port}`);
-    ws.addEventListener('open', _event => {
-      console.log('WebSocket connection established!');
-      // Sends a message to the WebSocket server.
+    ws.addEventListener('open', () => {
       ws.send('Hello Server!');
       ws.close();
       resolve(true);
