32af1b06705703a0c068cd467c17331df5bed23a
zhennann <zhen.nann@icloud.com>
2025-03-20 11:21:58 +0800

    Update socket.test.ts

--- src/suite-vendor/cabloy-test/modules/cabloy-test/test/socket.test.ts
+++ src/suite-vendor/cabloy-test/modules/cabloy-test/test/socket.test.ts
@@ -2,7 +2,7 @@
 import { describe, it } from 'node:test';
 import { app } from 'vona-mock';
 
-describe.only('socket.test.ts', () => {
+describe('socket.test.ts', () => {
   it('action:socket', async () => {
     await test();
   });
@@ -11,10 +11,10 @@ describe.only('socket.test.ts', () => {
 function test() {
   return new Promise(resolve => {
     const ws = new WebSocket(`ws://${app.config.server.listen.hostname}:${app.config.server.listen.port}`);
-    ws.addEventListener('open', () => {
+    ws.onopen = () => {
       ws.send('Hello Server!');
       ws.close();
       resolve(true);
-    });
+    };
   });
 }
