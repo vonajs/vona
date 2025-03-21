2a3ffb4833dc19a5f4075fd03e4daf5b70b96b48
zhennann <zhen.nann@icloud.com>
2025-03-21 11:01:23 +0800

    feat: update

--- src/suite-vendor/a-cabloy/modules/a-status/src/model/status.ts
+++ src/suite-vendor/a-cabloy/modules/a-status/src/model/status.ts
@@ -1,5 +1,5 @@
 import { BeanModelBase, Model } from 'vona-module-a-database';
 import { EntityStatus } from '../entity/status.ts';
 
-@Model({ entity: EntityStatus, disableDeleted: true })
+@Model({ entity: EntityStatus, disableDeleted: true, cacheOptions: { preset: 'allWithIgnoreNull' } })
 export class ModelStatus extends BeanModelBase<EntityStatus> {}
--- src/suite-vendor/a-vona/modules/a-instance/src/model/instance.ts
+++ src/suite-vendor/a-vona/modules/a-instance/src/model/instance.ts
@@ -1,5 +1,5 @@
 import { BeanModelBase, Model } from 'vona-module-a-database';
 import { EntityInstance } from '../entity/instance.ts';
 
-@Model({ entity: EntityInstance, disableDeleted: false, disableInstance: true })
+@Model({ entity: EntityInstance, disableDeleted: false, disableInstance: true, cacheOptions: { preset: 'allWithIgnoreNull' } })
 export class ModelInstance extends BeanModelBase<EntityInstance> {}
