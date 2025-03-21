81edc7f96db704bda84b0ac82391d8b6f5516f02
zhennann <zhen.nann@icloud.com>
2025-03-21 16:25:42 +0800

    Update socketPacket.cabloy.ts

--- src/suite-vendor/a-cabloy/modules/cabloy-socket/src/bean/socketPacket.cabloy.ts
+++ src/suite-vendor/a-cabloy/modules/cabloy-socket/src/bean/socketPacket.cabloy.ts
@@ -1,6 +1,7 @@
 import type { Next } from 'vona';
 import type { IDecoratorSocketPacketOptions, ISocketPacketExecute } from 'vona-module-a-socket';
 import type { WebSocket } from 'ws';
+import type { TypeSocketPacketCabloy } from '../types/socket.ts';
 import { BeanBase } from 'vona';
 import { SocketPacket } from 'vona-module-a-socket';
 
@@ -8,8 +9,9 @@ export interface ISocketPacketOptionsCabloy extends IDecoratorSocketPacketOption
 
 @SocketPacket<ISocketPacketOptionsCabloy>({ match: 'cabloy' })
 export class SocketPacketCabloy extends BeanBase implements ISocketPacketExecute {
-  async execute(_data: any, _ws: WebSocket, _options: ISocketPacketOptionsCabloy, next: Next): Promise<void> {
+  async execute(data: any, _ws: WebSocket, _options: ISocketPacketOptionsCabloy, next: Next): Promise<void> {
+    const packet: TypeSocketPacketCabloy = (data && typeof data === 'string') ? JSON.parse(data) : [undefined, data];
     // next
-    return next();
+    return next(packet);
   }
 }
