f26826ac11c9f903a6120f7ed308db1340e72eee
zhennann <zhen.nann@icloud.com>
2025-03-21 21:02:34 +0800

    Update socket.test.ts

--- src/suite-vendor/cabloy-test/modules/cabloy-test/test/socket.test.ts
+++ src/suite-vendor/cabloy-test/modules/cabloy-test/test/socket.test.ts
@@ -14,8 +14,15 @@ function test() {
     const ws = new WebSocket(`ws://${app.config.server.listen.hostname}:${app.config.server.listen.port}/cabloy?name=zhennann`);
     ws.onopen = async () => {
       ws.sendEvent('default', 'Hello Server!');
+      // success
       const res = await ws.performAction('get', '/');
       assert.equal(res, 'Hello Vona!');
+      // fail
+      try {
+        await ws.performAction('get', '/__not_found_fail');
+      } catch (err: any) {
+        assert.equal(err.code, 404);
+      }
       ws.close();
       resolve(true);
     };
