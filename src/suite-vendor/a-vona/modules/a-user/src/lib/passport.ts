e244bef003a8bf94fad34c8dd3411216ee89e88e
zhennann <zhen.nann@icloud.com>
2025-03-23 16:15:54 +0800

    feat: public

--- src/suite-vendor/a-vona/modules/a-user/src/lib/passport.ts
+++ src/suite-vendor/a-vona/modules/a-user/src/lib/passport.ts
@@ -1,4 +1,13 @@
-import { Public } from './public.ts';
+import type { TypeUseOnionOmitOptionsGlobal } from 'vona-module-a-onion';
+import type { IGuardOptionsPassport } from '../bean/guard.passport.ts';
+import { Aspect } from 'vona-module-a-aspect';
+
+export function Public(
+  options?: Partial<TypeUseOnionOmitOptionsGlobal<IGuardOptionsPassport>>,
+): ClassDecorator & MethodDecorator {
+  const _public = options?.public === undefined ? true : options.public;
+  return Aspect.guardGlobal('a-user:passport', { public: _public });
+}
 
 export const Passport = {
   public: Public,
