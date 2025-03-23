ab83b6556a72a35a69ae20f9d8db6db2ca259f76
zhennann <zhen.nann@icloud.com>
2025-03-23 20:44:50 +0800

    Update upload.test.ts

--- src/suite-vendor/vona-test/modules/vona-test/test/upload.test.ts
+++ src/suite-vendor/vona-test/modules/vona-test/test/upload.test.ts
@@ -1,4 +1,5 @@
 import assert from 'node:assert';
+import { Blob } from 'node:buffer';
 import { describe, it } from 'node:test';
 import { app } from 'vona-mock';
 import { $apiPath } from 'vona-module-a-web';
@@ -8,6 +9,20 @@ describe.only('upload.test.ts', () => {
     await app.bean.executor.mockCtx(async () => {
       const formData = new FormData();
       formData.append('name', 'zhennann');
+      const url = app.util.getAbsoluteUrlByApiPath($apiPath('/vona/test/upload/fields'));
+      const res = await fetch(url, {
+        method: 'POST',
+        body: formData,
+      });
+      const data = await res.json();
+      assert.equal(data.data.fields.find(item => item.name === 'name')?.value, 'zhennann');
+    });
+  });
+  it('action:upload:file', async () => {
+    await app.bean.executor.mockCtx(async () => {
+      const formData = new FormData();
+      formData.append('name', 'zhennann');
+      formData.append('file', new (Blob as any)(['hello world!']), 'file-test.txt');
       const url = app.util.getAbsoluteUrlByApiPath($apiPath('/vona/test/upload/fields'));
       const res = await fetch(url, {
         method: 'POST',
