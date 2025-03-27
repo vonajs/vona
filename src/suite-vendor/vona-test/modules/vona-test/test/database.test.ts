453c925f8e7dedc5d8edb3ab5bbf2a02f13bc2f1
zhennann <zhen.nann@icloud.com>
2025-03-27 10:07:59 +0800

    Update database.test.ts

--- src/suite-vendor/vona-test/modules/vona-test/test/database.test.ts
+++ src/suite-vendor/vona-test/modules/vona-test/test/database.test.ts
@@ -3,7 +3,7 @@ import { describe, it } from 'node:test';
 import { app } from 'vona-mock';
 
 describe('database.test.ts', () => {
-  it('action:dataSource:switch', async () => {
+  it('action:database:switchClient', async () => {
     await app.bean.executor.mockCtx(async () => {
       // current
       assert.equal(app.ctx.dbMeta.currentClientName, app.config.database.defaultClient);
@@ -17,7 +17,7 @@ describe('database.test.ts', () => {
       assert.equal(app.ctx.dbMeta.currentClientName, app.config.database.defaultClient);
     });
   });
-  it('action:dataSource:model', async () => {
+  it('action:model:clientName', async () => {
     await app.bean.executor.mockCtx(async () => {
       // scope
       const scopeTest = app.bean.scope('vona-test');
