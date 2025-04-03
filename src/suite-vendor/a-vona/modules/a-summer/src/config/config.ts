99a87049ad984a9e822a4549b20349455687a938
zhennann <zhen.nann@icloud.com>
2025-04-03 10:05:04 +0800

    Update config.ts

--- src/suite-vendor/a-vona/modules/a-summer/src/config/config.ts
+++ src/suite-vendor/a-vona/modules/a-summer/src/config/config.ts
@@ -2,6 +2,16 @@ import type { VonaApplication } from 'vona';
 import type { IOnionOptionsMeta } from 'vona-module-a-onion';
 import type { IDecoratorSummerCacheOptions, TSummerCachePreset } from '../types/summerCache.ts';
 
+export const configMem = {
+  mode: 'mem', // only mem
+  mem: {
+    max: 500,
+    broadcastOnSet: 'del',
+  },
+} as IDecoratorSummerCacheOptions;
+
+export const configMemWithIgnoreNull = { ...configMem, ignoreNull: true };
+
 export const configRedis = {
   mode: 'redis', // only redis
   redis: {
@@ -31,6 +41,8 @@ export function config(_app: VonaApplication) {
       meta: {} as IOnionOptionsMeta,
       presetDefault: 'all' as TSummerCachePreset,
       preset: {
+        mem: configMem,
+        memWithIgnoreNull: configMemWithIgnoreNull,
         redis: configRedis,
         redisWithIgnoreNull: configRedisWithIgnoreNull,
         all: configAll,
