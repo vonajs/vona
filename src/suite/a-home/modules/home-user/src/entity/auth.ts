eb6c0a0c5b8af711813db3d2ebc2b243a6f76b1c
zhennann <zhen.nann@icloud.com>
2025-04-15 15:18:09 +0800

    Update auth.ts

--- src/suite/a-home/modules/home-user/src/entity/auth.ts
+++ src/suite/a-home/modules/home-user/src/entity/auth.ts
@@ -1,8 +1,8 @@
-import { Entity, EntityBase, TableIdentity } from 'vona-module-a-database';
+import { Entity, EntityBaseEmpty, TableIdentity } from 'vona-module-a-database';
 import { Api, v } from 'vona-module-a-openapi';
 
 @Entity('homeAuth')
-export class EntityAuth extends EntityBase {
+export class EntityAuth extends EntityBaseEmpty {
   @Api.field(v.tableIdentity())
   id: TableIdentity;
 }
