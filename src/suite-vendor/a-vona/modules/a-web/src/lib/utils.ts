2aa2c3d0e8521fe668bde136a980ae78287dfe3e
zhennann <zhen.nann@icloud.com>
2025-04-29 11:36:45 +0800

    Update utils.ts

--- src/suite-vendor/a-vona/modules/a-web/src/lib/utils.ts
+++ src/suite-vendor/a-vona/modules/a-web/src/lib/utils.ts
@@ -1,6 +1,10 @@
 import type { IApiPathRecord } from '../types/controller.ts';
 import { combineParamsAndQuery } from '@cabloy/utils';
 
-export function $apiPath<K extends keyof IApiPathRecord>(path: K, options?: { params?: object; query?: object }): K {
-  return combineParamsAndQuery(path, options) as unknown as K;
+export function $apiPath<K extends keyof IApiPathRecord>(path: K): K {
+  return path;
+}
+
+export function $apiPathAndCombineParamsAndQuery<K extends keyof IApiPathRecord>(path: K, options?: { params?: object; query?: object }): string {
+  return combineParamsAndQuery(path, options);
 }
