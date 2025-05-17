4308ce14303f55de1d8151273119f4a983ae3740
zhennann <zhen.nann@icloud.com>
2025-05-17 21:57:02 +0800

    Update rest.ts

--- src/suite-vendor/a-vona/modules/a-openapi/src/types/rest.ts
+++ src/suite-vendor/a-vona/modules/a-openapi/src/types/rest.ts
@@ -1,8 +1,12 @@
 import 'openapi3-ts/oas30';
 import 'openapi3-ts/oas31';
 
+export interface ISchemaObjectExtensionRest {
+  render?: string;
+}
+
 export interface ISchemaObjectExtension {
-  rest: any;
+  rest?: ISchemaObjectExtensionRest;
 }
 
 declare module 'openapi3-ts/oas30' {
