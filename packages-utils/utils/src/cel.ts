320fb43870d80f8d62da6382d2c15ad2e95f3f13
zhennann <zhen.nann@icloud.com>
2025-05-20 12:15:11 +0800

    feat: update

--- packages-utils/utils/src/cel.ts
+++ packages-utils/utils/src/cel.ts
@@ -1,4 +1,5 @@
 import type * as CelJS from 'cel-js' with { 'resolution-mode': 'import' };
+import type { CstNode } from 'chevrotain';
 import * as celjs from 'cel-js';
 import { isNil } from './check.ts';
 
@@ -17,12 +18,24 @@ export function evaluateExpressions(expressions: any, context?: Record<string, u
   return evaluate(expressions, context, functions);
 }
 
-export function evaluate(expression: string, context?: Record<string, unknown>, functions?: Record<string, CallableFunction>): unknown {
-  if (typeof expression !== 'string') return expression;
-  if (expression.startsWith('##')) {
-    return expression.substring('##'.length);
+export function evaluate(
+  expression: CstNode | string,
+  context?: Record<string, unknown>,
+  functions?: Record<string, CallableFunction>,
+): unknown {
+  // CstNode
+  if (typeof expression === 'object' && expression.children) {
+    return celjs.evaluate(expression, context, functions);
   }
-  return celjs.evaluate(expression, context, functions);
+  // string
+  if (typeof expression === 'string') {
+    if (expression.startsWith('##')) {
+      return expression.substring('##'.length);
+    }
+    return celjs.evaluate(expression, context, functions);
+  }
+  // others
+  return expression;
 }
 
 export function parse(expression: string): CelJS.ParseResult {
