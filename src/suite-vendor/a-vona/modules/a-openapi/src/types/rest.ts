4516e2fc32f6fab9f646fc73d9e365f34412031c
zhennann <zhen.nann@icloud.com>
2025-05-20 10:16:51 +0800

    Update rest.ts

--- src/suite-vendor/a-vona/modules/a-openapi/src/types/rest.ts
+++ src/suite-vendor/a-vona/modules/a-openapi/src/types/rest.ts
@@ -3,6 +3,8 @@ import 'openapi3-ts/oas31';
 
 export interface ISchemaObjectExtensionFieldRest {
   render?: string;
+  table?: Omit<ISchemaObjectExtensionFieldRest, 'table' | 'form'>;
+  form?: Omit<ISchemaObjectExtensionFieldRest, 'table' | 'form'>;
 }
 
 export interface ISchemaObjectExtensionField {
