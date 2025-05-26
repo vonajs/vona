a39ccc5830d971ed7c0926940100dd2bba9be5f8
zhennann <zhen.nann@icloud.com>
2025-05-26 23:16:58 +0800

    feat: update

--- src/backend/config/config/config.test.ts
+++ src/backend/config/config/config.test.ts
@@ -3,6 +3,11 @@ import type { VonaAppInfo, VonaConfigOptional } from 'vona';
 export default function (_appInfo: VonaAppInfo) {
   const config = {} as VonaConfigOptional;
 
+  // instances
+  config.instances = [
+    { instanceName: '', password: '', title: '' },
+  ];
+
   // modules
   config.modules = {};
 
