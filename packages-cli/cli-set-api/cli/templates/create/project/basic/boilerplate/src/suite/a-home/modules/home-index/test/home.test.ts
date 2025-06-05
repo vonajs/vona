d1e09c6f5327e1c9d85b78dc3b52ab2daec66ec0
zhennann <zhen.nann@icloud.com>
2025-06-05 13:39:54 +0800

    feat: update

--- packages-cli/cli-set-api/cli/templates/create/project/basic/boilerplate/src/suite/a-home/modules/home-index/test/home.test.ts
+++ packages-cli/cli-set-api/cli/templates/create/project/basic/boilerplate/src/suite/a-home/modules/home-index/test/home.test.ts
@@ -2,7 +2,7 @@ import assert from 'node:assert';
 import { describe, it } from 'node:test';
 import { app } from 'vona-mock';
 
-describe.only('home.test.ts', () => {
+describe('home.test.ts', () => {
   it('action:home', async () => {
     await app.bean.executor.mockCtx(async () => {
       const res = await app.bean.executor.performAction('get', '//');
