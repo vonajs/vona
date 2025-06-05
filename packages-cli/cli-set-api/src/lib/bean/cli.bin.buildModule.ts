dd72fe1da68e4ea7f3300660a832feb45e5e131c
zhennann <zhen.nann@icloud.com>
2025-06-05 16:48:25 +0800

    feat: update

--- packages-cli/cli-set-api/src/lib/bean/cli.bin.buildModule.ts
+++ packages-cli/cli-set-api/src/lib/bean/cli.bin.buildModule.ts
@@ -1,7 +1,4 @@
-import type { glob } from '@cabloy/module-glob';
-import type { VonaConfigMeta, VonaMetaFlavor, VonaMetaMode } from '@cabloy/module-info';
 import type { LogLevel, LogOrStringHandler, OutputOptions, RollupBuild, RollupLog, RollupOptions } from 'rollup';
-import type { VonaBinConfigOptions } from './toolsBin/types.ts';
 import path from 'node:path';
 import { BeanCliBase } from '@cabloy/cli';
 import aliasImport from '@rollup/plugin-alias';
@@ -9,13 +6,9 @@ import babelImport from '@rollup/plugin-babel';
 import commonjsImport from '@rollup/plugin-commonjs';
 import jsonImport from '@rollup/plugin-json';
 import resolveImport from '@rollup/plugin-node-resolve';
-import replaceImport from '@rollup/plugin-replace';
 import terserImport from '@rollup/plugin-terser';
-import fse from 'fs-extra';
 import { rimraf } from 'rimraf';
 import { rollup } from 'rollup';
-import { generateConfigDefine } from '../utils.ts';
-import { generateVonaMeta } from './toolsBin/generateVonaMeta.ts';
 
 const commonjs = commonjsImport as any as typeof commonjsImport.default;
 const resolve = resolveImport as any as typeof resolveImport.default;
@@ -24,12 +17,11 @@ const json = jsonImport as any as typeof jsonImport.default;
 const babel = babelImport as any as typeof babelImport.default;
 const terser = terserImport as any as typeof terserImport.default;
 const alias = aliasImport as any as typeof aliasImport.default;
-const replace = replaceImport as any as typeof replaceImport.default;
 
 declare module '@cabloy/cli' {
   interface ICommandArgv {
-    workers?: number;
-    flavor?: VonaMetaFlavor;
+    minify: boolean;
+    sourcemap: boolean;
   }
 }
 
@@ -43,55 +35,23 @@ export class CliBinBuildModule extends BeanCliBase {
   }
 
   async _build(projectPath: string) {
-    const { argv } = this.context;
-    const mode: VonaMetaMode = 'prod';
-    const flavor: VonaMetaFlavor = argv.flavor || 'normal';
-    const configMeta: VonaConfigMeta = { flavor, mode };
-    const configOptions: VonaBinConfigOptions = {
-      appDir: projectPath,
-      runtimeDir: '.vona',
-      workers: argv.workers,
-    };
-    const { env, modulesMeta } = await generateVonaMeta(configMeta, configOptions);
     await rimraf(path.join(projectPath, 'dist'));
-    await this._rollup(projectPath, env);
-    await this._assets(projectPath, modulesMeta);
-    await rimraf(path.join(projectPath, '.vona'));
+    await this._rollup(projectPath);
   }
 
-  async _assets(projectPath: string, modulesMeta: Awaited<ReturnType<typeof glob>>) {
-    const assetsPath = path.join(projectPath, 'dist/assets');
-    for (const relativeName in modulesMeta.modules) {
-      const module = modulesMeta.modules[relativeName];
-      if (!module.package.files) continue;
-      for (const scene of module.package.files) {
-        if (['dist', 'cli', 'test'].includes(scene)) continue;
-        const scenePath = path.join(module.root, scene);
-        if (fse.existsSync(scenePath)) {
-          const destPath = path.join(assetsPath, scene, relativeName);
-          await fse.copy(scenePath, destPath);
-        }
-      }
-    }
-  }
-
-  async _rollup(projectPath: string, env: NodeJS.ProcessEnv) {
+  async _rollup(projectPath: string) {
+    const { argv } = this.context;
     const aliasEntries: aliasImport.Alias[] = [];
     for (const name of ['better-sqlite3', 'mysql', 'oracledb', 'pg-native', 'pg-query-stream', 'sqlite3', 'tedious', 'cloudflare:sockets']) {
       aliasEntries.push({ find: name, replacement: 'vona-shared' });
     }
 
-    const replaceValues = generateConfigDefine(env, ['NODE_ENV', 'META_MODE', 'META_FLAVOR']);
     const plugins = [
       alias({
         entries: aliasEntries,
       }),
       resolve({
         preferBuiltins: true,
-      }),
-      replace({
-        values: replaceValues,
-        preventAssignment: false,
       }),
       json(),
       commonjs(),
@@ -111,7 +71,7 @@ export class CliBinBuildModule extends BeanCliBase {
         ],
       }),
     ];
-    if (process.env.BUILD_MINIFY === 'true') {
+    if (argv.minify) {
       plugins.push(terser({
         keep_classnames: true,
       }));
@@ -120,7 +80,7 @@ export class CliBinBuildModule extends BeanCliBase {
       input: path.join(projectPath, '.vona/bootstrap.ts'),
       plugins,
       onLog: (level: LogLevel, log: RollupLog, defaultHandler: LogOrStringHandler) => {
-        if (log.code === 'CIRCULAR_DEPENDENCY' && process.env.BUILD_LOG_CIRCULAR_DEPENDENCY === 'false') return;
+        if (log.code === 'CIRCULAR_DEPENDENCY') return;
         if (log.code === 'THIS_IS_UNDEFINED' && (log.message.includes('ramda/es/partialObject.js') || log.message.includes("The 'this' keyword is equivalent to 'undefined' at the top level of an ES module"))) return;
         if (log.code === 'EVAL' && log.message.includes('depd/index.js')) return;
         if (log.code === 'EVAL' && log.message.includes('bluebird/js/release/util.js')) return;
@@ -132,9 +92,9 @@ export class CliBinBuildModule extends BeanCliBase {
       dir: path.join(projectPath, 'dist'),
       // file: path.join(projectPath, 'dist/index.js'),
       format: 'esm',
-      sourcemap: process.env.BUILD_SOURCEMAP === 'true',
+      sourcemap: argv.sourcemap,
       // https://github.com/rollup/rollup/issues/4166
-      inlineDynamicImports: process.env.BUILD_INLINEDYNAMICIMPORTS === 'true',
+      inlineDynamicImports: true,
     };
 
     let bundle: RollupBuild | undefined;
--- packages-cli/cli-set-api/src/lib/command/bin.buildModule.ts
+++ packages-cli/cli-set-api/src/lib/command/bin.buildModule.ts
@@ -3,8 +3,16 @@ export default {
   info: {
     version: '5.0.0',
     title: 'Cli: Tools: Bin',
-    usage: 'vona :bin:buildModule [--sourcemap=]',
+    usage: 'vona :bin:buildModule [--minify] [--sourcemap]',
   },
   options: {
+    minify: {
+      description: 'minify',
+      type: 'boolean',
+    },
+    sourcemap: {
+      description: 'sourcemap',
+      type: 'boolean',
+    },
   },
 };
