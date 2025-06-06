a5ea342482d8ef52de50503c18cabece24221a44
zhennann <zhen.nann@icloud.com>
2025-06-06 10:15:55 +0800

    feat: update

---
+++ packages-vona/vona/scripts/versionTemplate.ts
@@ -0,0 +1,17 @@
+import fs from 'node:fs/promises';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+async function versionTemplate() {
+  // version
+  const pkgFileCurrent = path.resolve(fileURLToPath(import.meta.url), '../../package.json');
+  const pkgContentCurrent = (await fs.readFile(pkgFileCurrent)).toString();
+  const version = pkgContentCurrent.match(/"version": "([^"]*)"/)![1];
+  // change
+  const pkgFile = path.resolve(fileURLToPath(import.meta.url), '../../../../packages-cli/cli-set-api/cli/templates/create/project/basic/boilerplate/package.original.json');
+  let pkgContent = (await fs.readFile(pkgFile)).toString();
+  pkgContent=pkgContent.replace(/"vona": "\^([^"]*)"/, () => { return `"vona": "^${version}"` });
+  fs.writeFile(pkgFile,pkgContent);
+}
+
+versionTemplate();
