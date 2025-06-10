a59e262c0bf5cbb39e33a2dafee29a98bf5023f4
zhennann <zhen.nann@icloud.com>
2025-06-10 16:15:22 +0800

    feat: update

--- packages-cli/cli-set-api/cli/templates/tools/crud/snippets/2-meta.index.ts
+++ packages-cli/cli-set-api/cli/templates/tools/crud/snippets/2-meta.index.ts
@@ -8,11 +8,16 @@ declare module '@cabloy/cli' {
   }
 }
 
-const __snippet_update = '';
+const __snippet_import1 = 'import { $tableColumns } from \'vona-module-a-database\';\n';
+const __snippet_import2 = 'import { Entity<%=argv.resourceNameCapitalize%> } from \'../entity/<%=argv.resourceName%>.ts\';\n';
+const __snippet_update = `...$tableColumns(
+      () => Entity<%=argv.resourceNameCapitalize%>,
+      entity => entity.name,
+    ),`;
 
 export default metadataCustomSnippet({
   file: 'src/bean/meta.index.ts',
-  language: 'gogo',
+  language: 'plain',
   init: async ({ cli, argv, targetFile }) => {
     await catchError(() => {
       return cli.helper.invokeCli([
@@ -25,10 +30,17 @@ export default metadataCustomSnippet({
     return fs.readFileSync(targetFile).toString('utf8');
   },
   async transform({ cli, ast }) {
+    // import1
+    if (!ast.includes(__snippet_import1)) {
+      const code = await cli.template.renderContent({ content: __snippet_import1 });
+      ast = `${code}${ast}`;
+    }
+    // import2
+    let code = await cli.template.renderContent({ content: __snippet_import2 });
+    ast = `${code}${ast}`;
     // update
-    ast.replace('async update(_options: IMetaVersionUpdateOptions) {$$$1}', 'async update(options: IMetaVersionUpdateOptions) {$$$1}');
-    const code = await cli.template.renderContent({ content: __snippet_update });
-    ast.replace('async update($$$0) {$$$1}', `async update($$$0) {$$$1 \n ${code}}`);
+    code = await cli.template.renderContent({ content: __snippet_update });
+    ast = ast.replace('indexes: {', `indexes: {\n${code}`);
     // ok
     return ast;
   },
