aba2220dde5a175a75a10ad17c589fcdc5fe78b1
zhennann <zhen.nann@icloud.com>
2025-06-10 20:21:30 +0800

    Update guard.admin.ts

--- src/suite-vendor/a-vona/modules/a-user/src/bean/guard.admin.ts
+++ src/suite-vendor/a-vona/modules/a-user/src/bean/guard.admin.ts
@@ -5,16 +5,16 @@ import { Guard } from 'vona-module-a-aspect';
 
 export interface IGuardOptionsAdmin extends IDecoratorGuardOptions {
   admin: boolean;
-  passWhenAdmin: boolean;
+  passWhenMatched: boolean;
 }
 
-@Guard<IGuardOptionsAdmin>({ admin: true, passWhenAdmin: true })
+@Guard<IGuardOptionsAdmin>({ admin: true, passWhenMatched: true })
 export class GuardAdmin extends BeanBase implements IGuardExecute {
   async execute(options: IGuardOptionsAdmin, next: Next): Promise<boolean> {
     if (options.admin) {
       const isAdmin = await this.bean.passport.isAdmin();
       if (!isAdmin) return this.app.throw(403);
-      if (options.passWhenAdmin) return true;
+      if (options.passWhenMatched) return true;
     }
     // next
     return next();
