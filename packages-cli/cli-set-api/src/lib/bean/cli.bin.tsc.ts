0d22c0b24a11f7389038a9d9b1209b0018fca99f
zhennann <zhen.nann@icloud.com>
2025-06-18 16:22:39 +0800

    fix: tsc

--- packages-cli/cli-set-api/src/lib/bean/cli.bin.tsc.ts
+++ packages-cli/cli-set-api/src/lib/bean/cli.bin.tsc.ts
@@ -17,9 +17,10 @@ export class CliBinTsc extends BeanCliBase {
   }
 
   async _tsc(projectPath: string, force: boolean) {
+    const suiteNames = Object.keys(this.modulesMeta.suites).filter(suiteName => !this.modulesMeta.suites[suiteName].info.node_modules);
+    const modulesArray = this.modulesMeta.modulesArray.filter(item => !item.suite && !item.info.node_modules);
     // count
-    const count = 1 +
-      Object.keys(this.modulesMeta.suites).length + this.modulesMeta.modulesArray.filter(item => !item.suite).length;
+    const count = 1 + suiteNames.length + modulesArray.length;
     // begin
     let counter = 0;
     const timeBegin = new Date();
@@ -35,19 +36,17 @@ export class CliBinTsc extends BeanCliBase {
     console.log(`===>  ${++counter}/${count} project`);
     await this.helper.processHelper.tsc(tscArgs, { cwd: projectPath });
     // suites
-    for (const key in this.modulesMeta.suites) {
+    for (const key of suiteNames) {
       const suite = this.modulesMeta.suites[key];
       // eslint-disable-next-line
       console.log(`===>  ${++counter}/${count} suite: ${suite.info.originalName}`);
       await this.helper.processHelper.tsc(tscArgs, { cwd: suite.root });
     }
     // modules
-    for (const module of this.modulesMeta.modulesArray) {
-      if (!module.suite) {
-        // eslint-disable-next-line
-        console.log(`===>  ${++counter}/${count} module: ${module.info.originalName}`);
-        await this.helper.processHelper.tsc(tscArgs, { cwd: module.root });
-      }
+    for (const module of modulesArray) {
+      // eslint-disable-next-line
+      console.log(`===>  ${++counter}/${count} module: ${module.info.originalName}`);
+      await this.helper.processHelper.tsc(tscArgs, { cwd: module.root });
     }
     // end
     const timeEnd = new Date();
