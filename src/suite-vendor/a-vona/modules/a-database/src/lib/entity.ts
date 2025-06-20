fef868d0e0537e4ef28392c272ef1effb3bfc7ca
zhennann <zhen.nann@icloud.com>
2025-06-20 15:49:51 +0800

    feat: update

--- src/suite-vendor/a-vona/modules/a-database/src/lib/entity.ts
+++ src/suite-vendor/a-vona/modules/a-database/src/lib/entity.ts
@@ -4,9 +4,9 @@ import { mergeFieldsOpenAPIMetadata } from 'vona-module-a-openapi';
 
 // const __tableNames = new Set();
 
-export function Entity<T extends IDecoratorEntityOptions<any>>(options?: T): ClassDecorator;
-export function Entity<T extends IDecoratorEntityOptions<any>>(table?: string, options?: Omit<T, 'table'>): ClassDecorator;
-export function Entity<T extends IDecoratorEntityOptions<any>>(table?: T | string, options?: T): ClassDecorator {
+export function Entity<T extends IDecoratorEntityOptions<any>>(options?: Omit<T, '_fieldsMore_'>): ClassDecorator;
+export function Entity<T extends IDecoratorEntityOptions<any>>(table?: string, options?: Omit<T, '_fieldsMore_' | 'table'>): ClassDecorator;
+export function Entity<T extends IDecoratorEntityOptions<any>>(table?: T | string, options?: Omit<T, '_fieldsMore_'>): ClassDecorator {
   if (typeof table === 'string') {
     options = Object.assign({}, options, { table });
   } else {
