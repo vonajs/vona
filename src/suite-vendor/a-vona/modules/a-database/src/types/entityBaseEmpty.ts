8846cb5e8295cf64d8f65d2090a8239df267320c
zhennann <zhen.nann@icloud.com>
2025-06-21 16:21:17 +0800

    feat: update

--- src/suite-vendor/a-vona/modules/a-database/src/types/entityBaseEmpty.ts
+++ src/suite-vendor/a-vona/modules/a-database/src/types/entityBaseEmpty.ts
@@ -1,8 +1,1 @@
-import type { IDecoratorEntityOptions } from './onion/entity.ts';
-import { BeanBaseSimple, cast } from 'vona';
-
-export class EntityBaseEmpty extends BeanBaseSimple {
-  public get $table(): string {
-    return cast<IDecoratorEntityOptions>(this.$onionOptions).table!;
-  }
-}
+export class EntityBaseEmpty {}
