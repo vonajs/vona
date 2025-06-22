956a04d02e1fc96c012fa8c63da09c8d742956b3
zhennann <zhen.nann@icloud.com>
2025-06-22 11:47:41 +0800

    Update cel.ts

--- packages-utils/utils/src/cel.ts
+++ packages-utils/utils/src/cel.ts
@@ -5,27 +5,41 @@ import { isNil } from './check.ts';
 
 export const CeljsPrefix = '#!#';
 
-export function evaluateExpressions(expressions: any, context?: Record<string, unknown>, functions?: Record<string, CallableFunction>): unknown {
-  if (isNil(expressions)) return expressions;
+export function evaluateExpressions(
+  expressions: any,
+  context?: Record<string, unknown>,
+  functions?: Record<string, CallableFunction>,
+  dry?: boolean,
+): unknown {
+  if (isNil(expressions)) return _returnExpressionWithDry(expressions, dry);
   if (Array.isArray(expressions)) {
-    return expressions.map(item => _evaluateExpressionInner(item, context, functions));
+    return expressions.map(item => _evaluateExpressionInner(item, context, functions, dry));
   } else if (typeof expressions === 'object') {
     const res = {};
     for (const key in expressions) {
-      res[key] = _evaluateExpressionInner(expressions[key], context, functions);
+      res[key] = _evaluateExpressionInner(expressions[key], context, functions, dry);
     }
     return res;
   }
   // others
-  return _evaluateExpressionInner(expressions, context, functions);
+  return _evaluateExpressionInner(expressions, context, functions, dry);
 }
 
-function _evaluateExpressionInner(expression: any, context?: Record<string, unknown>, functions?: Record<string, CallableFunction>): unknown {
-  if (isNil(expression)) return expression;
-  if (typeof expression === 'object') return evaluateExpressions(expression, context, functions);
-  if (typeof expression !== 'string') return expression;
-  if (!expression.startsWith(CeljsPrefix)) return expression;
-  return evaluate(expression.substring(CeljsPrefix.length), context, functions);
+function _evaluateExpressionInner(
+  expression: any,
+  context?: Record<string, unknown>,
+  functions?: Record<string, CallableFunction>,
+  dry?: boolean,
+): unknown {
+  if (isNil(expression)) return _returnExpressionWithDry(expression, dry);
+  if (typeof expression === 'object') return evaluateExpressions(expression, context, functions, dry);
+  if (typeof expression !== 'string') return _returnExpressionWithDry(expression, dry);
+  if (!expression.startsWith(CeljsPrefix)) return _returnExpressionWithDry(expression, dry);
+  return dry ? true : evaluate(expression.substring(CeljsPrefix.length), context, functions);
+}
+
+function _returnExpressionWithDry(expression: any, dry?: boolean) {
+  return dry ? false : expression;
 }
 
 export function evaluate(
