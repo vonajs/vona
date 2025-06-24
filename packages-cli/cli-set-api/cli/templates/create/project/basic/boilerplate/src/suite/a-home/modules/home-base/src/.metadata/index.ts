be889275521c4b217f2a582408172ad9c47cf116
zhennann <zhen.nann@icloud.com>
2025-06-24 22:26:56 +0800

    feat: update

--- packages-cli/cli-set-api/cli/templates/create/project/basic/boilerplate/src/suite/a-home/modules/home-base/src/.metadata/index.ts
+++ packages-cli/cli-set-api/cli/templates/create/project/basic/boilerplate/src/suite/a-home/modules/home-base/src/.metadata/index.ts
@@ -1,87 +1,89 @@
-import type { BeanScopeUtil } from 'vona';
-
+/* eslint-disable */
 import type { TypeControllerOptionsActions } from 'vona-module-a-openapi';
-import type { IControllerOptionsMenu } from '../controller/menu.ts';
-/** service: end */
 /** service: begin */
+export * from '../service/menu.ts';
 
-/** controller: end */
-/** controller: begin */
-// @ts-ignore ignore
-import type { ControllerMenu } from '../controller/menu.ts';
+import 'vona';
+declare module 'vona-module-a-web' {
+  
+    export interface IServiceRecord {
+      'home-base:menu': never;
+    }
+
+  
+}
+declare module 'vona-module-home-base' {
+  
+        export interface ServiceMenu {
+          /** @internal */
+          get scope(): ScopeModuleHomeBase;
+        } 
+}
 /** service: end */
 /** service: begin */
 import type { ServiceMenu } from '../service/menu.ts';
-/** controller: end */
-/** scope: begin */
-import { BeanScopeBase } from 'vona';
-import { Scope } from 'vona-module-a-bean';
-import 'vona';
-import 'vona';
-import 'vona';
-
-import 'vona';
-/** service: end */
-/** controller: begin */
-export * from '../controller/menu.ts';
-declare module 'vona-module-a-web' {
-
-  export interface IServiceRecord {
-    'home-base:menu': never;
-  }
-
-}
-declare module 'vona-module-home-base' {
-
-  export interface ServiceMenu {
-    /** @internal */
-    get scope(): ScopeModuleHomeBase;
-  }
-}
 export interface IModuleService {
-  menu: ServiceMenu;
+  'menu': ServiceMenu;
 }
+/** service: end */
+/** service: begin */
+
+import 'vona';
 declare module 'vona' {
   export interface IBeanRecordGeneral {
     'home-base.service.menu': ServiceMenu;
   }
 }
-/** service: begin */
-export * from '../service/menu.ts';
+/** service: end */
+/** controller: begin */
+export * from '../controller/menu.ts';
+import type { IControllerOptionsMenu } from '../controller/menu.ts';
+import 'vona';
 declare module 'vona-module-a-web' {
+  
+    export interface IControllerRecord {
+      'home-base:menu': IControllerOptionsMenu;
+    }
 
-  export interface IControllerRecord {
-    'home-base:menu': IControllerOptionsMenu;
-  }
-
+  
 }
 declare module 'vona-module-home-base' {
-
-  export interface ControllerMenu {
-    /** @internal */
-    get scope(): ScopeModuleHomeBase;
-  }
+  
+        export interface ControllerMenu {
+          /** @internal */
+          get scope(): ScopeModuleHomeBase;
+        } 
 }
+/** controller: end */
+/** controller: begin */
+// @ts-ignore ignore
+import type { ControllerMenu } from '../controller/menu.ts';
 declare module 'vona-module-home-base' {
-
-  export interface IControllerOptionsMenu {
-    actions?: TypeControllerOptionsActions<ControllerMenu>;
-  }
+  
+    export interface IControllerOptionsMenu {
+      actions?: TypeControllerOptionsActions<ControllerMenu>;
+    }
 }
 declare module 'vona-module-a-web' {
-  export interface IApiPathGetRecord {
-    '/home/base/menu/:publicPath?': undefined;
-  }
+  export interface IApiPathGetRecord{
+        '/home/base/menu/:publicPath?': undefined;
+    }
 
 }
+/** controller: end */
+/** scope: begin */
+import { BeanScopeBase, type BeanScopeUtil } from 'vona';
+import { Scope } from 'vona-module-a-bean';
 
 @Scope()
 export class ScopeModuleHomeBase extends BeanScopeBase {}
 
 export interface ScopeModuleHomeBase {
   util: BeanScopeUtil;
-  service: IModuleService;
+service: IModuleService;
 }
+
+import 'vona';
 declare module 'vona' {
   export interface IBeanScopeRecord {
     'home-base': ScopeModuleHomeBase;
@@ -90,7 +92,10 @@ declare module 'vona' {
   export interface IBeanScopeContainer {
     homeBase: ScopeModuleHomeBase;
   }
+  
+  
 
+  
 }
 
 /** scope: end */
