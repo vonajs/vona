b8669a03baeec651e376e56248f2042e6c801a6f
zhennann <zhen.nann@icloud.com>
2025-06-26 17:50:11 +0800

    feat: update

--- src/suite-vendor/a-vona/modules/test-vona/src/service/caching.ts
+++ src/suite-vendor/a-vona/modules/test-vona/src/service/caching.ts
@@ -1,9 +1,11 @@
 import type { TSummerCacheTestData } from '../bean/summerCache.test.ts';
 import { BeanBase } from 'vona';
 import { Service } from 'vona-module-a-bean';
+import { Caching } from 'vona-module-a-caching';
 
 @Service()
 export class ServiceCaching extends BeanBase {
+  @Caching.get({ cacheName: 'test-vona:test', cacheKeyFn: 'cacheKey' })
   async get(id: number): Promise<TSummerCacheTestData> {
     return {
       id,
--- src/suite-vendor/a-vona/modules/test-vona/test/caching.test.ts
+++ src/suite-vendor/a-vona/modules/test-vona/test/caching.test.ts
@@ -2,10 +2,12 @@ import assert from 'node:assert';
 import { describe, it } from 'node:test';
 import { app } from 'vona-mock';
 
-describe('caching.test.ts', () => {
+describe.only('caching.test.ts', () => {
   it('action:caching', async () => {
     await app.bean.executor.mockCtx(async () => {
-
+      const scopeTest = app.bean.scope('test-vona');
+      const res = await scopeTest.service.caching.get(2);
+      assert.equal(res.id, 2);
     });
   });
 });
