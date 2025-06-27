2d5551e6a7e828536b60b009155baa9aa0477142
zhennann <zhen.nann@icloud.com>
2025-06-27 16:21:51 +0800

    Update aopMethod.cachingDel.ts

--- src/suite-vendor/a-vona/modules/a-caching/src/bean/aopMethod.cachingDel.ts
+++ src/suite-vendor/a-vona/modules/a-caching/src/bean/aopMethod.cachingDel.ts
@@ -1,15 +1,25 @@
 import type { Next } from 'vona';
 import type { IAopMethodExecute, IDecoratorAopMethodOptions } from 'vona-module-a-aspect';
 import type { TypeCachingActionOptions } from '../types/caching.ts';
-import { BeanAopMethodBase } from 'vona';
+import { isNil } from '@cabloy/utils';
+import { BeanAopMethodBase, beanFullNameFromOnionName } from 'vona';
 import { AopMethod } from 'vona-module-a-aspect';
+import { combineCachingKey } from '../lib/utils.ts';
 
 export interface IAopMethodOptionsCachingDel extends IDecoratorAopMethodOptions, TypeCachingActionOptions {}
 
 @AopMethod<IAopMethodOptionsCachingDel>()
 export class AopMethodCachingDel extends BeanAopMethodBase implements IAopMethodExecute {
-  async execute(_options: IAopMethodOptionsCachingDel, _args: [], next: Next, _receiver: any, _prop: string): Promise<any> {
+  async execute(options: IAopMethodOptionsCachingDel, args: [], next: Next, receiver: any, prop: string): Promise<any> {
     // next
-    return next();
+    const value = await next();
+    // key
+    const key = combineCachingKey(options, args, receiver, prop);
+    if (isNil(key) || key === false || key === '') return value;
+    // cache
+    const cache = this.bean.summer.cache(beanFullNameFromOnionName(options.cacheName, 'summerCache'));
+    await cache.del(key);
+    // ok
+    return value;
   }
 }
