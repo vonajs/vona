9ad4ed4ac3dc1f92951030311f1bdffb14abeb56
zhennann <zhen.nann@icloud.com>
2025-06-27 16:35:49 +0800

    Update aopMethod.cachingClear.ts

--- src/suite-vendor/a-vona/modules/a-caching/src/bean/aopMethod.cachingClear.ts
+++ src/suite-vendor/a-vona/modules/a-caching/src/bean/aopMethod.cachingClear.ts
@@ -1,15 +1,20 @@
 import type { Next } from 'vona';
 import type { IAopMethodExecute, IDecoratorAopMethodOptions } from 'vona-module-a-aspect';
 import type { TypeCachingActionClearOptions } from '../types/caching.ts';
-import { BeanAopMethodBase } from 'vona';
+import { BeanAopMethodBase, beanFullNameFromOnionName } from 'vona';
 import { AopMethod } from 'vona-module-a-aspect';
 
 export interface IAopMethodOptionsCachingClear extends IDecoratorAopMethodOptions, TypeCachingActionClearOptions {}
 
 @AopMethod<IAopMethodOptionsCachingClear>()
 export class AopMethodCachingClear extends BeanAopMethodBase implements IAopMethodExecute {
-  async execute(_options: IAopMethodOptionsCachingClear, _args: [], next: Next, _receiver: any, _prop: string): Promise<any> {
+  async execute(options: IAopMethodOptionsCachingClear, _args: [], next: Next, _receiver: any, _prop: string): Promise<any> {
     // next
-    return next();
+    const value = await next();
+    // cache
+    const cache = this.bean.summer.cache(beanFullNameFromOnionName(options.cacheName, 'summerCache'));
+    await cache.clear();
+    // ok
+    return value;
   }
 }
