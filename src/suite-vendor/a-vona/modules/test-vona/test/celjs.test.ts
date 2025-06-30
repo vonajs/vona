975ca7c825d4fb3672c15fd9648462133c6d70e5
zhennann <zhen.nann@icloud.com>
2025-06-30 11:52:09 +0800

    feat: update

--- src/suite-vendor/a-vona/modules/test-vona/test/celjs.test.ts
+++ src/suite-vendor/a-vona/modules/test-vona/test/celjs.test.ts
@@ -12,9 +12,12 @@ describe('celjs.test.ts', () => {
       assert.equal(evaluateExpressions('cel://self.app.config.server.workers', { self: beanJwt, app: cast(beanJwt).app, ctx: cast(beanJwt).ctx }) > 0, true);
       assert.equal(evaluateExpressions('cel://app.config.server.workers', { self: beanJwt, app: cast(beanJwt).app, ctx: cast(beanJwt).ctx }) > 0, true);
       assert.equal(evaluateExpressions('cel://ctx.app.config.server.workers', { self: beanJwt, app: cast(beanJwt).app, ctx: cast(beanJwt).ctx }) > 0, true);
-      //
+      // map
       assert.deepEqual(evaluateExpressions('cel://{"a": 1, "b": true}'), { a: 1, b: true });
       assert.deepEqual(evaluateExpressions('cel://{"id": 1, "name": "tom"}'), { id: 1, name: 'tom' });
+      // array
+      assert.deepEqual(evaluateExpressions('cel://concat(1,[2,3],4)'), [1, 2, 3, 4]);
+      assert.deepEqual(evaluateExpressions('cel://join(concat(1,[2,3],4),"_")'), '1_2_3_4');
     });
   });
 });
