12d2c9c9ca5b67af2ac594f4f9cd3f116dcb5eb3
zhennann <zhen.nann@icloud.com>
2025-07-05 16:12:38 +0800

    feat: update

--- src/suite-vendor/a-vona/modules/a-database/src/lib/model.ts
+++ src/suite-vendor/a-vona/modules/a-database/src/lib/model.ts
@@ -1,6 +1,6 @@
 import type { IDecoratorModelOptions } from '../types/onion/model.ts';
 import { createBeanDecorator } from 'vona';
 
-export function Model(options?: IDecoratorModelOptions): ClassDecorator {
+export function Model<T extends IDecoratorModelOptions>(options?: T): ClassDecorator {
   return createBeanDecorator('model', options);
 }
--- src/suite-vendor/a-vona/modules/test-vona/src/model/post.ts
+++ src/suite-vendor/a-vona/modules/test-vona/src/model/post.ts
@@ -1,5 +1,8 @@
+import type { IDecoratorModelOptions } from 'vona-module-a-database';
 import { BeanModelBase, Model } from 'vona-module-a-database';
 import { EntityPost } from '../entity/post.ts';
 
-@Model({ entity: EntityPost })
+export interface IModelOptionsPost extends IDecoratorModelOptions {}
+
+@Model<IModelOptionsPost>({ entity: EntityPost })
 export class ModelPost extends BeanModelBase<EntityPost> {}
