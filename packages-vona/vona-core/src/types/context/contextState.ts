60d3b5fa746ebf4abdbc6a60e1a93b1b56c39b68
zhennann <zhen.nann@icloud.com>
2025-07-06 21:06:23 +0800

    Update contextState.ts

--- packages-vona/vona-core/src/types/context/contextState.ts
+++ packages-vona/vona-core/src/types/context/contextState.ts
@@ -1,9 +1,1 @@
-export interface JwtPayload {
-  exp: number;
-  token: string;
-}
-
-export interface ContextState {
-  // todo: jwt可能不需要存入此处。因为已经解析出来state.user数据了
-  jwt?: JwtPayload; // not use null | undefined
-}
+export interface ContextState {}
