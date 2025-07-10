116fd48bafd0ad857a2c9298494a684cca87f453
zhennann <zhen.nann@icloud.com>
2025-07-10 21:21:21 +0800

    Update modelPro.ts

--- src/suite-vendor/a-vona/modules/a-database/src/types/modelPro.ts
+++ src/suite-vendor/a-vona/modules/a-database/src/types/modelPro.ts
@@ -1,8 +1,6 @@
-// not use Extract<keyof TRecord, 'string'>
 export type TypeModelWhere<TRecord> = {
   [prop in keyof TRecord]?: TRecord[prop] | Array<TRecord[prop]>;
 };
 
-// not use Extract<keyof TRecord, 'string'>
 export type TypeModelColumn<TRecord> = keyof TRecord | '*';
 export type TypeModelColumns<TRecord> = TypeModelColumn<TRecord> | Array<TypeModelColumn<TRecord>>;
