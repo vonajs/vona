254adf8c36f7646355db9ee4f8c0f8f54c3ff315
zhennann <zhen.nann@icloud.com>
2025-07-10 21:13:55 +0800

    Update relationsColumns.ts

--- src/suite-vendor/a-vona/modules/a-database/src/types/relationsColumns.ts
+++ src/suite-vendor/a-vona/modules/a-database/src/types/relationsColumns.ts
@@ -1,5 +1,5 @@
 import type { TypeConfirmArray, TypeRecordValues } from 'vona';
-import type { EntityPost, EntityPostMeta, ModelPost } from 'vona-module-test-vona';
+import type { EntityPost, EntityPostMeta, ModelPost, ModelUser } from 'vona-module-test-vona';
 import type { BeanModelMeta } from '../bean/bean.model/bean.model_meta.ts';
 import type { IDecoratorModelOptions, IModelClassRecord } from './onion/model.ts';
 import type { TypeSymbolKeyEntity, TypeSymbolKeyEntityMeta, TypeUtilGetModelOptions, TypeUtilGetRelationEntity, TypeUtilGetRelationEntityMeta, TypeUtilGetRelationModel } from './relations.ts';
@@ -10,7 +10,7 @@ export type TypeEntityTableColumnNamesShort<Entity> = keyof Entity;
 export type TypeEntityTableColumns<Entity extends {} | undefined, EntityMeta extends { $table: string } | undefined> = Entity extends { } ? EntityMeta extends { $table: string } ? { [K in keyof Entity as K extends string ? `${EntityMeta['$table']}.${K}` : never ]: Entity[K] } : {} : {};
 export type TypeEntityTableColumnsShort<Entity> = Entity;
 
-export type TypeEntityTableColumnsOfModelDirect<TModel extends BeanModelMeta>=TypeEntityTableColumns<TModel[TypeSymbolKeyEntity], TModel[TypeSymbolKeyEntityMeta]>;
+export type TypeEntityTableColumnsOfModelDirect<TModel extends BeanModelMeta | undefined> = TModel extends BeanModelMeta ? TypeEntityTableColumns<TModel[TypeSymbolKeyEntity], TModel[TypeSymbolKeyEntityMeta]> : {};
 
 export type TypeEntityTableColumnNamesOfModels<A extends BeanModelMeta[]> = TypeEntityTableColumnNames<A[number][TypeSymbolKeyEntityMeta]>;
 
@@ -21,9 +21,11 @@ export type TypeEntityTableColumnNamesOfModelOptions<TModelOptions extends IDeco
   [RelationName in keyof TModelOptions['relations']]: TypeEntityTableColumnNames<TypeUtilGetRelationEntityMeta<TModelOptions['relations'][RelationName]>>;
 }>;
 
-export type TypeEntityTableColumnsOfModelOptions<TModelOptions extends IDecoratorModelOptions> = TypeEntityTableColumnsOfModelDirect<TypeRecordValues<{
+export type TypeEntityTableColumnsOfModelOptions<TModelOptions extends IDecoratorModelOptions> = TypeEntityTableColumnsOfModelDirect<TypeRecordModelValues<{
   [RelationName in keyof TModelOptions['relations']]: TypeUtilGetRelationModel<TModelOptions['relations'][RelationName]>;
 }>>;
+
+export type TypeRecordModelValues<TRecord extends Record<string, BeanModelMeta | undefined>> = TRecord[keyof TRecord];
 
 export type TypeEntityTableColumnNamesOfModelClass<TModel extends BeanModelMeta> =
   TypeEntityTableColumnNamesOfModelOptions<TypeUtilGetModelOptions<TModel>>;
@@ -58,9 +60,3 @@ export type TypeEntityTableColumnsOfGeneral<
   TModelJoins extends (keyof IModelClassRecord) | (keyof IModelClassRecord)[] ?
       (TypeEntityTableColumnsOfModelJoins<TModelJoins> & TypeEntityTableColumnsOfModelSelf<TModel>) :
       (TypeEntityTableColumnsOfModelClass<TModel> & TypeEntityTableColumnsOfModelSelf<TModel>);
-
-const a: TypeEntityTableColumns<EntityPost, EntityPostMeta>;
-const b: TypeEntityTableColumnsOfModelJoins<'test-vona:post'>;
-const c: TypeEntityTableColumnsOfModelSelf<ModelPost>;
-const d: TypeEntityTableColumnsOfModelClass<ModelPost>;
-d.
