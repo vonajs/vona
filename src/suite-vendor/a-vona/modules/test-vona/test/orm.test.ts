2fc35de8a07ae51d6f68e726858a3c8f437e1f1b
zhennann <zhen.nann@icloud.com>
2025-07-10 11:52:55 +0800

    Update orm.test.ts

--- src/suite-vendor/a-vona/modules/test-vona/test/orm.test.ts
+++ src/suite-vendor/a-vona/modules/test-vona/test/orm.test.ts
@@ -1,17 +1,17 @@
 import { describe, it } from 'node:test';
 import { app } from 'vona-mock';
 
-describe('orm.test.ts', () => {
+describe.only('orm.test.ts', () => {
   it('action:orm:hasOne', async () => {
     await app.bean.executor.mockCtx(async () => {
       // scope
       const scopeTest = app.bean.scope('test-vona');
       // create: post/postContent
       const post1 = await scopeTest.model.post.insert({
-        title: 'post1',
+        title: 'orm:hasOne:post1',
       });
       const postContent1 = await scopeTest.model.postContent.insert({
-        content: 'content',
+        content: 'orm:hasOne:content',
         postId: post1.id,
       });
       // delete
