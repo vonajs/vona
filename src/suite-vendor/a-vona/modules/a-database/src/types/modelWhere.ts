76989c6b9c24019098d6991dac371e468fb4d114
zhennann <zhen.nann@icloud.com>
2025-07-11 16:26:04 +0800

    Update modelWhere.ts

--- src/suite-vendor/a-vona/modules/a-database/src/types/modelWhere.ts
+++ src/suite-vendor/a-vona/modules/a-database/src/types/modelWhere.ts
@@ -21,8 +21,10 @@ export type TypeOpsAll = TypeRecordValues<typeof Op>;
 
 export type TypeModelColumnValue<TRecord, Column> = keyof TRecord | Column | Column[] | Knex.Raw | '_skip_';
 
-export type TypeModelWhere<TRecord, Columns extends {} | undefined = undefined> =
-  Columns extends {} ? TypeModelWhereInner<Columns> | Knex.Raw : TypeModelWhereInner<TRecord> | Knex.Raw;
+export type TypeModelWhere<TRecord, Columns extends {} | undefined = undefined> = 
+  Columns extends {} ? TypeModelWhereInner<Columns> : TypeModelWhereInner<TRecord>;
+  // not use Knex.Raw
+  // Columns extends {} ? TypeModelWhereInner<Columns> | Knex.Raw : TypeModelWhereInner<TRecord> | Knex.Raw;
 
 export type TypeModelWhereInner<TRecord> = {
   [prop in keyof TRecord]?: TypeModelColumnValue<TRecord, TRecord[prop]> | TypeModelWhereFieldAll<TRecord, TRecord[prop]>;
