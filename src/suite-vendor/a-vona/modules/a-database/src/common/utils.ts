bcac80a5328edd1e45c8965c8ce7c8acac02517a
zhennann <zhen.nann@icloud.com>
2025-07-12 15:09:47 +0800

    Update utils.ts

--- src/suite-vendor/a-vona/modules/a-database/src/common/utils.ts
+++ src/suite-vendor/a-vona/modules/a-database/src/common/utils.ts
@@ -2,6 +2,11 @@ export function isRaw(raw) {
   return typeof raw?.constructor === 'function' && raw?.constructor?.name === 'Raw';
 }
 
+export function getTableOrTableAlias(table: string) {
+  const _table = table.toString();
+  return _table.includes(' as ') ? _table.split(' as ')[1].trim() : _table;
+}
+
 // export function formatValue(value) {
 //   if (typeof value !== 'object' || value instanceof Date) return value;
 //   // date
@@ -24,11 +29,6 @@ export function isRaw(raw) {
 //   const arr = typeof value.val === 'string' ? value.val.split(',') : value.val;
 //   if (arr.length === 0) return null;
 //   return arr;
-// }
-
-// export function getTableOrTableAlias(table: string) {
-//   const _table = table.toString();
-//   return _table.includes(' as ') ? _table.split(' as ')[1].trim() : _table;
 // }
 
 // export function getTargetColumnName(column: string) {
