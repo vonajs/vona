935989cce108266d1b4936c23475181456a190d1
zhennann <zhen.nann@icloud.com>
2025-07-13 22:04:32 +0800

    Update cacheQuery.ts

--- src/suite-vendor/a-vona/modules/a-database/src/service/cacheQuery.ts
+++ src/suite-vendor/a-vona/modules/a-database/src/service/cacheQuery.ts
@@ -1,5 +1,10 @@
-import { BeanBase } from 'vona';
+import type { BeanModelCache } from '../bean/bean.model/bean.model_cache.ts';
 import { Service } from 'vona-module-a-bean';
+import { ModelCacheBase } from '../lib/modelCacheBase.ts';
 
 @Service()
-export class ServiceCacheQuery extends BeanBase {}
+export class ServiceCacheQuery extends ModelCacheBase {
+  protected __init__(model: BeanModelCache) {
+    super.__init__(model, 'query');
+  }
+}
