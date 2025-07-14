2878f147624ca6e44932558d106b4ef8683bfca0
zhennann <zhen.nann@icloud.com>
2025-07-14 22:31:57 +0800

    Update config.ts

--- src/suite-vendor/a-vona/modules/a-auth/src/config/config.ts
+++ src/suite-vendor/a-vona/modules/a-auth/src/config/config.ts
@@ -2,6 +2,6 @@ import type { VonaApplication } from 'vona';
 
 export function config(_app: VonaApplication) {
   return {
-    oauthCodeField: 'x-vona-passort-code',
+    oauthCodeField: 'x-vona-oauth-code',
   };
 }
