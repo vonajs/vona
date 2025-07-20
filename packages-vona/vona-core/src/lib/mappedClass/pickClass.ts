f3c1d6fcb4a15d12ac4a3d5b90e73b05f5d35276
zhennann <zhen.nann@icloud.com>
2025-07-20 22:26:46 +0800

    Update pickClass.ts

--- packages-vona/vona-core/src/lib/mappedClass/pickClass.ts
+++ packages-vona/vona-core/src/lib/mappedClass/pickClass.ts
@@ -1,18 +1,26 @@
 import type { Constructable } from '../decorator/type/constructable.ts';
 import { copyMetadataOfClasses, copyPropertiesOfClasses } from './utils.ts';
 
+export function PickClass<T>(
+  classRef: Constructable<T>,
+  keys?: undefined,
+): Constructable<T>;
 export function PickClass<T, K extends keyof T>(
   classRef: Constructable<T>,
   keys: K[],
-): Constructable<Pick<T, (typeof keys)[number]>> {
+): Constructable<Pick<T, (typeof keys)[number]>>;
+export function PickClass<T, K extends keyof T>(
+  classRef: Constructable<T>,
+  keys?: K[],
+) {
   abstract class TargetClass {}
   copyMetadataOfClasses(TargetClass.prototype, [classRef.prototype], (rules, key) => {
-    if (keys.includes(key)) {
+    if (!keys || keys.includes(key)) {
       return rules[key];
     }
   });
   copyPropertiesOfClasses(TargetClass as any, [classRef], key => {
-    return keys.includes(key);
+    return !keys || keys.includes(key);
   });
   return TargetClass as any;
 }
