8a2fba5b9f93ba42a3efff60785ef5884113d959
zhennann <zhen.nann@icloud.com>
2025-07-23 21:02:12 +0800

    feat: update

---
+++ src/suite-vendor/a-vona/modules/test-vona/test/database/modelRelationsMutate.test.ts
@@ -0,0 +1,30 @@
+import assert from 'node:assert';
+import { describe, it } from 'node:test';
+import { app } from 'vona-mock';
+
+describe('modelRelationsMutate.test.ts', () => {
+  it('action:modelRelationsMutate', async () => {
+    await app.bean.executor.mockCtx(async () => {
+      const prefix = 'action:modelRelationsMutate';
+      // scope
+      const scopeTest = app.bean.scope('test-vona');
+      // insert: roles
+      const roles = await scopeTest.model.role.insertBulk([
+        { name: `${prefix}:family` },
+        { name: `${prefix}:friend` },
+      ]);
+      assert.equal(roles.length, 2);
+      assert.equal(roles[0].id !== undefined, true);
+      // delete: roles
+      await scopeTest.model.role.deleteBulk(roles.map(item => item.id));
+      const roles2 = await scopeTest.model.role.select({
+        where: {
+          id: roles.map(item => item.id),
+        },
+      }, { disableDeleted: true });
+      assert.equal(roles2.length, 2);
+      assert.equal(roles2[0].id !== undefined, true);
+      assert.equal(roles2[0].deleted, true);
+    });
+  });
+});
