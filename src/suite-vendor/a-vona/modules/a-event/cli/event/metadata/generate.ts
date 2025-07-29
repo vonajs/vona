3795e80cb15d430f0daf6f7994923cb401f0c1b8
zhennann <zhen.nann@icloud.com>
2025-07-29 15:14:55 +0800

    Update generate.ts

--- src/suite-vendor/a-vona/modules/a-event/cli/event/metadata/generate.ts
+++ src/suite-vendor/a-vona/modules/a-event/cli/event/metadata/generate.ts
@@ -1,13 +1,11 @@
 import type { IMetadataCustomGenerateOptions } from '@cabloy/cli';
-import { toUpperCaseFirstChar } from '@cabloy/word-utils';
 
 export default async function (options: IMetadataCustomGenerateOptions): Promise<string> {
   const { sceneName, globFiles } = options;
   const contentImports: string[] = [];
   const contentRecords: string[] = [];
   for (const globFile of globFiles) {
-    const { beanName, beanNameFull, fileNameJSRelative } = globFile;
-    const beanNameCapitalize = toUpperCaseFirstChar(beanName);
+    const { beanNameCapitalize, beanNameFull, fileNameJSRelative } = globFile;
     const typeData = `TypeEvent${beanNameCapitalize}Data`;
     const typeResult = `TypeEvent${beanNameCapitalize}Result`;
     contentImports.push(`import type { ${typeData}, ${typeResult} } from '${fileNameJSRelative}';`);
