2aaaf825b4a0bf3bdd3221670f35523d77bd19ef
zhennann <zhen.nann@icloud.com>
2025-07-30 15:51:18 +0800

    Update model.ts

--- src/suite-vendor/a-vona/modules/a-orm/src/types/model.ts
+++ src/suite-vendor/a-vona/modules/a-orm/src/types/model.ts
@@ -1,7 +1,6 @@
 import type { Knex } from 'knex';
 import type { BeanModelMeta } from '../bean/bean.model/bean.model_meta.ts';
 import type { TypeModelColumn, TypeModelColumns, TypeModelColumnsPatch, TypeModelWhere } from './modelWhere.ts';
-import type { IModelClassRecord } from './onion/model.ts';
 import type { TypeModelParamsInclude, TypeModelsClassLikeGeneral } from './relations.ts';
 import type { TypeEntityTableColumnNamesOfGeneral, TypeEntityTableColumnsOfGeneral } from './relationsColumns.ts';
 import type { TypeModelMutateParamsInclude } from './relationsMutate.ts';
