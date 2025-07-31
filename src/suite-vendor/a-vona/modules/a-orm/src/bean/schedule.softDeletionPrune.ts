388a4471a842c1b66dfa5d4b217291087ff6f065
zhennann <zhen.nann@icloud.com>
2025-07-31 15:09:37 +0800

    Update schedule.softDeletionPrune.ts

--- src/suite-vendor/a-vona/modules/a-orm/src/bean/schedule.softDeletionPrune.ts
+++ src/suite-vendor/a-vona/modules/a-orm/src/bean/schedule.softDeletionPrune.ts
@@ -5,9 +5,7 @@ import type { BeanModelCache } from './bean.model/bean.model_cache.ts';
 import { BeanBase } from 'vona';
 import { Schedule } from 'vona-module-a-schedule';
 
-@Schedule({ repeat: {
-  every: 24 * 3600 * 1000,
-} })
+@Schedule({ repeat: { every: 24 * 3600 * 1000 } })
 export class ScheduleSoftDeletionPrune extends BeanBase implements IScheduleExecute {
   async execute() {
     const onionSlices = this.bean.onion.model.getOnionsEnabled();
