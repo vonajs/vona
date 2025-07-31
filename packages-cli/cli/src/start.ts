ea148958ad48a67db9f9d9d316160d4921ecaf4b
zhennann <zhen.nann@icloud.com>
2025-07-31 10:21:22 +0800

    Update start.ts

--- packages-cli/cli/src/start.ts
+++ packages-cli/cli/src/start.ts
@@ -1,7 +1,15 @@
+import { createRequire } from 'node:module';
 import { CabloyCommand } from '@cabloy/cli';
 
 export class VonaCommand extends CabloyCommand {
   constructor(rawArgv?) {
     super('vona', rawArgv);
+    this.version = this._extractPackageVersion();
+  }
+
+  private _extractPackageVersion() {
+    const require = createRequire(import.meta.url);
+    const pkg = require('../package.json');
+    return pkg.version;
   }
 }
