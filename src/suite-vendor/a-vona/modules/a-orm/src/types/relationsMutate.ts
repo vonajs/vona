f5016626e1ce270e1debbaf1ca181d154efbb2a2
zhennann <zhen.nann@icloud.com>
2025-08-23 10:27:24 +0800

    Update relationsMutate.ts

--- src/suite-vendor/a-vona/modules/a-orm/src/types/relationsMutate.ts
+++ src/suite-vendor/a-vona/modules/a-orm/src/types/relationsMutate.ts
@@ -21,47 +21,65 @@ export type TypeModelMutateParamsRelationOptions<Relation> =
       with?: Record<string, unknown>;
     });
 
-export type TypeModelMutateRelationResultMergeInclude<TModelOptions extends IDecoratorModelOptions, TInclude extends {} | undefined | unknown> = {
+export type TypeModelMutateRelationResultMergeInclude<
+  TModelOptions extends IDecoratorModelOptions,
+  TInclude extends {} | undefined | unknown,
+  TForInsertResult extends boolean | undefined = undefined,
+> = {
   [RelationName in (keyof TModelOptions['relations'])]: // not use ?: for OmitNever take effect
   TInclude extends {} ?
     TInclude[RelationName] extends {} | boolean ?
-      TypeModelMutateRelationResultMergeIncludeWrapper<TModelOptions['relations'][RelationName], TInclude[RelationName]> :
-      TypeModelMutateRelationResultMergeAutoload<TModelOptions['relations'][RelationName]> :
-    TypeModelMutateRelationResultMergeAutoload<TModelOptions['relations'][RelationName]>;
+      TypeModelMutateRelationResultMergeIncludeWrapper<TModelOptions['relations'][RelationName], TInclude[RelationName], TForInsertResult> :
+      TypeModelMutateRelationResultMergeAutoload<TModelOptions['relations'][RelationName], TForInsertResult> :
+    TypeModelMutateRelationResultMergeAutoload<TModelOptions['relations'][RelationName], TForInsertResult>;
 };
 
-export type TypeModelMutateRelationResultMergeAutoload<Relation> =
-  TypeUtilGetRelationOptionsAutoload<Relation> extends true ? TypeUtilMutateGetRelationEntityByType<Relation, undefined> : never;
+export type TypeModelMutateRelationResultMergeAutoload<Relation, TForInsertResult extends boolean | undefined = undefined> =
+  TypeUtilGetRelationOptionsAutoload<Relation> extends true ? TypeUtilMutateGetRelationEntityByType<Relation, undefined, TForInsertResult> : never;
 
-export type TypeModelMutateRelationResultMergeIncludeWrapper<Relation, IncludeWrapper> =
+export type TypeModelMutateRelationResultMergeIncludeWrapper<Relation, IncludeWrapper, TForInsertResult extends boolean | undefined = undefined> =
   IncludeWrapper extends false ? never :
   IncludeWrapper extends true ?
-    TypeUtilMutateGetRelationEntityByType<Relation, undefined> :
-    IncludeWrapper extends {} ? TypeUtilMutateGetRelationEntityByType<Relation, IncludeWrapper> : never;
+    TypeUtilMutateGetRelationEntityByType<Relation, undefined, TForInsertResult> :
+    IncludeWrapper extends {} ? TypeUtilMutateGetRelationEntityByType<Relation, IncludeWrapper, TForInsertResult> : never;
 
-export type TypeUtilMutateGetRelationEntityByType<Relation, IncludeWrapper extends {} | undefined | unknown> =
+export type TypeUtilMutateGetRelationEntityByType<
+  Relation,
+  IncludeWrapper extends {} | undefined | unknown,
+  TForInsertResult extends boolean | undefined = undefined,
+> =
   TypeUtilMutateGetEntityByType<
     TypeUtilGetRelationEntity<Relation>,
     TypeUtilGetRelationType<Relation>,
     TypeUtilGetRelationModel<Relation>,
-    IncludeWrapper
+    IncludeWrapper,
+    TForInsertResult
   >;
 
-export type TypeUtilMutateGetEntityByType<TRecord, TYPE, TModel extends BeanModelMeta | undefined, IncludeWrapper extends {} | undefined | unknown> =
+export type TypeUtilMutateGetEntityByType<
+  TRecord,
+  TYPE,
+  TModel extends BeanModelMeta | undefined,
+  IncludeWrapper extends {} | undefined | unknown,
+  TForInsertResult extends boolean | undefined = undefined,
+> =
     TYPE extends 'belongsTo' ? never :
     TYPE extends 'belongsToMany' ? Array<{ id: TableIdentity; deleted?: boolean }> | undefined :
-    TYPE extends 'hasMany' ? Array<TypeModelMutateRelationData<TRecord, TModel, IncludeWrapper>> | undefined : TypeModelMutateRelationData<TRecord, TModel, IncludeWrapper> | undefined;
+    TYPE extends 'hasMany' ? Array<TypeModelMutateRelationData<TRecord, TModel, IncludeWrapper, TForInsertResult>> | undefined : TypeModelMutateRelationData<TRecord, TModel, IncludeWrapper, TForInsertResult> | undefined;
 
-export type TypeModelMutateRelationResultMergeWith<TWith extends {} | undefined | unknown> =
+export type TypeModelMutateRelationResultMergeWith<
+  TWith extends {} | undefined | unknown,
+  TForInsertResult extends boolean | undefined = undefined,
+> =
   TWith extends {} ?
-      { [RelationName in (keyof TWith)]: TypeModelMutateRelationResultMergeWithRelation<TWith[RelationName]> }
+      { [RelationName in (keyof TWith)]: TypeModelMutateRelationResultMergeWithRelation<TWith[RelationName], TForInsertResult> }
     : {};
 
-export type TypeModelMutateRelationResultMergeWithRelation<WithRelation> =
+export type TypeModelMutateRelationResultMergeWithRelation<WithRelation, TForInsertResult extends boolean | undefined = undefined> =
   WithRelation extends false ? never :
   WithRelation extends true ?
     never :
-    WithRelation extends {} ? TypeUtilMutateGetRelationEntityByType<WithRelation, TypeUtilGetRelationOptions<WithRelation>> : never;
+    WithRelation extends {} ? TypeUtilMutateGetRelationEntityByType<WithRelation, TypeUtilGetRelationOptions<WithRelation>, TForInsertResult> : never;
 
 export type TypeModelMutateRelationData<
   TRecord,
@@ -72,6 +90,10 @@ export type TypeModelMutateRelationData<
   (TForInsertResult extends true ? TRecord : Partial<TRecord>) &
   (TModel extends BeanModelMeta ?
       (
-      Partial<OmitNever<TypeModelMutateRelationResultMergeInclude<TypeUtilGetModelOptions<TModel>, TypeUtilGetParamsInlcude<TOptionsRelation>>>> &
-      Partial<OmitNever<TypeModelMutateRelationResultMergeWith<TypeUtilGetParamsWith<TOptionsRelation>>>>
+      Partial<OmitNever<TypeModelMutateRelationResultMergeInclude<
+        TypeUtilGetModelOptions<TModel>,
+        TypeUtilGetParamsInlcude<TOptionsRelation>,
+        TForInsertResult
+      >>> &
+      Partial<OmitNever<TypeModelMutateRelationResultMergeWith<TypeUtilGetParamsWith<TOptionsRelation>, TForInsertResult>>>
       ) : {});
