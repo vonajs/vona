67d9ff25289dfad508f4da0158737d0c6737874f
zhennann <zhen.nann@icloud.com>
2025-08-29 14:49:59 +0800

    Update middleware.captcha.ts

--- src/suite-vendor/a-vona/modules/a-captcha/src/bean/middleware.captcha.ts
+++ src/suite-vendor/a-vona/modules/a-captcha/src/bean/middleware.captcha.ts
@@ -15,9 +15,12 @@ export interface IMiddlewareOptionsCaptcha extends IDecoratorMiddlewareOptions {
 })
 export class MiddlewareCaptcha extends BeanBase implements IMiddlewareExecute {
   async execute(options: IMiddlewareOptionsCaptcha, next: Next) {
+    const sceneName = options.scene;
+    if (!sceneName) throw new Error('please specify the captcha scene name');
     // captcha
-    const captcha: DtoCaptchaVerify = this.ctx.request.body[options.bodyField!];
-    const verified = await this.bean.captcha.verify(captcha.id, captcha.token, options.scene!);
+    const captcha: DtoCaptchaVerify | undefined = this.ctx.request.body[options.bodyField!];
+    if (!captcha) throw new Error('not found captcha data');
+    const verified = await this.bean.captcha.verify(captcha.id, captcha.token, sceneName);
     if (!verified) this.app.throw(403);
     // next
     return next();
