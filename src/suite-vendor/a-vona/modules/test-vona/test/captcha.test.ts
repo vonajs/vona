de20a987522b64d1959e42c1b842dcb33f9bb369
zhennann <zhen.nann@icloud.com>
2025-08-29 11:12:01 +0800

    Update captcha.test.ts

--- src/suite-vendor/a-vona/modules/test-vona/test/captcha.test.ts
+++ src/suite-vendor/a-vona/modules/test-vona/test/captcha.test.ts
@@ -5,19 +5,22 @@ import { app } from 'vona-mock';
 describe('captcha.test.ts', () => {
   it('action:captcha', async () => {
     await app.bean.executor.mockCtx(async () => {
+      // scene/provider
+      const sceneName = 'a-captchasimple:simple';
+      const providerName = 'a-captchasimple:simple';
       // create
-      const captcha = await app.bean.captcha.create('a-captchasimple:simple');
-      assert.equal(captcha.provider, 'a-captchasimple:simple');
+      const captcha = await app.bean.captcha.create(sceneName);
+      assert.equal(captcha.provider, providerName);
       // get
       const captchaData = await app.bean.captcha.getCaptchaData(captcha.id);
-      assert.equal(captchaData?.provider, 'a-captchasimple:simple');
+      assert.equal(captchaData?.provider, providerName);
       // token
       const token = captchaData?.token;
       // verify: false
-      const verifiedFalse = await app.bean.captcha.verify(captcha.id, `${token}!`);
+      const verifiedFalse = await app.bean.captcha.verify(captcha.id, `${token}!`, sceneName);
       assert.equal(verifiedFalse, false);
       // verify: true
-      const verifiedTrue = await app.bean.captcha.verify(captcha.id, token);
+      const verifiedTrue = await app.bean.captcha.verify(captcha.id, token, sceneName);
       assert.equal(verifiedTrue, true);
     });
   });
