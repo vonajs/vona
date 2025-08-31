9ba9473c907ff587511622c978ee1b4d6ea9f283
zhennann <zhen.nann@icloud.com>
2025-08-31 21:35:28 +0800

    Update captchaProvider.imageText.ts

--- src/suite-vendor/a-vona/modules/a-captchasimple/src/bean/captchaProvider.imageText.ts
+++ src/suite-vendor/a-vona/modules/a-captchasimple/src/bean/captchaProvider.imageText.ts
@@ -6,45 +6,45 @@ import { BeanBase, cast } from 'vona';
 import { CaptchaProvider } from 'vona-module-a-captcha';
 import { __ThisModule__ } from '../.metadata/this.ts';
 
-export type TypeCaptchaProviderSimpleToken = string;
-export type TypeCaptchaProviderSimplePayload = string;
-export type TypeCaptchaProviderSimpleData = ICaptchaProviderData<TypeCaptchaProviderSimpleToken, TypeCaptchaProviderSimplePayload>;
+export type TypeCaptchaProviderImageTextToken = string;
+export type TypeCaptchaProviderImageTextPayload = string;
+export type TypeCaptchaProviderImageTextData = ICaptchaProviderData<TypeCaptchaProviderImageTextToken, TypeCaptchaProviderImageTextPayload>;
 
-export type TypeCaptchaProviderSimpleType = 'char' | 'math';
-const CaptchaProviderSimpleTypes = ['char', 'math'] as const;
-export interface ICaptchaProviderOptionsSimple extends IDecoratorCaptchaProviderOptions {
-  type?: TypeCaptchaProviderSimpleType;
+export type TypeCaptchaProviderImageTextType = 'char' | 'math';
+const CaptchaProviderImageTextTypes = ['char', 'math'] as const;
+export interface ICaptchaProviderOptionsImageText extends IDecoratorCaptchaProviderOptions {
+  type?: TypeCaptchaProviderImageTextType;
   fontPath?: string;
   opts: ConfigObject;
 }
 
-@CaptchaProvider<ICaptchaProviderOptionsSimple>({
+@CaptchaProvider<ICaptchaProviderOptionsImageText>({
   opts: {
     size: 4,
     color: true,
   },
 })
-export class CaptchaProviderSimple
-  extends BeanBase implements ICaptchaProviderExecute<TypeCaptchaProviderSimpleToken, TypeCaptchaProviderSimplePayload> {
-  async create(options: ICaptchaProviderOptionsSimple): Promise<TypeCaptchaProviderSimpleData> {
+export class CaptchaProviderImageText
+  extends BeanBase implements ICaptchaProviderExecute<TypeCaptchaProviderImageTextToken, TypeCaptchaProviderImageTextPayload> {
+  async create(options: ICaptchaProviderOptionsImageText): Promise<TypeCaptchaProviderImageTextData> {
     this._confirmFont(options);
     let type = options.type;
     if (!type) {
-      type = CaptchaProviderSimpleTypes[getRandomInt(2, 0)];
+      type = CaptchaProviderImageTextTypes[getRandomInt(2, 0)];
     }
     const captcha = type === 'char' ? svgCaptcha.create(options.opts) : svgCaptcha.createMathExpr(options.opts);
     return { token: captcha.text, payload: svg64(captcha.data) };
   }
 
   async verify(
-    token: TypeCaptchaProviderSimpleToken,
-    tokenInput: TypeCaptchaProviderSimpleToken,
-    _options: ICaptchaProviderOptionsSimple,
+    token: TypeCaptchaProviderImageTextToken,
+    tokenInput: TypeCaptchaProviderImageTextToken,
+    _options: ICaptchaProviderOptionsImageText,
   ): Promise<boolean> {
     return !!tokenInput && !!token && tokenInput.toLowerCase() === token.toLowerCase();
   }
 
-  private _confirmFont(options: ICaptchaProviderOptionsSimple) {
+  private _confirmFont(options: ICaptchaProviderOptionsImageText) {
     if (cast(svgCaptcha.options).font) return;
     const url = options.fontPath || this.app.util.getAssetPathPhysical(__ThisModule__, 'fonts', 'Comismsh.ttf')!;
     svgCaptcha.loadFont(url);
