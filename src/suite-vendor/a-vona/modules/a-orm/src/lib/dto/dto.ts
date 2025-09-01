4eda15f683f8d6873a58503859254bb056d53c8b
zhennann <zhen.nann@icloud.com>
2025-09-01 15:52:29 +0800

    feat: update

--- src/suite-vendor/a-vona/modules/a-orm/src/lib/dto/dto.ts
+++ src/suite-vendor/a-vona/modules/a-orm/src/lib/dto/dto.ts
@@ -2,6 +2,7 @@ import { DtoAggregate } from './dtoAggregate.ts';
 import { DtoCreate } from './dtoCreate.ts';
 import { DtoGet } from './dtoGet.ts';
 import { DtoGroup } from './dtoGroup.ts';
+import { DtoQuery } from './dtoQuery.ts';
 import { DtoQueryPage } from './dtoQueryPage.ts';
 import { DtoUpdate } from './dtoUpdate.ts';
 
@@ -11,5 +12,6 @@ export const $Dto = {
   get: DtoGet,
   aggregate: DtoAggregate,
   group: DtoGroup,
+  query: DtoQuery,
   queryPage: DtoQueryPage,
 };
--- src/suite-vendor/a-vona/modules/test-vona/src/dto/postQuery.ts
+++ src/suite-vendor/a-vona/modules/test-vona/src/dto/postQuery.ts
@@ -1,13 +1,9 @@
 import type { IDecoratorDtoOptions } from 'vona-module-a-web';
-import {  $Dto  } from 'vona-module-a-orm';
+import { $Dto } from 'vona-module-a-orm';
 import { Dto } from 'vona-module-a-web';
 import { EntityPost } from '../entity/post.ts';
 
 export interface IDtoOptionsPostQuery extends IDecoratorDtoOptions {}
 
 @Dto<IDtoOptionsPostQuery>()
-export class DtoPostQuery extends $Dto.queryPage(EntityPost,['title','createdAt']) {
-  tt() {
-    this.
-  }
-}
+export class DtoPostQuery extends $Dto.queryPage(EntityPost, ['title', 'createdAt']) {}
