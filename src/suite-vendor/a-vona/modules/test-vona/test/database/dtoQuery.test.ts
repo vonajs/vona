564933130209a3620e5ccd015ac8f46fcd0c6879
zhennann <zhen.nann@icloud.com>
2025-09-01 20:58:26 +0800

    Create dtoQuery.test.ts

---
+++ src/suite-vendor/a-vona/modules/test-vona/test/database/dtoQuery.test.ts
@@ -0,0 +1,9 @@
+import { describe, it } from 'node:test';
+import { app } from 'vona-mock';
+
+describe('dtoQuery.test.ts', () => {
+  it('action:dtoQuery', async () => {
+    await app.bean.executor.mockCtx(async () => {
+    });
+  });
+});
