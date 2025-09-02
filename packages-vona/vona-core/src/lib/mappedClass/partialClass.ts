53af89a60f436d3e4701d7a313ed6594f573ec82
zhennann <zhen.nann@icloud.com>
2025-09-02 10:43:28 +0800

    Update partialClass.ts

--- packages-vona/vona-core/src/lib/mappedClass/partialClass.ts
+++ packages-vona/vona-core/src/lib/mappedClass/partialClass.ts
@@ -1,12 +1,13 @@
 import type { Constructable } from '../decorator/type/constructable.ts';
 import { copyMetadataOfClasses, copyPropertiesOfClasses } from './utils.ts';
 
-export function PartialClass<T>(classRef: Constructable<T>): Constructable<Partial<T>>;
-export function PartialClass<T, K extends keyof T>(
+export type TypePartialClass<T, KEYS extends Array<keyof T> | undefined = undefined> =
+  KEYS extends any[] ? Constructable<Partial<Pick<T, KEYS[number]>> & Omit<T, KEYS[number]>> : Constructable<Partial<T>>;
+
+export function PartialClass<T, KEYS extends Array<keyof T> | undefined = undefined>(
   classRef: Constructable<T>,
-  keys: K[],
-): Constructable<Partial<Pick<T, (typeof keys)[number]>> & Omit<T, (typeof keys)[number]>>;
-export function PartialClass<T, K extends keyof T>(classRef: Constructable<T>, keys?: K[]): any {
+  keys?: KEYS,
+): TypePartialClass<T, KEYS> {
   abstract class TargetClass {}
   copyMetadataOfClasses(TargetClass.prototype, [classRef.prototype], (rules, key, metadataKeyOptions) => {
     if (keys && !keys.includes(key)) return rules[key];
