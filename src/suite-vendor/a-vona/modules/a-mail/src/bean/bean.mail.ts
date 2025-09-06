0438301c6daced2ce4ed708c19a8bc878f4a5836
zhennann <zhen.nann@icloud.com>
2025-09-06 18:20:58 +0800

    Update bean.mail.ts

--- src/suite-vendor/a-vona/modules/a-mail/src/bean/bean.mail.ts
+++ src/suite-vendor/a-vona/modules/a-mail/src/bean/bean.mail.ts
@@ -1,3 +1,4 @@
+import type { Address } from 'nodemailer/lib/mailer/index.js';
 import type { IMailClientRecord, IMailOptions } from '../types/config.ts';
 import nodemailer from 'nodemailer';
 import { BeanBase } from 'vona';
@@ -6,15 +7,24 @@ import { Bean } from 'vona-module-a-bean';
 @Bean()
 export class BeanMail extends BeanBase {
   async send(mail: IMailOptions, clientName?: keyof IMailClientRecord) {
-    const transporter = nodemailer.createTransport({
-      host: 'smtp.ethereal.email',
-      port: 587,
-      secure: false, // true for 465, false for other ports
-      auth: {
-        user: 'maddison53@ethereal.email',
-        pass: 'jn7jnAPss4f63QBp6D',
-      },
+    await this.scope.model.mail.insert({
+      clientName,
+      from: __parseFrom(mail.from),
+      to: __parseTo(mail.to),
+      subject: mail.subject,
+      message: mail,
     });
-    transporter.sendMail();
   }
+}
+
+function __parseFrom(address: string | Address | undefined): string | undefined {
+  if (!address) return address;
+  if (typeof address === 'object') return address.address;
+  return address;
+}
+
+function __parseTo(address: string | Address | Array<string | Address> | undefined): string | undefined {
+  if (!address) return address;
+  if (!Array.isArray(address)) return __parseFrom(address);
+  return address.map(item => __parseFrom(item)).join(',');
 }
