33c06aea7a6bebfcac9235d4fca6e82642255af5
zhennann <zhen.nann@icloud.com>
2025-09-09 22:18:24 +0800

    Update login.ts

--- src/suite/a-home/modules/home-user/src/dto/login.ts
+++ src/suite/a-home/modules/home-user/src/dto/login.ts
@@ -7,7 +7,7 @@ export interface IDtoOptionsLogin extends IDecoratorDtoOptions {}
 
 @Dto<IDtoOptionsLogin>()
 export class DtoLogin {
-  @Api.field()
+  @Api.field(v.min(3))
   username: string;
 
   @Api.field(v.min(6))
