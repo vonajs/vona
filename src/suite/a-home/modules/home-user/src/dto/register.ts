a70014690820cf32312ea7e74d25a15bbc569963
zhennann <zhen.nann@icloud.com>
2025-09-09 10:17:40 +0800

    Update register.ts

--- src/suite/a-home/modules/home-user/src/dto/register.ts
+++ src/suite/a-home/modules/home-user/src/dto/register.ts
@@ -13,10 +13,10 @@ export class DtoRegister {
   @Api.field(v.email())
   email: string;
 
-  @Api.field(v.min(6))
+  @Api.field(v.min(6), v.max(20))
   password: string;
 
-  @Api.field(v.min(6))
+  @Api.field(v.min(6), v.max(20))
   passwordConfirm: string;
 
   @Api.field()
