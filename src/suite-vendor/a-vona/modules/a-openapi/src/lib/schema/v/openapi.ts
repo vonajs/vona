e886f2ddc9c916a931e56551e22c4eeadefb1eb3
zhennann <zhen.nann@icloud.com>
2025-09-10 16:32:38 +0800

    Update openapi.ts

--- src/suite-vendor/a-vona/modules/a-openapi/src/lib/schema/v/openapi.ts
+++ src/suite-vendor/a-vona/modules/a-openapi/src/lib/schema/v/openapi.ts
@@ -1,27 +1,27 @@
 import type { ZodOpenAPIMetadata } from '@asteasolutions/zod-to-openapi';
-import type { z, ZodTypeAny } from 'zod';
+import type { z } from 'zod';
 
-export function schemaOpenapi<T extends ZodTypeAny>(metadata: Partial<ZodOpenAPIMetadata<z.input<T>>>);
-export function schemaOpenapi<T extends ZodTypeAny>(refId: string, metadata?: Partial<ZodOpenAPIMetadata<z.input<T>>>);
-export function schemaOpenapi<T extends ZodTypeAny>(refId: any, metadata?: any) {
+export function schemaOpenapi<T extends z.ZodType>(metadata: Partial<ZodOpenAPIMetadata<z.input<T>>>): T;
+export function schemaOpenapi<T extends z.ZodType>(refId: string, metadata?: Partial<ZodOpenAPIMetadata<z.input<T>>>): T;
+export function schemaOpenapi<T extends z.ZodType>(refId: any, metadata?: any) {
   return function (schema: T): T {
     return schema.openapi(refId, metadata);
   };
 }
 
-export function schemaTitle<T extends ZodTypeAny>(title?: string) {
+export function schemaTitle<T extends z.ZodType>(title?: string) {
   return function (schema: T): T {
     return schema.openapi({ title });
   };
 }
 
-export function schemaDescription<T extends ZodTypeAny>(description?: string) {
+export function schemaDescription<T extends z.ZodType>(description?: string) {
   return function (schema: T): T {
     return schema.openapi({ description });
   };
 }
 
-export function schemaExample<T extends ZodTypeAny>(example?: any) {
+export function schemaExample<T extends z.ZodType>(example?: any) {
   return function (schema: T): T {
     return schema.openapi({ example });
   };
