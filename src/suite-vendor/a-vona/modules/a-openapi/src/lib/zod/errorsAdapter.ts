b07da409600becc6bcb36cfec680dafaadf44023
zhennann <zhen.nann@icloud.com>
2025-09-11 15:55:44 +0800

    feat: update

--- src/suite-vendor/a-vona/modules/a-openapi/src/lib/zod/errorsAdapter.ts
+++
@@ -1,7 +0,0 @@
-import type { VonaApplication } from 'vona';
-
-export function errorsAdapter(app: VonaApplication) {
-  // setErrorMapDefault((text: string, ...args: any[]) => {
-  //   return app.meta.text(text, ...args);
-  // });
-}
--- src/suite-vendor/a-vona/modules/a-openapi/src/lib/zod/index.ts
+++ src/suite-vendor/a-vona/modules/a-openapi/src/lib/zod/index.ts
@@ -1,2 +1,1 @@
-export * from './errorsAdapter.ts';
 export * from './schemaRefCustomAdapter.ts';
