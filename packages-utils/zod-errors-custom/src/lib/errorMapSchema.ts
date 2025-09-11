53e88f6a72582a7bc76f5b88d943737d2d707046
zhennann <zhen.nann@icloud.com>
2025-09-11 14:50:15 +0800

    feat: update

--- packages-utils/zod-errors-custom/src/lib/errorMapSchema.ts
+++ packages-utils/zod-errors-custom/src/lib/errorMapSchema.ts
@@ -1,75 +1,9 @@
 import type { LocaleAdapterFn } from './utils.ts';
+import { cast } from 'vona';
 import { z } from 'zod';
-import { prepareIssue, toRaw, translateError } from './utils.ts';
 
-const SymbolTranslated = Symbol('SymbolTranslated');
-
-const __zodTypes = [
-  'ZodString',
-  'ZodNumber',
-  'ZodBigInt',
-  'ZodBoolean',
-  'ZodDate',
-  'ZodSymbol',
-  'ZodUndefined',
-  'ZodNull',
-  'ZodAny',
-  'ZodUnknown',
-  'ZodNever',
-  'ZodVoid',
-  'ZodArray',
-  'ZodObject',
-  'ZodUnion',
-  'ZodDiscriminatedUnion',
-  'ZodIntersection',
-  'ZodTuple',
-  'ZodRecord',
-  'ZodMap',
-  'ZodSet',
-  'ZodFunction',
-  'ZodLazy',
-  'ZodLiteral',
-  'ZodEnum',
-  'ZodPromise',
-  'ZodOptional',
-  'ZodNullable',
-  'ZodDefault',
-  'ZodCatch',
-  'ZodNaN',
-  'ZodReadonly',
-];
-
-export function setErrorMapSchema(localeAdapterFn: LocaleAdapterFn) {
-  for (const typeName of __zodTypes) {
-    if (!z[typeName]) throw new Error(`${typeName} does not exist`);
-    const _parseOriginal = z[typeName].prototype._parse;
-    z[typeName].prototype._parse = function (this: any, input) {
-      if (this._def.errorMap && this._def.errorMap.name === 'customMap' && !this._def._errorMapPatched) {
-        const def = toRaw(this._def);
-        def._errorMapPatched = true;
-        const res = this._def.errorMap({ code: 'invalid_type' }, { defaultError: undefined });
-        const key = res.message;
-        def.errorMap = (issue, ctx) => {
-          if (!key || issue.code !== 'invalid_type') return { message: ctx.defaultError };
-          issue = prepareIssue(localeAdapterFn, issue);
-          const message = translateError(localeAdapterFn, key, issue);
-          return { message };
-        };
-      }
-      const res = _parseOriginal.call(this, input);
-      if (this._def.checks && this._def.checks.length > 0) {
-        for (const issue of input.parent.common.issues) {
-          if (!issue[SymbolTranslated]) {
-            issue[SymbolTranslated] = true;
-            const issue2 = prepareIssue(localeAdapterFn, issue);
-            const message = translateError(localeAdapterFn, issue.message, issue2);
-            if (message !== issue.message) {
-              issue.message = message;
-            }
-          }
-        }
-      }
-      return res;
-    };
-  }
+export function setLocaleAdapter(localeAdapterFn: LocaleAdapterFn) {
+  cast(z.util).setLocaleAdapter(localeAdapterFn);
 }
+
+export function set
--- packages-utils/zod-errors-custom/src/lib/locales.ts
+++
@@ -1,7 +0,0 @@
-import enUs from '../locale/en-us.ts';
-import zhCn from '../locale/zh-cn.ts';
-
-export const locales = {
-  'en-us': enUs,
-  'zh-cn': zhCn,
-};
