3a4bd575443708bfcfe6a60c96f4187dda9487ce
zhennann <zhen.nann@icloud.com>
2025-09-15 09:14:18 +0800

    Update bean.bodyReq.ts

--- src/suite-vendor/a-vona/modules/a-body/src/bean/bean.bodyReq.ts
+++ src/suite-vendor/a-vona/modules/a-body/src/bean/bean.bodyReq.ts
@@ -1,10 +1,9 @@
-import type { BodyParserOptions } from '../types/bodyParser.ts';
 import { BeanBase } from 'vona';
 import { Bean } from 'vona-module-a-bean';
 
 @Bean()
 export class BeanBodyReq extends BeanBase {
-  async parse(options: BodyParserOptions) {
-    return await this.scope.service.bodyReq.parse(false);
+  async parse() {
+    return await this.scope.service.bodyReq.parse();
   }
 }
