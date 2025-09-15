178ab090112ddd0fbfcd2717a23bbbea35674081
zhennann <zhen.nann@icloud.com>
2025-09-15 08:50:47 +0800

    feat: update

--- src/suite-vendor/a-vona/modules/a-body/src/bean/interceptor.bodyReq.ts
+++ src/suite-vendor/a-vona/modules/a-body/src/bean/interceptor.bodyReq.ts
@@ -1,13 +1,28 @@
 import type { Next } from 'vona';
 import type { IDecoratorInterceptorOptionsGlobal, IInterceptorExecute } from 'vona-module-a-aspect';
+import type { BodyParserOptions } from '../types/bodyParser.ts';
 import { BeanBase } from 'vona';
 import { Interceptor } from 'vona-module-a-aspect';
 
-export interface IInterceptorOptionsBodyReq extends IDecoratorInterceptorOptionsGlobal {}
+export interface IInterceptorOptionsBodyReq extends IDecoratorInterceptorOptionsGlobal {
+  parser: BodyParserOptions;
+}
 
 @Interceptor<IInterceptorOptionsBodyReq>({
   global: true,
   dependencies: 'a-openapischema:openapiSchema',
+  parser: {
+    enableTypes: ['json', 'form'],
+    encoding: 'utf8',
+    formLimit: '20mb',
+    jsonLimit: '20mb',
+    textLimit: '20mb',
+    xmlLimit: '20mb',
+    jsonStrict: true,
+    detectJSON: undefined,
+    parsedMethods: ['POST', 'PUT', 'PATCH'],
+    extendTypes: {},
+  },
 })
 export class InterceptorBodyReq extends BeanBase implements IInterceptorExecute {
   async execute(_options: IInterceptorOptionsBodyReq, next: Next) {
