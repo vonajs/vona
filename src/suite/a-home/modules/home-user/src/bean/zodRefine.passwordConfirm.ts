6e9f4baf6dd96bbec581bb3487db40ed4b8cf040
zhennann <zhen.nann@icloud.com>
2025-09-15 20:34:35 +0800

    feat: update

--- src/suite/a-home/modules/home-user/src/bean/zodRefine.passwordConfirm.ts
+++ src/suite/a-home/modules/home-user/src/bean/zodRefine.passwordConfirm.ts
@@ -2,13 +2,13 @@ import type { IDecoratorZodRefineOptions, IZodRefineExecute, TypeRefinementCtx }
 import { BeanBase } from 'vona';
 import { ZodRefine } from 'vona-module-a-zod';
 
-interface IZodRefinePasswordConfirmValue { password: string; passwordConfirm: string }
+export interface TypeZodRefineUsernameUniqueData { password: string; passwordConfirm: string }
 
 export interface IZodRefineOptionsPasswordConfirm extends IDecoratorZodRefineOptions {}
 
 @ZodRefine<IZodRefineOptionsPasswordConfirm>()
-export class ZodRefinePasswordConfirm extends BeanBase implements IZodRefineExecute<IZodRefinePasswordConfirmValue> {
-  async execute(value: IZodRefinePasswordConfirmValue, refinementCtx: TypeRefinementCtx, _options: IZodRefineOptionsPasswordConfirm) {
+export class ZodRefinePasswordConfirm extends BeanBase implements IZodRefineExecute<TypeZodRefineUsernameUniqueData> {
+  async execute(value: TypeZodRefineUsernameUniqueData, refinementCtx: TypeRefinementCtx, _options: IZodRefineOptionsPasswordConfirm) {
     if (value.password !== value.passwordConfirm) {
       refinementCtx.addIssue({
         code: 'custom',
