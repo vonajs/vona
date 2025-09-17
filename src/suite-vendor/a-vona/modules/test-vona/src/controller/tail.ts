190baa9a39b5011cfcd193e06913202619b54807
zhennann <zhen.nann@icloud.com>
2025-09-17 09:54:10 +0800

    Update tail.ts

--- src/suite-vendor/a-vona/modules/test-vona/src/controller/tail.ts
+++ src/suite-vendor/a-vona/modules/test-vona/src/controller/tail.ts
@@ -17,10 +17,10 @@ export class ControllerTail extends BeanBase {
     cast(this.ctx)._tail_test = 1;
 
     // tail
-    this.ctx.db.commit(() => {
+    this.ctx.commit(() => {
       assert.equal(cast(this.ctx)._tail_test_als_caller, undefined);
       assert.equal(cast(this.ctx)._tail_test, 2);
-      this.ctx.db.commit(() => {
+      this.ctx.commit(() => {
         assert.equal(cast(this.ctx)._tail_test, 3);
       });
       cast(this.ctx)._tail_test = 3;
