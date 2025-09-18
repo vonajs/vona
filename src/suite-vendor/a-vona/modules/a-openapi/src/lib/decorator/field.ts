87f4a296e7c63493bd1f3840701e58845cede408
zhennann <zhen.nann@icloud.com>
2025-09-18 18:11:08 +0800

    Update field.ts

--- src/suite-vendor/a-vona/modules/a-openapi/src/lib/decorator/field.ts
+++ src/suite-vendor/a-vona/modules/a-openapi/src/lib/decorator/field.ts
@@ -2,14 +2,12 @@ import type { MetadataKey } from 'vona';
 import type { SchemaLike } from 'vona-module-a-openapiutils';
 import { appMetadata } from 'vona';
 import { makeSchemaLikes } from '../schema/makeSchemaLikes.ts';
-import { getTargetDecoratorRules } from '../utils.ts';
+import { mergeFieldOpenapiMetadata } from '../utils.ts';
 
 export function Field(...schemaLikes: SchemaLike[]): PropertyDecorator {
   return function (target: object, prop: MetadataKey) {
-    // rules
-    const rules = getTargetDecoratorRules(target);
-    // rule
     const metaType = appMetadata.getDesignType(target, prop);
-    rules[prop as string] = makeSchemaLikes(schemaLikes, metaType);
+    const schema = makeSchemaLikes(schemaLikes, metaType);
+    mergeFieldOpenapiMetadata(target, prop as string, schema);
   };
 }
