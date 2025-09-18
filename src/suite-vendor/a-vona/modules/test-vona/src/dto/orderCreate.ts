e802ec0778098959d7cc94db3e0fce0686f636cc
zhennann <zhen.nann@icloud.com>
2025-09-18 21:35:00 +0800

    feat: update

--- src/suite-vendor/a-vona/modules/test-vona/src/dto/orderCreate.ts
+++ src/suite-vendor/a-vona/modules/test-vona/src/dto/orderCreate.ts
@@ -1,10 +1,18 @@
 import type { IDecoratorDtoOptions } from 'vona-module-a-web';
-import { $Dto } from 'vona-module-a-orm';
+import { $Dto, $relationDynamic } from 'vona-module-a-orm';
 import { Dto } from 'vona-module-a-web';
 import { ModelOrder } from '../model/order.ts';
+import { ModelProduct } from '../model/product.ts';
 
 export interface IDtoOptionsOrderCreate extends IDecoratorDtoOptions {}
 
 @Dto<IDtoOptionsOrderCreate>()
 export class DtoOrderCreate
-  extends $Dto.create(() => ModelOrder, { columns: ['orderNo', 'remark'] }) {}
+  extends $Dto.create(() => ModelOrder, {
+    columns: ['orderNo', 'remark'],
+    with: {
+      products: $relationDynamic.hasMany(() => ModelProduct, 'orderId', {
+        columns: ['name', 'price', 'quantity', 'amount'],
+      }),
+    },
+  }) {}
