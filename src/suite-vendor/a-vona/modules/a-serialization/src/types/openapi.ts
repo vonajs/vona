c5fd333c67bf100f162192846e6a5f84d8a6d1a4
zhennann <zhen.nann@icloud.com>
2025-09-20 19:15:19 +0800

    feat: update

--- src/suite-vendor/a-vona/modules/a-serialization/src/types/openapi.ts
+++ src/suite-vendor/a-vona/modules/a-serialization/src/types/openapi.ts
@@ -1,5 +1,12 @@
+import type { ISerializerTransformRecord } from './serializerTransform.ts';
+
+export type TypeSchemaObjectExtensionFieldSerializerTransforms = {
+  [K in keyof ISerializerTransformRecord]?: Partial<ISerializerTransformRecord[K]>;
+};
+
 declare module 'vona-module-a-openapi' {
   export interface ISchemaObjectExtensionField {
     exclude?: boolean;
+    serializerTransforms?: TypeSchemaObjectExtensionFieldSerializerTransforms;
   }
 }
