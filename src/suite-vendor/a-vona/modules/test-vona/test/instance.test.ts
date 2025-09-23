7dba9375a80b4ce2f1396f331354eb3855b0c99c
zhennann <zhen.nann@icloud.com>
2025-09-23 17:32:02 +0800

    Update instance.test.ts

--- src/suite-vendor/a-vona/modules/test-vona/test/instance.test.ts
+++ src/suite-vendor/a-vona/modules/test-vona/test/instance.test.ts
@@ -5,14 +5,22 @@ import { app } from 'vona-mock';
 describe('instance.test.ts', () => {
   it('action:instance:singleTest', async () => {
     await app.bean.executor.mockCtx(async () => {
+      const scopeDatabase = app.bean.scope('a-orm');
+      const defaultClientName = scopeDatabase.service.database.getDefaultClientName();
       assert.equal(app.ctx.instanceName, 'singleTest');
       assert.equal(app.ctx.instance.name, 'singleTest');
+      assert.equal(app.bean.database.current.clientName, defaultClientName);
+      assert.equal(['pg', 'mysql'].includes(app.bean.database.current.clientName), true);
     }, { instanceName: 'singleTest' as any });
   });
   it('action:instance:isolateTest', async () => {
     await app.bean.executor.mockCtx(async () => {
+      const scopeDatabase = app.bean.scope('a-orm');
+      const defaultClientName = scopeDatabase.service.database.getDefaultClientName();
       assert.equal(app.ctx.instanceName, 'isolateTest');
       assert.equal(app.ctx.instance.name, 'isolateTest');
+      assert.equal(app.bean.database.current.clientName, defaultClientName);
+      assert.equal(app.bean.database.current.clientName, 'isolateTest');
     }, { instanceName: 'isolateTest' as any });
   });
 });
