13ced62dcdad95310038d90d9d263e0de3c32a4f
zhennann <zhen.nann@icloud.com>
2025-09-23 17:07:05 +0800

    Update config.test.ts

--- src/backend/config/config/config.test.ts
+++ src/backend/config/config/config.test.ts
@@ -1,11 +1,27 @@
-import type { VonaAppInfo, VonaConfigOptional } from 'vona';
+import type { VonaAppInfo, VonaConfigEnv, VonaConfigOptional } from 'vona';
+import type { IDatabaseClientRecord } from 'vona-module-a-orm';
 
-export default function (_appInfo: VonaAppInfo) {
+declare module 'vona' {
+  export interface IInstanceRecord {
+    singleTest: never;
+    isolateTest: never;
+  }
+}
+
+declare module 'vona-module-a-orm' {
+  export interface IDatabaseClientRecord {
+    isolateTest: never;
+  }
+}
+
+export default function (_appInfo: VonaAppInfo, env: VonaConfigEnv) {
   const config = {} as VonaConfigOptional;
 
   // instances
   config.instances = [
     { name: '', password: '', title: '' },
+    { name: 'singleTest', password: '', title: '' },
+    { name: 'isolateTest', password: '', title: '', id: 1000, isolate: true, isolateClient: 'isolateTest' },
   ];
 
   // redis
@@ -21,6 +37,33 @@ export default function (_appInfo: VonaAppInfo) {
     },
     clients: {},
   };
+
+  // Multi-Instance/Multi-Tenancy
+  const databaseDefaultClient = env.DATABASE_DEFAULT_CLIENT as keyof IDatabaseClientRecord;
+  config.database.clients!.isolateTest =
+    databaseDefaultClient === 'pg'
+      ? {
+          client: 'pg',
+          connection: {
+            host: env.DATABASE_CLIENT_PG_HOST,
+            port: Number.parseInt(env.DATABASE_CLIENT_PG_PORT!),
+            user: env.DATABASE_CLIENT_PG_USER,
+            password: env.DATABASE_CLIENT_PG_PASSWORD,
+            database: env.DATABASE_CLIENT_PG_DATABASE,
+          },
+        }
+      : databaseDefaultClient === 'mysql'
+        ? {
+            client: 'mysql2',
+            connection: {
+              host: env.DATABASE_CLIENT_MYSQL_HOST,
+              port: Number.parseInt(env.DATABASE_CLIENT_MYSQL_PORT!),
+              user: env.DATABASE_CLIENT_MYSQL_USER,
+              password: env.DATABASE_CLIENT_MYSQL_PASSWORD,
+              database: env.DATABASE_CLIENT_MYSQL_DATABASE,
+            },
+          }
+        : undefined;
 
   // modules
   config.modules = {};
