3d04e40548eaa733cace63632006457aee5481f0
zhennann <zhen.nann@icloud.com>
2025-09-28 10:57:31 +0800

    Update guard.userName.ts

--- src/suite-vendor/a-vona/modules/a-user/src/bean/guard.userName.ts
+++ src/suite-vendor/a-vona/modules/a-user/src/bean/guard.userName.ts
@@ -7,21 +7,32 @@ import { Guard } from 'vona-module-a-aspect';
 export interface IGuardOptionsUserName extends IDecoratorGuardOptions {
   name?: keyof IUserNameRecord | (keyof IUserNameRecord)[];
   passWhenMatched: boolean;
+  rejectWhenDismatched: boolean;
 }
 
 @Guard<IGuardOptionsUserName>({
   passWhenMatched: true,
+  rejectWhenDismatched: true,
 })
 export class GuardUserName extends BeanBase implements IGuardExecute {
   async execute(options: IGuardOptionsUserName, next: Next): Promise<boolean> {
-    if (!options.name) return this.app.throw(403);
-    const user = this.bean.passport.getCurrentUser();
-    if (!user || user.anonymous) return this.app.throw(403);
-    const userName = user.name as keyof IUserNameRecord;
-    const optionsName = Array.isArray(options.name) ? options.name : [options.name];
-    if (!optionsName.includes(userName)) return this.app.throw(403);
-    if (options.passWhenMatched) return true;
+    const result = await this._check(options);
+    if (!result) {
+      if (options.rejectWhenDismatched) return this.app.throw(403);
+    } else {
+      if (options.passWhenMatched) return true;
+    }
     // next
     return next();
+  }
+
+  private async _check(options: IGuardOptionsUserName) {
+    if (!options.name) return false;
+    const user = this.bean.passport.getCurrentUser();
+    if (!user || user.anonymous) return false;
+    const userName = user.name as keyof IUserNameRecord;
+    const optionsName = Array.isArray(options.name) ? options.name : [options.name];
+    if (!optionsName.includes(userName)) return false;
+    return true;
   }
 }
