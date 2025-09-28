e0e39929e71257897ea9048ebcb9e1af4c9c7e23
zhennann <zhen.nann@icloud.com>
2025-09-28 11:00:28 +0800

    Update guard.roleName.ts

--- src/suite-vendor/a-vona/modules/a-user/src/bean/guard.roleName.ts
+++ src/suite-vendor/a-vona/modules/a-user/src/bean/guard.roleName.ts
@@ -7,23 +7,34 @@ import { Guard } from 'vona-module-a-aspect';
 export interface IGuardOptionsRoleName extends IDecoratorGuardOptions {
   name?: keyof IRoleNameRecord | (keyof IRoleNameRecord)[];
   passWhenMatched: boolean;
+  rejectWhenDismatched: boolean;
 }
 
 @Guard<IGuardOptionsRoleName>({
   passWhenMatched: true,
+  rejectWhenDismatched: true,
 })
 export class GuardRoleName extends BeanBase implements IGuardExecute {
   async execute(options: IGuardOptionsRoleName, next: Next): Promise<boolean> {
-    if (!options.name) return this.app.throw(403);
-    const user = this.bean.passport.getCurrentUser();
-    if (!user || user.anonymous) return this.app.throw(403);
-    const roles = this.bean.passport.getCurrentRoles();
-    if (!roles) return this.app.throw(403);
-    const roleNames = roles?.map(item => item.name as keyof IRoleNameRecord);
-    const optionsName = Array.isArray(options.name) ? options.name : [options.name];
-    if (!roleNames.some(roleName => optionsName.includes(roleName))) return this.app.throw(403);
-    if (options.passWhenMatched) return true;
+    const result = await this._check(options);
+    if (!result) {
+      if (options.rejectWhenDismatched) return this.app.throw(403);
+    } else {
+      if (options.passWhenMatched) return true;
+    }
     // next
     return next();
+  }
+
+  private async _check(options: IGuardOptionsRoleName) {
+    if (!options.name) return false;
+    const user = this.bean.passport.getCurrentUser();
+    if (!user || user.anonymous) return false;
+    const roles = this.bean.passport.getCurrentRoles();
+    if (!roles) return false;
+    const roleNames = roles?.map(item => item.name as keyof IRoleNameRecord);
+    const optionsName = Array.isArray(options.name) ? options.name : [options.name];
+    if (!roleNames.some(roleName => optionsName.includes(roleName))) return false;
+    return true;
   }
 }
