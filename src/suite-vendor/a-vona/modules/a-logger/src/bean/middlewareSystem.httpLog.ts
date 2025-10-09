4cb411c67356b99d5dd941e9f14e065c3f381e20
zhennann <zhen.nann@icloud.com>
2025-10-09 10:45:23 +0800

    Update middlewareSystem.httpLog.ts

--- src/suite-vendor/a-vona/modules/a-logger/src/bean/middlewareSystem.httpLog.ts
+++ src/suite-vendor/a-vona/modules/a-logger/src/bean/middlewareSystem.httpLog.ts
@@ -12,7 +12,7 @@ export class MiddlewareSystemHttpLog extends BeanBase implements IMiddlewareSyst
     const ctx = this.ctx;
     // start
     const req = StdSerializers.req(ctx.req);
-    this.$loggerChild('req').http(JSON.stringify(req));
+    this.$loggerChild('req').http(() => JSON.stringify(req));
     const profiler = this.$loggerChild('res').startTimer();
     // next
     await next();
