9fcd0add5466d97228a84b00723d32c2c8a0e08f
zhennann <zhen.nann@icloud.com>
2025-10-21 18:21:18 +0800

    feat: update

--- src/suite-vendor/a-vona/modules/a-meta/src/types/meta.ts
+++ src/suite-vendor/a-vona/modules/a-meta/src/types/meta.ts
@@ -5,6 +5,13 @@ export interface IMetaRecord {}
 
 export interface IDecoratorMetaOptions {}
 
+export type TypeMetaRecordSelectorSpecificName<NAME extends string> = {
+  [K in keyof IMetaRecord as K extends `${string}:${NAME}` ? K : never]: IMetaRecord[K];
+};
+
+export type TypeMetaRecordSelectorSpecificNameKeys<NAME extends string> =
+  keyof TypeMetaRecordSelectorSpecificName<NAME>;
+
 declare module 'vona-module-a-onion' {
   export interface BeanOnion {
     meta: ServiceOnion<IMetaRecord>;
