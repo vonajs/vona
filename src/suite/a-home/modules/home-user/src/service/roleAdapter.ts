a83cf905fe39fda436995de048703691fae263cf
zhennann <zhen.nann@icloud.com>
2025-10-28 11:25:58 +0800

    feat: update

--- src/suite/a-home/modules/home-user/src/service/roleAdapter.ts
+++ src/suite/a-home/modules/home-user/src/service/roleAdapter.ts
@@ -1,19 +1,20 @@
 import type { TableIdentity } from 'table-identity';
-import type { IRoleAdapter, IRoleBase } from 'vona-module-a-user';
+import type { IRoleAdapter } from 'vona-module-a-user';
+import type { EntityRole } from 'vona-module-test-vona';
 import { BeanBase } from 'vona';
 import { Service } from 'vona-module-a-bean';
 
 @Service()
 export class ServiceRoleAdapter extends BeanBase implements IRoleAdapter {
-  async findOneByName(name: string): Promise<IRoleBase | undefined> {
+  async findOneByName(name: string): Promise<EntityRole | undefined> {
     return await this.scope.model.role.getByNameEqI(name);
   }
 
-  async findOne(role: Partial<IRoleBase>): Promise<IRoleBase | undefined> {
+  async findOne(role: Partial<EntityRole>): Promise<EntityRole | undefined> {
     return await this.scope.model.role.get(role);
   }
 
-  async findAllByUserId(userId: TableIdentity): Promise<IRoleBase[] | undefined> {
+  async findAllByUserId(userId: TableIdentity): Promise<EntityRole[] | undefined> {
     const user = await this.scope.model.user.get({ id: userId }, { include: { roles: true } });
     return user?.roles;
   }
