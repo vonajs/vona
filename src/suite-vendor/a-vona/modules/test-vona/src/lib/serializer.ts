d99a1deaddd457c43f28c63e0ceacc0a9be44983
zhennann <zhen.nann@icloud.com>
2025-11-13 16:48:55 +0800

    Update serializer.ts

--- src/suite-vendor/a-vona/modules/test-vona/src/lib/serializer.ts
+++ src/suite-vendor/a-vona/modules/test-vona/src/lib/serializer.ts
@@ -1,7 +1,7 @@
 import { Serializer } from 'vona-module-a-serialization';
 
 export function SensitiveEmail(): PropertyDecorator {
-  return Serializer.sensitive({
+  return Serializer.replace({
     // eslint-disable-next-line
     patternFrom: /(\w?)(\w+)(\w)(@\w+\.[a-z]+)/,
     patternTo: '$1****$3$4',
