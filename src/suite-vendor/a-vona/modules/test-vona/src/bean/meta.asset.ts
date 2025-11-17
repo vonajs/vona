50fd34acacd7fdbd3c324129547c24cf93c502d5
zhennann <zhen.nann@icloud.com>
2025-11-17 18:05:30 +0800

    feat: update

--- src/suite-vendor/a-vona/modules/test-vona/src/bean/meta.asset.ts
+++ src/suite-vendor/a-vona/modules/test-vona/src/bean/meta.asset.ts
@@ -1,9 +1,9 @@
 import { Meta } from 'vona-module-a-meta';
 import { BeanAssetBase } from 'vona-module-a-static';
 
-export interface TypeAssetGetPath {
+export interface IAssetGetPath {
   static: 'img/vona.svg' | 'img/vona.png';
 }
 
 @Meta()
-export class MetaAsset extends BeanAssetBase<TypeAssetGetPath> {}
+export class MetaAsset extends BeanAssetBase<IAssetGetPath> {}
