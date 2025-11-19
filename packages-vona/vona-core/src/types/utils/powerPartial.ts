0341d2db3518ad1602bb827065c933af9e999037
zhennann <zhen.nann@icloud.com>
2025-11-19 14:47:37 +0800

    Update powerPartial.ts

--- packages-vona/vona-core/src/types/utils/powerPartial.ts
+++ packages-vona/vona-core/src/types/utils/powerPartial.ts
@@ -1,7 +1,7 @@
 export type PowerPartial<T> = {
   [U in keyof T]?:
   T[U] extends (...args: any) => any
-    ? (args: Parameters<T[U]>) => PowerPartialFunctionResult<ReturnType<T[U]>>
+    ? (...args: Parameters<T[U]>) => PowerPartialFunctionResult<ReturnType<T[U]>>
     : T[U] extends object ? PowerPartial<T[U]> : T[U];
 };
 
