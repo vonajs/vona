974f8084f4211187bd4792083e2a778dc352a113
zhennann <zhen.nann@icloud.com>
2025-11-20 10:48:07 +0800

    feat: update

--- src/suite-vendor/a-vona/modules/a-election/src/bean/cacheRedis.election.ts
+++ src/suite-vendor/a-vona/modules/a-election/src/bean/cacheRedis.election.ts
@@ -3,5 +3,5 @@ import { BeanCacheRedisBase, CacheRedis } from 'vona-module-a-cache';
 export type TCacheRedisElectionKey = 'leaders';
 export type TCacheRedisElectionData = string[];
 
-@CacheRedis({ disableTransactionCompensate: true })
+@CacheRedis({ disableInstance: true, disableTransactionCompensate: true })
 export class CacheRedisElection extends BeanCacheRedisBase<TCacheRedisElectionKey, TCacheRedisElectionData> {}
--- src/suite-vendor/a-vona/modules/a-election/src/types/election.ts
+++ src/suite-vendor/a-vona/modules/a-election/src/types/election.ts
@@ -1,7 +1,8 @@
+import type { FunctionAsync } from 'vona';
 import type { IRedlockLockOptions } from 'vona-module-a-redlock';
 
 export interface IElectionElectOptions extends IRedlockLockOptions {
   tickets: number;
 }
 
-export type TypeFunctionRelease = (fnRelease: Function) => void;
+export type TypeFunctionRelease = (fnRelease: FunctionAsync<void>) => void;
