d7f7b03e53d7b5269d2c01fb2e6a310bbf14b2b1
zhennann <zhen.nann@icloud.com>
2025-11-25 11:43:09 +0800

    Update vona.ts

--- packages-cli/cli/src/bin/vona.ts
+++ packages-cli/cli/src/bin/vona.ts
@@ -4,6 +4,7 @@ import path from 'node:path';
 import { ProcessHelper } from '@cabloy/process-helper';
 import fse from 'fs-extra';
 import semver from 'semver';
+import { VonaCommand } from '../start.ts';
 
 const pnpm_version = '10.19.0';
 
@@ -37,13 +38,14 @@ function main() {
   if (!fse.existsSync(bootstrapFile)) {
     bootstrapFile = path.join(import.meta.dirname, '../bootstrap.js');
   }
-  args.push(bootstrapFile);
   const rawArgv = process.argv.slice(2);
-  if (rawArgv[0] === 'play') {
+  const isPlay = rawArgv[0] === 'play';
+  if (isPlay) {
     args = args.concat([':bin:play']).concat(rawArgv.slice(1)).concat(['--dummy']);
+    new VonaCommand(args).start();
   } else {
+    args.push(bootstrapFile);
     args = args.concat(rawArgv);
+    processHelper.spawnCmd({ cmd: 'tsx', args });
   }
-
-  processHelper.spawnCmd({ cmd: 'tsx', args });
 }
