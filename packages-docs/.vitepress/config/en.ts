36abbdf157855ece0e793549d6bc0a6244f9e635
zhennann <zhen.nann@icloud.com>
2025-11-28 19:58:14 +0800

    Update en.ts

--- packages-docs/.vitepress/config/en.ts
+++ packages-docs/.vitepress/config/en.ts
@@ -407,7 +407,11 @@ function sidebarGuide(): DefaultTheme.SidebarItem[] {
         {
           text: 'I18n',
           base: '/guide/techniques/i18n/',
-          link: 'introduction',
+          collapsed: false,
+          items: [
+            { text: 'Locale', link: 'locale' },
+            { text: 'Timezone', link: 'timezone' },
+          ],
         },
         {
           text: 'JWT',
