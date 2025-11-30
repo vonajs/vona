841ca0d2e999a19cbb3bc42565a8f887370cdf64
zhennann <zhen.nann@icloud.com>
2025-11-30 11:24:44 +0800

    feat: update

--- src/suite-vendor/a-vona/modules/a-web/src/types/index.ts
+++ src/suite-vendor/a-vona/modules/a-web/src/types/index.ts
@@ -1,5 +1,6 @@
 export * from './controller.ts';
 export * from './dto.ts';
+export * from './filterTransform.ts';
 export * from './request.ts';
 export * from './response.ts';
 export * from './router.ts';
