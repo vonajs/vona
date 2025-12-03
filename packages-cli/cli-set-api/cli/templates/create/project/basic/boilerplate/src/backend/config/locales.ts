869aea007d9454a88d2f447aa955d58218df40d4
zhennann <zhen.nann@icloud.com>
2025-12-03 15:57:22 +0800

    feat: update

--- packages-cli/cli-set-api/cli/templates/create/project/basic/boilerplate/src/backend/config/locales.ts
+++ packages-cli/cli-set-api/cli/templates/create/project/basic/boilerplate/src/backend/config/locales.ts
@@ -1,7 +1,8 @@
+import type { VonaLocaleOptionalMap } from 'vona';
 import en_us from './locale/en-us.ts';
 import zh_cn from './locale/zh-cn.ts';
 
-export const locales = {
+export const locales: VonaLocaleOptionalMap = {
   'en-us': en_us,
   'zh-cn': zh_cn,
 };
--- packages-vona/vona-core/src/types/config/locale.ts
+++ packages-vona/vona-core/src/types/config/locale.ts
@@ -1,3 +1,4 @@
+import type { ILocaleRecord } from 'vona';
 import type { IBeanScopeLocale } from '../../lib/bean/type.ts';
 import type { PowerPartial } from '../utils/powerPartial.ts';
 
@@ -10,4 +11,4 @@ export type VonaLocale = {
 } & typeof localeDefault;
 
 export type VonaLocaleOptional = PowerPartial<VonaLocale>;
-export type VonaLocaleOptionalMap = Record<string, VonaLocaleOptional>;
+export type VonaLocaleOptionalMap = Record<keyof ILocaleRecord, VonaLocaleOptional>;
