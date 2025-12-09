df9ce87426c679fb1a4eede623acebe5a8a8e2b9
zhennann <zhen.nann@icloud.com>
2025-12-09 11:18:56 +0800

    Update monkey.ts

--- src/suite-vendor/a-vona/modules/a-hmr/src/monkey.ts
+++ src/suite-vendor/a-vona/modules/a-hmr/src/monkey.ts
@@ -7,6 +7,8 @@ export class Monkey extends BeanSimple implements IMonkeyAppStarted {
     const scope = this.app.scope(__ThisModule__);
     scope.election.obtain('hmr', async () => {
       await scope.service.watch.start();
+      // eslint-disable-next-line
+      console.log(`[hmr] ready: ${process.pid}`);
     }, async () => {
       await scope.service.watch.stop();
     });
