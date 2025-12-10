ee9bca7f656fc578814448c1fbe91a1b88cc80cc
zhennann <zhen.nann@icloud.com>
2025-12-10 20:50:49 +0800

    Update 2-meta.version.ts

--- packages-cli/cli-set-api/cli/templates/tools/crud/snippets/2-meta.version.ts
+++ packages-cli/cli-set-api/cli/templates/tools/crud/snippets/2-meta.version.ts
@@ -9,14 +9,14 @@ declare module '@cabloy/cli' {
 }
 
 const __snippet_update = `if (options.version === <%=argv.fileVersion%>) {
-  const entity<%=argv.resourceNameCapitalize%> = this.scope.entity.<%=argv.resourceName%>;
-  await this.bean.model.createTable(entity<%=argv.resourceNameCapitalize%>.$table, table => {
-    table.comment(entity<%=argv.resourceNameCapitalize%>.$comment.$table);
-    table.basicFields();
-    table.string(entity<%=argv.resourceNameCapitalize%>.name, 50).defaultTo(entity<%=argv.resourceNameCapitalize%>.$default.name).comment(entity<%=argv.resourceNameCapitalize%>.$comment.name);
-    table.string(entity<%=argv.resourceNameCapitalize%>.description, 255).comment(entity<%=argv.resourceNameCapitalize%>.$comment.description);
-  });
-}`;
+    const entity<%=argv.resourceNameCapitalize%> = this.scope.entity.<%=argv.resourceName%>;
+    await this.bean.model.createTable(entity<%=argv.resourceNameCapitalize%>.$table, table => {
+      table.comment(entity<%=argv.resourceNameCapitalize%>.$comment.$table);
+      table.basicFields();
+      table.string(entity<%=argv.resourceNameCapitalize%>.name, 50).defaultTo(entity<%=argv.resourceNameCapitalize%>.$default.name).comment(entity<%=argv.resourceNameCapitalize%>.$comment.name);
+      table.string(entity<%=argv.resourceNameCapitalize%>.description, 255).comment(entity<%=argv.resourceNameCapitalize%>.$comment.description);
+    });
+  }`;
 
 export default metadataCustomSnippet({
   file: 'src/bean/meta.version.ts',
@@ -38,7 +38,7 @@ export default metadataCustomSnippet({
     // update
     ast.replace('async update(_options: IMetaVersionUpdateOptions) {$$$1}', 'async update(options: IMetaVersionUpdateOptions) {$$$1}');
     const code = await cli.template.renderContent({ content: __snippet_update });
-    ast.replace('async update($$$0) {$$$1}', `async update($$$0) {$$$1 \n ${code}}`);
+    ast.replace('async update($$$0) {$$$1}', `async update($$$0) {$$$1 \n  ${code}}`);
     // ok
     return ast;
   },
