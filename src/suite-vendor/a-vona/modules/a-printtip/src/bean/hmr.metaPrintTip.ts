5f710eeaff86cef0949ff4e2680a7030219a7f41
zhennann <zhen.nann@icloud.com>
2025-12-10 20:23:52 +0800

    Update hmr.metaPrintTip.ts

--- src/suite-vendor/a-vona/modules/a-printtip/src/bean/hmr.metaPrintTip.ts
+++ src/suite-vendor/a-vona/modules/a-printtip/src/bean/hmr.metaPrintTip.ts
@@ -5,6 +5,8 @@ import { Hmr } from 'vona-module-a-hmr';
 @Hmr()
 export class HmrMetaPrintTip extends BeanBase implements IHmrReload {
   async reload() {
-    await this.$scope.instance.service.instance.instanceStartup(this.ctx.instanceName!, { force: true });
+    await this.bean.executor.newCtx(async () => {
+      await this.$scope.instance.service.instance.instanceStartup(this.ctx.instanceName!, { force: true });
+    }, { dbInfo: {}, instanceName: '' });
   }
 }
