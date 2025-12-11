ca3fbd2801056ed6123969ec27794f7290aa455f
zhennann <zhen.nann@icloud.com>
2025-12-11 17:16:22 +0800

    Update hmr.controller.ts

--- src/suite-vendor/a-vona/modules/a-web/src/bean/hmr.controller.ts
+++ src/suite-vendor/a-vona/modules/a-web/src/bean/hmr.controller.ts
@@ -1,10 +1,9 @@
+import type { IDecoratorBeanOptionsBase } from 'vona';
 import type { IHmrReload } from 'vona-module-a-hmr';
 import { BeanBase } from 'vona';
 import { Hmr } from 'vona-module-a-hmr';
 
 @Hmr()
 export class HmrController extends BeanBase implements IHmrReload {
-  async reload() {
-    this.bean.router.reRegisterController();
-  }
+  async reload(_beanOptions: IDecoratorBeanOptionsBase) {}
 }
