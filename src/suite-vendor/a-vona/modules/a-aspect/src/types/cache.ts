b2f420a17d7769d8cab881ee28e93f09c7d05377
zhennann <zhen.nann@icloud.com>
2025-12-11 21:43:17 +0800

    feat: update

--- src/suite-vendor/a-vona/modules/a-aspect/src/types/cache.ts
+++ src/suite-vendor/a-vona/modules/a-aspect/src/types/cache.ts
@@ -1,6 +1,17 @@
+import type { VonaApplication } from 'vona';
+
 export const SymbolCacheMiddlewareSystems = Symbol('SymbolCacheMiddlewareSystems');
 export const SymbolCacheComposeMiddlewares = Symbol('SymbolCacheComposeMiddlewares');
 export const SymbolCacheComposeGuards = Symbol('SymbolCacheComposeGuards');
 export const SymbolCacheComposeInterceptors = Symbol('SymbolCacheComposeInterceptors');
 export const SymbolCacheComposePipes = Symbol('SymbolCacheComposePipes');
 export const SymbolCacheComposeFilters = Symbol('SymbolCacheComposeFilters');
+
+export function clearAllCacheComposes(app: VonaApplication) {
+  delete app.meta[SymbolCacheMiddlewareSystems];
+  delete app.meta[SymbolCacheComposeMiddlewares];
+  delete app.meta[SymbolCacheComposeGuards];
+  delete app.meta[SymbolCacheComposeInterceptors];
+  delete app.meta[SymbolCacheComposePipes];
+  delete app.meta[SymbolCacheComposeFilters];
+}
--- src/suite-vendor/a-vona/modules/a-web/src/bean/hmr.controller.ts
+++ src/suite-vendor/a-vona/modules/a-web/src/bean/hmr.controller.ts
@@ -1,11 +1,19 @@
 import type { IDecoratorBeanOptionsBase } from 'vona';
 import type { IHmrReload } from 'vona-module-a-hmr';
-import { BeanBase } from 'vona';
+import { BeanBase, beanFullNameFromOnionName } from 'vona';
+import { clearAllCacheComposes } from 'vona-module-a-aspect';
 import { Hmr } from 'vona-module-a-hmr';
 
 @Hmr()
 export class HmrController extends BeanBase implements IHmrReload {
   async reload(beanOptions: IDecoratorBeanOptionsBase) {
+    clearAllCacheComposes(this.app);
     this.bean.router.reRegisterController(beanOptions.beanFullName);
+    const cacheOpenapiSchema = this.bean.summer.cache(beanFullNameFromOnionName('a-openapi:json', 'summerCache'));
+    await cacheOpenapiSchema.clear();
+    const cacheSwagger = this.bean.summer.cache(beanFullNameFromOnionName('a-swagger:swagger', 'summerCache'));
+    await cacheSwagger.clear();
+    const cacheRapidoc = this.bean.summer.cache(beanFullNameFromOnionName('a-swagger:rapidoc', 'summerCache'));
+    await cacheRapidoc.clear();
   }
 }
