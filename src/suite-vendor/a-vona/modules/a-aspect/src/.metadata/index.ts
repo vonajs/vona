eb26e04469835de32bfcd016fc08975d847bfb92
zhennann <zhen.nann@icloud.com>
2025-12-11 23:07:49 +0800

    feat: update

--- src/suite-vendor/a-vona/modules/a-aspect/src/.metadata/index.ts
+++ src/suite-vendor/a-vona/modules/a-aspect/src/.metadata/index.ts
@@ -1,4 +1,43 @@
 /* eslint-disable */
+/** hmr: begin */
+export * from '../bean/hmr.aop.ts';
+export * from '../bean/hmr.aopMethod.ts';
+
+import 'vona';
+declare module 'vona' {
+  
+    export interface IHmrRecord {
+      'a-aspect:aop': never;
+'a-aspect:aopMethod': never;
+    }
+
+  
+}
+declare module 'vona-module-a-aspect' {
+  
+        export interface HmrAop {
+          /** @internal */
+          get scope(): ScopeModuleAAspect;
+        }
+
+          export interface HmrAop {
+            get $beanFullName(): 'a-aspect.hmr.aop';
+            get $onionName(): 'a-aspect:aop';
+            
+          }
+
+        export interface HmrAopMethod {
+          /** @internal */
+          get scope(): ScopeModuleAAspect;
+        }
+
+          export interface HmrAopMethod {
+            get $beanFullName(): 'a-aspect.hmr.aopMethod';
+            get $onionName(): 'a-aspect:aopMethod';
+            
+          } 
+}
+/** hmr: end */
 /** scope: begin */
 import { BeanScopeBase, type BeanScopeUtil } from 'vona';
 import { Scope } from 'vona-module-a-bean';
---
+++ src/suite-vendor/a-vona/modules/a-aspect/src/bean/hmr.aopMethod.ts
@@ -0,0 +1,12 @@
+import type { IDecoratorBeanOptionsBase } from 'vona';
+import type { IHmrReload } from 'vona-module-a-hmr';
+import { BeanBase, SymbolCacheAopChains, SymbolCacheAopChainsKey } from 'vona';
+import { Hmr } from 'vona-module-a-hmr';
+
+@Hmr()
+export class HmrAopMethod extends BeanBase implements IHmrReload {
+  async reload(_beanOptions: IDecoratorBeanOptionsBase) {
+    delete this.app[SymbolCacheAopChains];
+    delete this.app[SymbolCacheAopChainsKey];
+  }
+}
