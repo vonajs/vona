35cf89c7c5b220682156506bc33db68dfa29d264
zhennann <zhen.nann@icloud.com>
2025-12-19 11:02:30 +0800

    Update meta.index.ts

--- src/suite/a-home/modules/home-user/src/bean/meta.index.ts
+++ src/suite/a-home/modules/home-user/src/bean/meta.index.ts
@@ -2,15 +2,12 @@ import type { IMetaOptionsIndex } from 'vona-module-a-index';
 import { BeanBase } from 'vona';
 import { Meta } from 'vona-module-a-meta';
 import { $tableColumns } from 'vona-module-a-orm';
-import { EntityRole } from '../entity/role.ts';
-import { EntityRoleUser } from '../entity/roleUser.ts';
-import { EntityUser } from '../entity/user.ts';
 
 @Meta<IMetaOptionsIndex>({
   indexes: {
-    ...$tableColumns(() => EntityUser, 'name'),
-    ...$tableColumns(() => EntityRole, 'name'),
-    ...$tableColumns(() => EntityRoleUser, ['userId', 'roleId']),
+    ...$tableColumns('homeUser', 'name'),
+    ...$tableColumns('homeRole', 'name'),
+    ...$tableColumns('homeRoleUser', ['userId', 'roleId']),
   },
 })
 export class MetaIndex extends BeanBase {}
