682da1d316098123c9aee9f8dba42b8b1c406b50
zhennann <zhen.nann@icloud.com>
2025-12-19 12:48:08 +0800

    Update queue.test.ts

--- src/suite-vendor/a-vona/modules/test-vona/test/queue.test.ts
+++ src/suite-vendor/a-vona/modules/test-vona/test/queue.test.ts
@@ -4,8 +4,8 @@ import { app } from 'vona-mock';
 describe('queue.test.ts', () => {
   it('action:queue', async () => {
     await app.bean.executor.mockCtx(async () => {
-      await app.bean.executor.performAction('post', '/test/vona/queue/pushAsync');
       await app.bean.executor.performAction('post', '/test/vona/queue/push');
+      await app.bean.executor.performAction('post', '/test/vona/queue/pushAsync');
     });
   });
 });
