a1e2a386244b40aeccb1acb441bb7beae6249adf
zhennann <zhen.nann@icloud.com>
2025-12-22 12:38:54 +0800

    Update base.ts

--- packages-utils/celjs/src/base.ts
+++ packages-utils/celjs/src/base.ts
@@ -12,6 +12,21 @@ for (let i = 0; i < 10; i++) {
   celEnvBase.registerFunction(`concat(${params.join(',')}):list`, _concat);
 }
 
+celEnvBase.registerFunction('join(list):string', list => {
+  return _join(list);
+});
+celEnvBase.registerFunction('join(list,string):string', (list, sep) => {
+  return _join(list, sep);
+});
+
+celEnvBase.registerOperator('string + int', (str, n) => str + String(n));
+celEnvBase.registerOperator('int + string', (n, str) => String(n) + str);
+
 function _concat(...args: any[]): any[] {
   return [].concat(...args);
+}
+
+function _join(list?: [], sep?: string): string {
+  if (!list) return '';
+  return list.join(sep);
 }
