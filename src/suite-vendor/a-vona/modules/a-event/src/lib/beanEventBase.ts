a98beae8ee922aa089c8f1e80ef1600a3d2f57fa
zhennann <zhen.nann@icloud.com>
2026-01-01 14:48:41 +0800

    Update beanEventBase.ts

--- src/suite-vendor/a-vona/modules/a-event/src/lib/beanEventBase.ts
+++ src/suite-vendor/a-vona/modules/a-event/src/lib/beanEventBase.ts
@@ -4,7 +4,7 @@ import { BeanBase, cast } from 'vona';
 import { ServiceEventListener } from '../service/eventListener.ts';
 
 export class BeanEventBase<DATA = unknown, RESULT = unknown> extends BeanBase {
-  async emit(data: DATA, nextOrDefault?: NextEventStrict<DATA, RESULT> | RESULT): Promise<RESULT> {
+  async emit(data?: DATA, nextOrDefault?: NextEventStrict<DATA, RESULT> | RESULT): Promise<RESULT> {
     const beanEventListener = this.bean._getBeanSelector(ServiceEventListener, this.$onionName);
     const next =
       typeof nextOrDefault === 'function'
@@ -15,7 +15,7 @@ export class BeanEventBase<DATA = unknown, RESULT = unknown> extends BeanBase {
     return beanEventListener.composer(data, next);
   }
 
-  emitSync(data: DATA, nextOrDefault?: NextEventSyncStrict<DATA, RESULT> | RESULT): RESULT {
+  emitSync(data?: DATA, nextOrDefault?: NextEventSyncStrict<DATA, RESULT> | RESULT): RESULT {
     const beanEventListener = this.bean._getBeanSelector(ServiceEventListener, this.$onionName);
     const next =
       typeof nextOrDefault === 'function'
