9f1cf0ef21457ef0623ddb963ec2646d1b198ccb
zhennann <zhen.nann@icloud.com>
2026-01-06 16:28:11 +0800

    feat: update

--- src/suite-vendor/a-vona/modules/a-openapiutils/src/lib/schema/v/rest.ts
+++ src/suite-vendor/a-vona/modules/a-openapiutils/src/lib/schema/v/rest.ts
@@ -1,7 +1,19 @@
 import type z from 'zod';
+import type { ISchemaOrderParams } from '../../../types/order.ts';
+import { OrderLevelBaseMap } from '../../const/database.ts';
 
-export function schemaOrder<T extends z.ZodType>(order: number) {
+export function schemaOrder<T extends z.ZodType>(order: number): T;
+export function schemaOrder<T extends z.ZodType>(order: ISchemaOrderParams): T;
+export function schemaOrder<T extends z.ZodType>(order: number | ISchemaOrderParams) {
+  const order2 = typeof order === 'number' ? { order } : order;
+  const levelBase = OrderLevelBaseMap[order2.level ?? 'business'];
+  const orderReal = levelBase + order2.order;
+  const scene = order2.scene;
   return function (schema: T): T {
-    return schema.openapi({ rest: { order } });
+    return schema.openapi(
+      scene
+        ? { rest: { [scene]: { order: orderReal } } }
+        : { rest: { order: orderReal } },
+    );
   };
 }
--- src/suite-vendor/a-vona/modules/a-openapiutils/src/types/order.ts
+++ src/suite-vendor/a-vona/modules/a-openapiutils/src/types/order.ts
@@ -1,6 +1,9 @@
+import type { TypeSchemaScene } from 'vona-module-a-openapi';
+
 export type TypeSchemaOrderLevel = 'core' | 'business' | 'max';
 
 export interface ISchemaOrderParams {
   order: number;
   level?: TypeSchemaOrderLevel;
+  scene?: TypeSchemaScene;
 }
