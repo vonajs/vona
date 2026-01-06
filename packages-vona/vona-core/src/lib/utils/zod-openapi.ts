100e01eddc88e698b8e035b63a0f0095ac3a3321
zhennann <zhen.nann@icloud.com>
2026-01-06 17:18:21 +0800

    Update zod-openapi.ts

--- packages-vona/vona-core/src/lib/utils/zod-openapi.ts
+++ packages-vona/vona-core/src/lib/utils/zod-openapi.ts
@@ -10,19 +10,23 @@ export function zodExtendOpenApi() {
   z.ZodType.prototype.openapi = function (this: z.ZodType, ...args) {
     // refId
     if (typeof args[0] === 'string') return openapiOriginal.call(this, ...args);
-    const refId = ZodMetadata.getRefId(this);
-    if (!refId) return openapiOriginal.call(this, ...args);
-    // metadata
+    // metadata/options
     const metadata = args[0];
+    const options = args[1];
+    // metadata
     if (isEmptyObject(metadata)) {
       return this;
       // return openapiOriginal.call(this, ...args);
     }
-    const options = args[1];
-    // refId: update
-    const refIdNew = `${refId}_${hashkey(metadata)}`;
     const metadataOld = ZodMetadata.getOpenapiMetadata(this);
     const metadataNew = deepExtend({}, metadataOld, metadata);
+    // refId
+    const refId = ZodMetadata.getRefId(this);
+    if (!refId) {
+      return openapiOriginal.call(this, metadataNew, options);
+    }
+    // refId: update
+    const refIdNew = `${refId}_${hashkey(metadata)}`;
     return openapiOriginal.call(this, refIdNew, metadataNew, options);
   };
 }
