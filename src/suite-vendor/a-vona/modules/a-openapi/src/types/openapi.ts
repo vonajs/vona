132570505666b67b23d3ac1000a6ba54c55d89ca
zhennann <zhen.nann@icloud.com>
2026-01-21 22:50:13 +0800

    Update openapi.ts

--- src/suite-vendor/a-vona/modules/a-openapi/src/types/openapi.ts
+++ src/suite-vendor/a-vona/modules/a-openapi/src/types/openapi.ts
@@ -1,9 +1,4 @@
-import type { IOpenapiSchema } from 'vona-module-a-openapischema';
 import type { IOpenApiOptionsResourceMeta } from './resourceMeta.ts';
-
-export interface IOpenapiSchemaBootstrap extends IOpenapiSchema {
-  api?: string;
-}
 
 declare module 'vona-module-a-openapiutils' {
   export interface IOpenApiOptions {
