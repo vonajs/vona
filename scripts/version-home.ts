dfa00f38b2563375667cdafeba6007a2248c57d1
zhennann <zhen.nann@icloud.com>
2026-02-25 11:01:51 +0800

    Update version-home.ts

--- scripts/version-home.ts
+++ scripts/version-home.ts
@@ -3,10 +3,10 @@ import path from 'node:path';
 import { fileURLToPath } from 'node:url';
 import fse from 'fs-extra';
 
-async function versionTemplate() {
+async function _versionTemplate(templateName: string) {
   // cp a-home
   const suiteHomeSrc = path.resolve(fileURLToPath(import.meta.url), '../../src/suite/a-home');
-  const suiteHomeDest = path.resolve(fileURLToPath(import.meta.url), '../../packages-cli/cli-set-api/cli/templates/create/project/basic/boilerplate/src/suite/a-home');
+  const suiteHomeDest = path.resolve(fileURLToPath(import.meta.url), `../../packages-cli/cli-set-api/cli/templates/create/project/${templateName}/boilerplate/src/suite/a-home`);
   await fse.remove(suiteHomeDest);
   await fse.copy(suiteHomeSrc, suiteHomeDest);
   for (const moduleName of ['home-base', 'home-index', 'home-user']) {
@@ -15,4 +15,10 @@ async function versionTemplate() {
   }
 }
 
-versionTemplate();
+async function versionTemplates() {
+   await _versionTemplate('basic');
+   await _versionTemplate('cabloy-start');
+}
+
+
+versionTemplates();
