00cbe2239e7683909de1b4d6c3e6eb93d579739f
zhennann <zhen.nann@icloud.com>
2026-02-27 21:13:45 +0800

    Update dtoSelectAndCount.ts

--- src/suite-vendor/a-vona/modules/a-orm/src/lib/dto/dtoSelectAndCount.ts
+++ src/suite-vendor/a-vona/modules/a-orm/src/lib/dto/dtoSelectAndCount.ts
@@ -3,9 +3,8 @@ import type { BeanModelMeta } from '../../bean/bean.model/bean.model_meta.ts';
 import type { IDtoGetParams } from '../../types/dto/dtoGet.ts';
 import type { TypeDtoSelectAndCountResult } from '../../types/dto/dtoSelectAndCount.ts';
 import type { IModelClassRecord } from '../../types/onion/model.ts';
-import { Api, v } from 'vona-module-a-openapiutils';
-import z from 'zod';
 import { DtoGet } from './dtoGet.ts';
+import { DtoListAndCount } from './dtoListAndCount.ts';
 
 export function DtoSelectAndCount<
   ModelLike extends BeanModelMeta | (keyof IModelClassRecord),
@@ -14,12 +13,6 @@ export function DtoSelectAndCount<
   modelLike: ModelLike extends BeanModelMeta ? ((() => Constructable<ModelLike>) | Constructable<ModelLike>) : ModelLike,
   params?: T,
 ): Constructable<TypeDtoSelectAndCountResult<ModelLike, T>> {
-  abstract class TargetClass {}
   const DtoGetResult = DtoGet(modelLike, params);
-  Api.field(v.array(DtoGetResult))(TargetClass.prototype, 'list');
-  Api.field(z.string())(TargetClass.prototype, 'total');
-  Api.field(z.number())(TargetClass.prototype, 'pageCount');
-  Api.field(z.number())(TargetClass.prototype, 'pageSize');
-  Api.field(z.number())(TargetClass.prototype, 'pageNo');
-  return TargetClass as any;
+  return DtoListAndCount(DtoGetResult);
 }
