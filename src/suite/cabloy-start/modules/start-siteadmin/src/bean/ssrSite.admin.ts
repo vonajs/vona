ca181e2c08060a765e8211bcf695f100e1eb55d0
zhennann <zhen.nann@icloud.com>
2026-03-07 22:59:31 +0800

    feat: update

--- src/suite/cabloy-start/modules/start-siteadmin/src/bean/ssrSite.admin.ts
+++ src/suite/cabloy-start/modules/start-siteadmin/src/bean/ssrSite.admin.ts
@@ -1,20 +1,21 @@
 import type { IDecoratorSsrSiteOptions } from 'vona-module-a-ssr';
+import type { IPagePathRecord } from 'zova-rest-cabloy-start-admin';
 import { BeanSsrSiteBase, SsrSite } from 'vona-module-a-ssr';
 
 declare module 'vona-module-a-ssr' {
   export interface ISsrSitePublicPathRecord {
-    some: never;
+    admin: never;
   }
 }
 
-export interface ISsrSiteOptionsAdminPages {}
+export interface ISsrSiteOptionsAdminPages extends IPagePathRecord {}
 
 export interface ISsrSiteOptionsAdminPagesData {}
 
 export interface ISsrSiteOptionsAdmin extends IDecoratorSsrSiteOptions<ISsrSiteOptionsAdminPages, ISsrSiteOptionsAdminPagesData> {}
 
 @SsrSite<ISsrSiteOptionsAdmin>({
-  publicPath: 'some',
-  assetPath: 'front-bundle-path',
+  publicPath: 'admin',
+  assetPath: 'ssr-cabloyStartAdmin-5.0.0',
 })
 export class SsrSiteAdmin extends BeanSsrSiteBase<ISsrSiteOptionsAdmin> {}
