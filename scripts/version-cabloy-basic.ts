3a165db23aa501350fb37c059633355e269979dc
zhennann <zhen.nann@icloud.com>
2026-03-08 00:00:08 +0800

    feat: update

--- scripts/version-cabloy-basic.ts
+++ scripts/version-cabloy-basic.ts
@@ -4,12 +4,12 @@ import { fileURLToPath } from 'node:url';
 import fse from 'fs-extra';
 
 async function _versionTemplate(templateName: string) {
-  // cp a-home
-  const suiteHomeSrc = path.resolve(fileURLToPath(import.meta.url), '../../src/suite/a-home');
-  const suiteHomeDest = path.resolve(fileURLToPath(import.meta.url), `../../packages-cli/cli-set-api/cli/templates/create/project/${templateName}/boilerplate/src/suite/a-home`);
+  // cp cabloy-start
+  const suiteHomeSrc = path.resolve(fileURLToPath(import.meta.url), '../../src/suite/cabloy-basic');
+  const suiteHomeDest = path.resolve(fileURLToPath(import.meta.url), `../../packages-cli/cli-set-api/cli/templates/create/project/${templateName}/boilerplate/src/suite/cabloy-basic`);
   await fse.remove(suiteHomeDest);
   await fse.copy(suiteHomeSrc, suiteHomeDest);
-  for (const moduleName of ['home-base', 'home-index', 'home-user']) {
+  for (const moduleName of ['basic-siteadmin']) {
     await fse.remove(path.join(suiteHomeDest, `modules/${moduleName}/dist`));
     await fse.remove(path.join(suiteHomeDest, `modules/${moduleName}/tsconfig.build.tsbuildinfo`));
   }
@@ -17,7 +17,6 @@ async function _versionTemplate(templateName: string) {
 
 async function versionTemplates() {
    await _versionTemplate('cabloy-basic');
-   await _versionTemplate('cabloy-start');
 }
 
 
