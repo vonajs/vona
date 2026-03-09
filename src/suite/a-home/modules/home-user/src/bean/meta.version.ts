91720221e1fd67e7d7623f6000da347226de8fb8
zhennann <zhen.nann@icloud.com>
2026-03-09 22:30:16 +0800

    feat: update

--- src/suite/a-home/modules/home-user/src/bean/meta.version.ts
+++ src/suite/a-home/modules/home-user/src/bean/meta.version.ts
@@ -39,16 +39,20 @@ export class MetaVersion extends BeanBase implements IMetaVersionUpdate, IMetaVe
   async init(options: IMetaVersionInitOptions) {
     if (options.version === 1) {
       // role: admin
-      await this.scope.model.role.insert({
-        name: 'admin',
-      });
+      if (!this.scope.config.disableRoleAdmin) {
+        await this.scope.model.role.insert({
+          name: 'admin',
+        });
+      }
       // user: admin
-      await this.bean.authSimple.authenticate({
-        username: 'admin',
-        password: options.password || this.scope.config.passwordDefault.admin,
-        avatar: ':emoji:flower',
-        confirmed: true,
-      }, 'register', 'default');
+      if (!this.scope.config.disableUserAdmin) {
+        await this.bean.authSimple.authenticate({
+          username: 'admin',
+          password: options.password || this.scope.config.passwordDefault.admin,
+          avatar: ':emoji:flower',
+          confirmed: true,
+        }, 'register', 'default');
+      }
     }
   }
 }
