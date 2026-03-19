da0e890ce49257ec25707fdc453ba7f191704d72
zhennann <zhen.nann@icloud.com>
2026-03-19 16:22:23 +0800

    feat: update

--- src/suite-vendor/a-vona/modules/a-openapiutils/src/lib/web.ts
+++ src/suite-vendor/a-vona/modules/a-openapiutils/src/lib/web.ts
@@ -1,4 +1,4 @@
-import type { IApiPathRecord, IFilterTransformRecord, TypeSchemaObjectExtensionFieldFilterTransform } from 'vona-module-a-web';
+import type { IApiPathRecord } from 'vona-module-a-web';
 import { combineParamsAndQuery } from '@cabloy/utils';
 
 export function $apiPath<K extends keyof IApiPathRecord>(path: K): K {
@@ -9,9 +9,9 @@ export function $apiPathAndCombineParamsAndQuery<K extends keyof IApiPathRecord>
   return combineParamsAndQuery(path, options);
 }
 
-export function $makeFilterTransform<T extends keyof IFilterTransformRecord>(
-  filterTransformName: T,
-  options?: Partial<IFilterTransformRecord[T]>,
-): TypeSchemaObjectExtensionFieldFilterTransform {
-  return [filterTransformName, options];
-};
+// export function $makeFilterTransform<T extends keyof IFilterTransformRecord>(
+//   filterTransformName: T,
+//   options?: Partial<IFilterTransformRecord[T]>,
+// ): TypeSchemaObjectExtensionFieldFilterTransform {
+//   return [filterTransformName, options];
+// };
