87a17f837069749c6334363c39fcfd1b4ee13d3c
zhennann <zhen.nann@icloud.com>
2026-03-21 19:38:50 +0800

    feat: update

--- packages-utils/process-helper/src/index.ts
+++ packages-utils/process-helper/src/index.ts
@@ -199,7 +199,7 @@ export class ProcessHelper {
     const timeBegin = new Date();
     await this.console.log('tsc -b begin');
     await this.spawnBin({
-      cmd: 'tsc',
+      cmd: 'tsgo',
       args,
       options,
     });
