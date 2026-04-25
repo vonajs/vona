9cbd8a14e77a5b2f67ffff6e9750629b2b860454
zhennann <zhen.nann@icloud.com>
2026-04-25 11:47:01 +0800

    feat: update

--- packages-cli/cli-set-api/cli/templates/create/project/cabloy-basic/boilerplate/src/suite/cabloy-basic/modules/basic-siteadmin/src/bean/ssrMenuGroup.management.ts
+++ packages-cli/cli-set-api/cli/templates/create/project/cabloy-basic/boilerplate/src/suite/cabloy-basic/modules/basic-siteadmin/src/bean/ssrMenuGroup.management.ts
@@ -4,14 +4,17 @@ import { BeanBase } from 'vona';
 import { $order } from 'vona-module-a-openapiutils';
 import { SsrMenuGroup } from 'vona-module-a-ssr';
 
+import type { ISsrSiteOptionsAdmin } from './ssrSite.admin.ts';
+
 import { $locale } from '../.metadata/locales.ts';
 
-export interface ISsrMenuGroupOptionsManagement extends IDecoratorSsrMenuGroupOptions {}
+export interface ISsrMenuGroupOptionsManagement extends IDecoratorSsrMenuGroupOptions<ISsrSiteOptionsAdmin> {}
 
 @SsrMenuGroup<ISsrMenuGroupOptionsManagement>({
   item: {
     title: $locale('Management'),
     order: $order(2),
+    icon: undefined,
   },
   site: ['basic-siteadmin:admin'],
 })
