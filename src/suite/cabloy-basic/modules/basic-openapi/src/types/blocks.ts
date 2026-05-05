cd60ca06d1fb81b12fcd7a706e99cc0fcf53af84
zhennann <zhen.nann@icloud.com>
2026-05-05 18:22:55 +0800

    Update blocks.ts

--- src/suite/cabloy-basic/modules/basic-openapi/src/types/blocks.ts
+++ src/suite/cabloy-basic/modules/basic-openapi/src/types/blocks.ts
@@ -3,6 +3,7 @@ import type { TableIdentity } from 'table-identity';
 import 'vona-module-a-openapi';
 import type {
   IResourceBlockOptionsBase,
+  IResourceComponentActionBulkOptionsAction,
   IResourceComponentActionRowOptionsAction,
   IResourceComponentBlockOptionsBlock,
   TypeFormScene,
@@ -14,6 +15,14 @@ export interface IResourceBlockOptionsPage extends IResourceBlockOptionsBase {
 }
 
 export interface IResourceBlockOptionsFilter extends IResourceBlockOptionsBase {}
+
+export interface IResourceBlockOptionsToolbarBulk extends IResourceBlockOptionsBase {
+  actions?: IResourceComponentActionBulkOptionsAction[];
+}
+
+export interface IResourceBlockOptionsTable extends IResourceBlockOptionsBase {}
+
+export interface IResourceBlockOptionsPager extends IResourceBlockOptionsBase {}
 
 export interface IResourceBlockOptionsPageEntry extends IResourceBlockOptionsBase {
   blocks?: IResourceComponentBlockOptionsBlock[];
@@ -32,6 +41,9 @@ declare module 'vona-module-a-openapi' {
   export interface IResourceComponentBlockRecord {
     BlockPage?: IResourceBlockOptionsPage;
     BlockFilter?: IResourceBlockOptionsFilter;
+    BlockToolbarBulk?: IResourceBlockOptionsToolbarBulk;
+    BlockTable?: IResourceBlockOptionsTable;
+    BlockPager?: IResourceBlockOptionsPager;
     BlockPageEntry?: IResourceBlockOptionsPageEntry;
     BlockToolbarRow?: IResourceBlockOptionsToolbarRow;
     BlockForm?: IResourceBlockOptionsForm;
