ec8d5db85493e512d2cc46f397b96799c327f407
zhennann <zhen.nann@icloud.com>
2026-05-06 09:33:12 +0800

    Update actionBulk.ts

--- src/suite/cabloy-basic/modules/basic-openapi/src/types/actionBulk.ts
+++ src/suite/cabloy-basic/modules/basic-openapi/src/types/actionBulk.ts
@@ -1,23 +1,14 @@
 import 'vona-module-a-openapi';
-import type {
-  IResourceActionBulkOptionsBase,
-  IResourceComponentActionBulkOptionsAction,
-} from 'vona-module-a-openapi';
+import type { IResourceActionBulkOptionsBase } from 'vona-module-a-openapi';
 
 export interface IResourceActionBulkOptionsCreate extends IResourceActionBulkOptionsBase {}
-
-export interface IResourceActionBulkOptionsOperationsBulk extends IResourceActionBulkOptionsBase {
-  actions?: IResourceComponentActionBulkOptionsAction[];
-}
 
 declare module 'vona-module-a-openapi' {
   export interface IResourceActionBulkRecord {
     create?: IResourceActionBulkOptionsCreate;
-    operationsBulk?: IResourceActionBulkOptionsOperationsBulk;
   }
 
   export interface IResourceComponentActionBulkRecord {
     ActionCreate?: IResourceActionBulkOptionsCreate;
-    ActionOperationsBulk?: IResourceActionBulkOptionsOperationsBulk;
   }
 }
