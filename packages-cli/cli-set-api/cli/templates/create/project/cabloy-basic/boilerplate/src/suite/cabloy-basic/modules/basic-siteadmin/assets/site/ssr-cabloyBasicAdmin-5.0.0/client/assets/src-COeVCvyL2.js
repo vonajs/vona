06dfffed881e8f399dff2f5d680c9380cc0a0b5e
zhennann <zhen.nann@icloud.com>
2026-05-13 15:41:00 +0800

    chore: release

--- packages-cli/cli-set-api/cli/templates/create/project/cabloy-basic/boilerplate/src/suite/cabloy-basic/modules/basic-siteadmin/assets/site/ssr-cabloyBasicAdmin-5.0.0/client/assets/src-COeVCvyL2.js
+++
@@ -1,1 +0,0 @@
-import{a,c as s,i as c,n as o,o as r,r as t,s as i,t as p}from"./basic-captcha-CPE22PfT.js";p();export{i as $useLocale,r as ControllerFormFieldCaptcha,o as ScopeModuleBasicCaptcha,a as ZFormFieldCaptcha,t as components,c as locale,s as locales};
