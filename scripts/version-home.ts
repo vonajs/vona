52cc950d6b6efe8c7ec341b894a57487a9f8548b
zhennann <zhen.nann@icloud.com>
2026-05-14 10:47:27 +0800

    Update version-home.ts

--- scripts/version-home.ts
+++ scripts/version-home.ts
@@ -5,6 +5,12 @@ import { fileURLToPath } from 'node:url';
 
 import { removeSpecialFiles } from './utils.ts';
 
+const __replaceFiles = ['modules/home-user/src/dto/login.ts', 'modules/home-user/src/dto/register.ts'];
+const __replaceContents = [
+  { from: 'zova-rest-cabloy-basic-admin', to: 'zova-rest-cabloy-start-admin' },
+  { from: 'basic-captcha:formFieldCaptcha', to: 'start-captcha:formFieldCaptcha' }
+]
+
 async function _versionTemplate(templateName: string) {
   // cp a-home
   const suiteHomeSrc = path.resolve(fileURLToPath(import.meta.url), '../../src/suite/a-home');
@@ -20,9 +26,25 @@ async function _versionTemplate(templateName: string) {
   }
 }
 
+async function _versionTemplateReplace(templateName: string) {
+  const suiteHomeDest = path.resolve(
+    fileURLToPath(import.meta.url),
+    `../../packages-cli/cli-set-api/cli/templates/create/project/${templateName}/boilerplate/src/suite/a-home`,
+  );
+  for (const file of __replaceFiles) {
+    const filePath = path.join(suiteHomeDest, file);
+    let content = await fs.readFile(filePath, 'utf-8');
+    for (const { from, to } of __replaceContents) {
+      content = content.replaceAll(from, to);
+    }
+    await fs.writeFile(filePath, content, 'utf-8');
+  }
+}
+
 async function versionTemplates() {
   await _versionTemplate('cabloy-basic');
   await _versionTemplate('cabloy-start');
+  await _versionTemplateReplace('cabloy-start');
 }
 
 versionTemplates();
