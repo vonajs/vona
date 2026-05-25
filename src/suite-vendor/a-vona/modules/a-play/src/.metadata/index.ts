ba1a890f6c23172c49e6531f95a41ce5502c6e76
zhennann <zhen.nann@icloud.com>
2026-05-25 15:24:38 +0800

    feat: update

--- src/suite-vendor/a-vona/modules/a-play/src/.metadata/index.ts
+++ src/suite-vendor/a-vona/modules/a-play/src/.metadata/index.ts
@@ -27,19 +27,33 @@ declare module 'vona-module-a-play' {
 }
 /** dto: end */
 /** controller: begin */
+export * from '../controller/memoryDiag.ts';
 export * from '../controller/play.ts';
+import type { IControllerOptionsMemoryDiag } from '../controller/memoryDiag.ts';
 import type { IControllerOptionsPlay } from '../controller/play.ts';
 import 'vona-module-a-web';
 declare module 'vona-module-a-web' {
   
     export interface IControllerRecord {
-      'a-play:play': IControllerOptionsPlay;
+      'a-play:memoryDiag': IControllerOptionsMemoryDiag;
+'a-play:play': IControllerOptionsPlay;
     }
 
   
 }
 declare module 'vona-module-a-play' {
   
+        export interface ControllerMemoryDiag {
+          /** @internal */
+          get scope(): ScopeModuleAPlay;
+        }
+
+          export interface ControllerMemoryDiag {
+            get $beanFullName(): 'a-play.controller.memoryDiag';
+            get $onionName(): 'a-play:memoryDiag';
+            get $onionOptions(): IControllerOptionsMemoryDiag;
+          }
+
         export interface ControllerPlay {
           /** @internal */
           get scope(): ScopeModuleAPlay;
@@ -54,16 +68,27 @@ declare module 'vona-module-a-play' {
 /** controller: end */
 /** controller: begin */
 // @ts-ignore ignore
+import type { ControllerMemoryDiag } from '../controller/memoryDiag.ts';
+// @ts-ignore ignore
 import type { ControllerPlay } from '../controller/play.ts';
 declare module 'vona-module-a-play' {
   
+    export interface IControllerOptionsMemoryDiag {
+      actions?: TypeControllerOptionsActions<ControllerMemoryDiag>;
+    }
+
     export interface IControllerOptionsPlay {
       actions?: TypeControllerOptionsActions<ControllerPlay>;
     }
 }
 declare module 'vona-module-a-web' {
-  export interface IApiPathPostRecord{
-        '/play': undefined;
+  export interface IApiPathGetRecord{
+        '/play/memoryDiag/stats': undefined;
+'/play/memoryDiag/heapSnapshot': undefined;
+    }
+export interface IApiPathPostRecord{
+        '/play/memoryDiag/gc': undefined;
+'/play': undefined;
     }
 
 }
